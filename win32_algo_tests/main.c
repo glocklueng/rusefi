0764aebb7b262cb965a73bbc7e91987a2263cc19
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-10-17 14:32:56 +0000

    algo test project

---
+++ win32_algo_tests/main.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Andrey Belomutskiy 
+ Copyright   : (c) 2012-2013
+ Description : First step towards unit-testing rusEfi algorithms on win32
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
