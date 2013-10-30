59091f7a39ac39f66baf4259f5bda7f23da68ba7
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-10-30 20:18:44 +0000

    findIndex test

---
+++ trunk/win32_algo_tests/test_find_index.h
@@ -0,0 +1,13 @@
+/*
+ * test_find_index.h
+ *
+ *  Created on: Oct 30, 2013
+ *      Author: Andrey
+ */
+
+#ifndef TEST_FIND_INDEX_H_
+#define TEST_FIND_INDEX_H_
+
+void testFindIndex(void);
+
+#endif /* TEST_FIND_INDEX_H_ */
