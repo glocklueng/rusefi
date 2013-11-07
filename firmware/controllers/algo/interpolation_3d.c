b6ad0953fc95677e3661b6c8e484509d8389fe90
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-07 01:17:26 +0000

    refactoring method signature

--- firmware/controllers/algo/interpolation_3d.c
+++ firmware/controllers/algo/interpolation_3d.c
@@ -24,10 +24,9 @@ float interpolate2d(float value, float bin[], float values[], int size) {
 			values[index + 1], value);
 }
 
-float interpolate3d(int rpm, float y, float rpmBin[], int rpmBinSize,
-		float yBin[], int yBinSize, float* map[]) {
+float interpolate3d(float x, float xBin[], int xBinSize, float y, float yBin[], int yBinSize, float* map[]) {
 
-	int rpm_index = findIndex(rpmBin, rpmBinSize, rpm);
+	int rpm_index = findIndex(xBin, xBinSize, x);
 #if	DEBUG_INTERPOLATION
 	printf("rpm index=%d\r\n", rpm_index);
 #endif
@@ -60,18 +59,18 @@ float interpolate3d(int rpm, float y, float rpmBin[], int rpmBinSize,
 	/**
 	 * first we find the interpolated value for this RPM
 	 */
-	int rpmMaxIndex = rpm_index == rpmBinSize - 1 ? rpm_index : rpm_index + 1;
+	int rpmMaxIndex = rpm_index == xBinSize - 1 ? rpm_index : rpm_index + 1;
 
-	int rpmMin = rpmBin[rpm_index];
-	int rpmMax = rpmBin[rpmMaxIndex];
+	int rpmMin = xBin[rpm_index];
+	int rpmMax = xBin[rpmMaxIndex];
 	float rpmMinKeyMinValue = map[rpm_index][key_index];
 	float rpmMaxKeyMinValue = map[rpmMaxIndex][key_index];
 
 	float keyMinValue = interpolate(rpmMin, rpmMinKeyMinValue, rpmMax,
-			rpmMaxKeyMinValue, rpm);
+			rpmMaxKeyMinValue, x);
 
 #if	DEBUG_INTERPOLATION
-	printf("rpm=%d:\r\nrange %d - %d\r\n", rpm, rpmMin, rpmMax);
+	printf("rpm=%d:\r\nrange %d - %d\r\n", x, rpmMin, rpmMax);
 	printf("rpm interpolation range %f   %f result %f\r\n", rpmMinKeyMinValue,
 			rpmMaxKeyMinValue, keyMinValue);
 #endif
@@ -83,7 +82,7 @@ float interpolate3d(int rpm, float y, float rpmBin[], int rpmBinSize,
 	float rpmMaxKeyMaxValue = map[rpmMaxIndex][keyMaxIndex];
 
 	float keyMaxValue = interpolate(rpmMin, rpmMinKeyMaxValue, rpmMax,
-			rpmMaxKeyMaxValue, rpm);
+			rpmMaxKeyMaxValue, x);
 
 #if	DEBUG_INTERPOLATION
 	printf("key=%f:\r\nrange %f - %f\r\n", y, keyMin, keyMax);
