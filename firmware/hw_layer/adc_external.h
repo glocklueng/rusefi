b0d949090fbe7ea7b7fd01e946b94e54cb880cf8
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-13 19:16:58 +0000

    removing dead code

--- firmware/hw_layer/adc_external.h
+++ firmware/hw_layer/adc_external.h
@@ -1,5 +1,6 @@
 /*
- * adc_external.h
+ * @file    adc_external.h
+ *
  *
  *  Created on: Aug 18, 2013
  *      Author: pc
@@ -9,15 +10,6 @@
 #define ADC_EXTERNAL_H_
 
 #include "mcp3208.h"
-
-#define ADC_CHANNEL_VREF ADC_LOGIC_0
-#define ADC_LOGIC_COOLANT ADC_LOGIC_0
-#define ADC_LOGIC_AIR ADC_LOGIC_1
-
-#define ADC_LOGIC_AFR ADC_LOGIC_1
-#define ADC_LOGIC_MAF ADC_LOGIC_2
-#define ADC_LOGIC_MAP ADC_LOGIC_2
-#define ADC_LOGIC_TPS ADC_LOGIC_2
 
 #define getAdcValue(channel) getMcp3208adc(channel)
 #define adcToVolts(adc) ((((float) 5.0) * adc / 4095))
--- firmware/hw_layer/adc_internal_dual_amp.h
+++ firmware/hw_layer/adc_internal_dual_amp.h
@@ -11,20 +11,5 @@
 #include "adc_inputs.h"
 #include "adc_internal_common.h"
 
-// PA6
-#define ADC_LOGIC_MAP ADC_LOGIC_0
-// PA7
-#define ADC_LOGIC_TPS ADC_LOGIC_1
-// PC3
-#define ADC_LOGIC_AFR ADC_LOGIC_7
-
-
-// x2: yellow PC5. SQ4 is channel 3
-#define ADC_CHANNEL_VREF ADC_LOGIC_3
-// x2 board: PB1
-#define ADC_LOGIC_MAF ADC_LOGIC_5
-#define ADC_LOGIC_COOLANT ADC_LOGIC_2
-// PC5
-#define ADC_LOGIC_AIR ADC_LOGIC_3
 
 #endif /* ADC_INTERNAL_DUAL_AMP_H_ */
