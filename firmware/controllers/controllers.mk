a89c6ddae9ee67e8e85804c8fe873840f4cf9cf1
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-16 04:53:51 +0000

    fixing Makefile

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -4,9 +4,10 @@ CONTROLLERSSRC = controllers/main_loop.c \
 	controllers/engine_controller.c \
 	controllers/idle_thread.c \
 	controllers/injector_central.c \
+	controllers/ignition_central.c \
 	controllers/settings.c \
 	controllers/flash_main.c \
-	controllers/rpm_reporter.c \
+	controllers/rpm_calculator.c \
 	controllers/map_multiplier_thread.c \
 	controllers/signal_executor_sleep.c \
 	controllers/signal_executor_hw_timer.c
