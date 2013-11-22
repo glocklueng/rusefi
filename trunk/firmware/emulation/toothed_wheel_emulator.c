678b5db1cfdee6f7d3bc864e243cf0bf1a5e8515
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-22 12:38:32 +0000

    extracting toothed emulator

---
+++ trunk/firmware/emulation/toothed_wheel_emulator.c
@@ -0,0 +1,36 @@
+/*
+ * @file	toothed_wheel_emulator.c
+ * @brief	Missing-tooth wheel signal emulator
+ *
+ *  Created on: Nov 22, 2013
+ *      Author: Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "main.h"
+
+#if EFI_USE_TOOTHED_SENSOR_SHAFT_SENSOR
+
+static myfloat switchTimes[2 * TOTAL_TEETH_COUNT];
+static int pinStates0[2 * TOTAL_TEETH_COUNT];
+
+void skippedToothPositionEmulatorShape(PwmConfig *state) {
+
+	for (int i = 0; i < 2 * TOTAL_TEETH_COUNT; i++) {
+		switchTimes[i] = 1.0 * (i + 1) / (2 * TOTAL_TEETH_COUNT);
+		if (i < 4) {
+			pinStates0[i] = 0;
+		} else {
+			pinStates0[i] = i % 2;
+		}
+		print("pinstate %d: %d\r\n", i, pinStates0[i]);
+	}
+
+	print("last %d\r\n", pinStates0[119]);
+	print("last %d\r\n", pinStates0[119] == 1);
+
+	int *pinStates[1] = { pinStates0 };
+
+	weComplexInit("position sensor", state, 0, 2 * TOTAL_TEETH_COUNT, switchTimes, 1, pinStates);
+}
+
+#endif /* EFI_USE_TOOTHED_SENSOR_SHAFT_SENSOR */
---
+++ trunk/firmware/emulation/toothed_wheel_emulator.h
@@ -0,0 +1,14 @@
+/*
+ * @file	toothed_wheel_emulator.h
+ * @brief	Missing-tooth wheel signal emulator
+ *
+ *  Created on: Nov 22, 2013
+ *      Author: Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#ifndef TOOTHED_WHEEL_EMULATOR_H_
+#define TOOTHED_WHEEL_EMULATOR_H_
+
+void skippedToothPositionEmulatorShape(PwmConfig *state);
+
+#endif /* TOOTHED_WHEEL_EMULATOR_H_ */
