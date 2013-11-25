2474180fdf9effc5fdf514735491c7e3223e322c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-25 23:51:50 +0000

    new engine to the Makefile

--- firmware/config/engines/engines.mk
+++ firmware/config/engines/engines.mk
@@ -1,5 +1,6 @@
 
-ENGINES_SRC = config/engines/ford_aspire.c \
+ENGINES_SRC = config/engines/audi_aan.c \
+	config/engines/ford_aspire.c \
 	config/engines/ford_fiesta.c \
 	config/engines/nissan_primera.c \
 	config/engines/snow_blower.c
