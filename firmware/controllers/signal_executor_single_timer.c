cb9d2694ffefafede6ac40a7382e5f70e6358c00
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-27 22:23:52 +0000

    home for the future single timer executor

---
+++ firmware/controllers/signal_executor_single_timer.c
@@ -0,0 +1,9 @@
+/**
+ * @file	signal_executor_single_timer.c
+ *
+ *
+ *  Created on: Nov 27, 2013
+ *
+ */
+
+
---
+++ firmware/controllers/signal_executor_single_timer.h
@@ -0,0 +1,12 @@
+/**
+ * @file signal_executor_single_timer.h
+ *
+ *  Created on: Nov 27, 2013
+ */
+
+#ifndef SIGNAL_EXECUTOR_SINGLE_TIMER_H_
+#define SIGNAL_EXECUTOR_SINGLE_TIMER_H_
+
+
+
+#endif /* SIGNAL_EXECUTOR_SINGLE_TIMER_H_ */
