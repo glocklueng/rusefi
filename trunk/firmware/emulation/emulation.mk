2f36bcfe96a5bf866a892eed7af860ba928df871
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-27 13:25:59 +0000

    fixing Makefile & fixing out of bounds array access

--- trunk/firmware/emulation/emulation.mk
+++ trunk/firmware/emulation/emulation.mk
@@ -3,5 +3,6 @@ EMULATIONSRC = emulation/dist_emulator.c \
 	emulation/hw_layer/poten.c \
 	emulation/wave_chart.c \
 	emulation/wave_analyzer.c \
+	emulation/toothed_wheel_emulator.c \
 	emulation/engine_emulator.c 
 	
