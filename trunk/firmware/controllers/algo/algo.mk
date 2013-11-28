e46cfeb5aece97f5d37a86555ee0aea797ed9b5a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-28 04:06:14 +0000

    HIP9011 driver

--- trunk/firmware/controllers/algo/algo.mk
+++ trunk/firmware/controllers/algo/algo.mk
@@ -3,4 +3,5 @@ CONTROLLERS_ALGO_SRC = controllers/algo/map_adjuster.c \
 	controllers/alog/fuel_map.c \
 	controllers/algo/engine_configuration.c \
 	controllers/algo/interpolation.c \
+	controllers/algo/event_registry.c \
 	controllers/algo/idle_controller.c
