5bced67c1a44e99070a20405fbce8cb5a801b503
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-11-28 04:06:14 +0000

    HIP9011 driver

---
+++ firmware/hw_layer/HIP9011.c
@@ -0,0 +1,9 @@
+/*
+ * @file	HIP9011.c
+ * @brief	HIP9011/TPIC8101 driver
+ *
+ *  Created on: Nov 27, 2013
+ *      Author: Andrey Belomutskiy, (c) 2012-2013
+ */
+
+
--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -7,6 +7,7 @@ HW_LAYERESRC = hw_layer/hardware.c \
 	hw_layer/algo/multi_shaft_sensor.c \
 	hw_layer/algo/toothed_shaft_sensor.c \
 	hw_layer/mcp3208.c \
+	hw_layer/HIP9011.c \
 	hw_layer/flash.c \
 	hw_layer/gpio_helper.c \
 	hw_layer/wave_analyzer_hw.c \
