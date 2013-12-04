389c8a8d5b893ef5da99d0ad8d21348ecee23355
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-04 12:25:46 +0000

    now with zipping

--- firmware/compile.bat
+++ firmware/compile.bat
@@ -4,5 +4,14 @@ svn info > ../firmware_binary/version.txt
 cp config/features.h ../firmware_binary
 cp build/rusefi.hex ../firmware_binary
 
+cp tunerstudio/rusefi.ini ../firmware_binary
+
+cd ../firmware_binary
+del firmaware_binary.zip
+7z a firmaware_binary.zip rusefi.hex rusefi.ini features.h flash.bat
+cd ../firmware
+
+
 cd build
 java -jar ../../java_tools/gcc_map_reader.jar > rusefi_ram_report.txt
+cd ..
