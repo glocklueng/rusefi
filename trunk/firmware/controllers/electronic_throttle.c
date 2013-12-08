df0ad8ece7c1a317d6a84a6627dd984fa8bde0de
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-08 00:58:18 +0000

    electronic throttle

--- trunk/firmware/controllers/electronic_throttle.c
+++ trunk/firmware/controllers/electronic_throttle.c
@@ -1,5 +1,8 @@
 /**
  * @file	electronic_throttle.c
+ * @brief	Electronic Throttle Module driver L298N
+ *
+ * todo: make this more universal if/when we get other hardware options
  *
  * @date Dec 7, 2013
  * @author Andrey Belomutskiy, (c) 2012-2013
