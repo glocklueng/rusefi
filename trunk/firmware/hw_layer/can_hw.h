c3e52607d54372d869aeae6d507954f69803bfe0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-12 02:20:50 +0000

    CAN bus test code

---
+++ trunk/firmware/hw_layer/can_hw.h
@@ -0,0 +1,13 @@
+/**
+ * @file	can_hw.h
+ *
+ * @date Dec 11, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#ifndef CAN_HW_H_
+#define CAN_HW_H_
+
+void initCan(void);
+
+#endif /* CAN_HW_H_ */
