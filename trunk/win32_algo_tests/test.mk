b454a5695c4fa02251590481ecdd57a8f714fbee
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-12 19:41:43 +0000

    nicer Makefile for tests

---
+++ trunk/win32_algo_tests/test.mk
@@ -0,0 +1,11 @@
+TEST_SRC = boards.c \
+	test_cyclic_buffer.c \
+	test_engine_math.c \
+	test_event_registry.c \
+	test_find_index.c \
+	test_fuel_map.c \
+	test_idle_controller.c \
+	test_interpolation_3d.c \
+	test_sensors.c \
+	test_signal_executor.c
+
