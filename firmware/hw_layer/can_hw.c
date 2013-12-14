1cf0be9806ea1531bcf4ec48237bef6885cfa27e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-14 03:08:49 +0000

    more CAN code

--- firmware/hw_layer/can_hw.c
+++ firmware/hw_layer/can_hw.c
@@ -18,20 +18,67 @@ static Logging logger;
  * See section 22.7.7 on the STM32 reference manual.
  */
 static const CANConfig canConfig = {
-  CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-  CAN_BTR_SJW(0) | CAN_BTR_TS2(1) |
-  CAN_BTR_TS1(8) | CAN_BTR_BRP(6)
-};
+CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+CAN_BTR_SJW(0) | CAN_BTR_TS2(1) |
+CAN_BTR_TS1(8) | CAN_BTR_BRP(6) };
 
-static CANRxFrame rxFrame;
+static CANRxFrame rxBuffer;
+static CANTxFrame txmsg;
+
+#define CAN_BMW_E46_CLUSTER_STATUS 0x613
+
+static void printPacket(CANRxFrame *rx) {
+	scheduleSimpleMsg(&logger, "GOT FMI ", rx->FMI);
+	scheduleSimpleMsg(&logger, "GOT TIME ", rx->TIME);
+	scheduleSimpleMsg(&logger, "GOT DLC ", rx->DLC);
+	scheduleSimpleMsg(&logger, "GOT SID ", rx->SID);
+
+	if ( rx->SID == CAN_BMW_E46_CLUSTER_STATUS) {
+		int odometerKm = 10 * (rx->data8[1] << 8) + rx->data8[0];
+		int odometerMi = odometerKm * 0.621371;
+		scheduleSimpleMsg(&logger, "GOT odometerKm ", odometerKm);
+		scheduleSimpleMsg(&logger, "GOT odometerMi ", odometerKm);
+		int timeValue = (rx->data8[4] << 8) + rx->data8[3];
+		scheduleSimpleMsg(&logger, "GOT time ", timeValue);
+	}
+
+	chThdSleepMilliseconds(5);
+	print("d0 %d\r\n", rx->data8[0]);
+	print("d1 %d\r\n", rx->data8[1]);
+	print("d2 %d\r\n", rx->data8[2]);
+	print("d3 %d\r\n", rx->data8[3]);
+	print("d4 %d\r\n", rx->data8[4]);
+	print("d5 %d\r\n", rx->data8[5]);
+	print("d6 %d\r\n", rx->data8[6]);
+	print("d7 %d\r\n", rx->data8[7]);
+
+	chThdSleepMilliseconds(5);
+
+}
+
+void sendRpm(void) {
+	txmsg.IDE = CAN_IDE_STD;
+	txmsg.EID = 0x316;
+	txmsg.RTR = CAN_RTR_DATA;
+	txmsg.DLC = 8;
+	txmsg.data16[1] = 1500 * 6.4;
+//	canTransmit(&EFI_CAN_DEVICE, CAN_ANY_MAILBOX, &txmsg, MS2ST(100));
+	canTransmit(&EFI_CAN_DEVICE, CAN_ANY_MAILBOX, &txmsg, TIME_INFINITE);
+
+}
 
 static void canRead(void) {
 	scheduleSimpleMsg(&logger, "waiting for CAN ", 0);
-	canReceive(&EFI_CAN_DEVICE, CAN_ANY_MAILBOX, &rxFrame, TIME_INFINITE);
-	scheduleSimpleMsg(&logger, "GOT CAN ", rxFrame.FMI);
-	scheduleSimpleMsg(&logger, "GOT CAN ", rxFrame.TIME);
-	scheduleSimpleMsg(&logger, "GOT CAN ", rxFrame.DLC);
-	scheduleSimpleMsg(&logger, "GOT CAN ", rxFrame.SID);
+	canReceive(&EFI_CAN_DEVICE, CAN_ANY_MAILBOX, &rxBuffer, TIME_INFINITE);
+
+	printPacket(&rxBuffer);
+
+//	while(1) {
+	sendRpm();
+//		chThdSleepMilliseconds(1);
+//	}
+
+//txmsg.IDE = CAN_IDE_EXT;
 }
 
 void initCan(void) {
