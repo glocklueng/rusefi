6d8a76a6017b601fde23a69bc389e670f301872c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-18 17:15:10 +0000

    improving configuration

--- firmware/controllers/settings.c
+++ firmware/controllers/settings.c
@@ -1,5 +1,6 @@
-/*
- * injector_control.c
+/**
+ * @file settings.c
+ * @brief This file is about configuring engine via the human-readable protocol
  *
  * @date Dec 30, 2012
  * @author Andrey Belomutskiy, (c) 2012-2013
@@ -10,11 +11,13 @@
 #include "rficonsole.h"
 #include "datalogging.h"
 #include "main.h"
+#include "engine_configuration.h"
 
-static volatile int crankingInjectionPeriod = 0;
 static volatile int injectionOffset = 0;
 
 static Logging logger;
+
+extern EngineConfiguration *engineConfiguration;
 
 static void printSettings(void) {
 	appendPrintf(&logger, "msg,injectionOffset %d%s", injectionOffset, DELIMETER);
@@ -22,23 +25,14 @@ static void printSettings(void) {
 	printLine(&logger);
 }
 
-int getInjectionOffset(void) {
-	return injectionOffset;
-}
-
-void setInjectionOffset(int value) {
-	injectionOffset = value;
+static void setIgnitonOffset(int value) {
+	engineConfiguration->ignitonOffset = value;
 	printSettings();
 }
 
 void initSettings(void) {
-	// todo: this whole method is legacy stuff
-	// todo: the whole file is prettu much legacy pre-TunerStudio stuff
-	initLogging(&logger, "inje control");
+	initLogging(&logger, "settings control");
 
-	setInjectionOffset(0);
-	//setCrankingInjectionPeriod(TICKS_IN_MS * 1.5);
-
-	addConsoleActionI("o", &setInjectionOffset);
+	addConsoleActionI("set_ignition_offset", &setIgnitonOffset);
 }
 
