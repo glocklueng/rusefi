4d1c9f7dcc0b9b407195fccd43fa85ebb0aef97d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-20 19:19:55 +0000

    malfunction indicator

---
+++ trunk/firmware/controllers/algo/malfunction_central.h
@@ -0,0 +1,14 @@
+/**
+ * @file malfunction_central.h
+ * @brief This data structure holds current malfunction codes
+ *
+ * @date Dec 20, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#ifndef MALFUNCTION_CENTRAL_H_
+#define MALFUNCTION_CENTRAL_H_
+
+
+
+#endif /* MALFUNCTION_CENTRAL_H_ */
--- trunk/firmware/controllers/malfunction_indicator.c
+++ trunk/firmware/controllers/malfunction_indicator.c
@@ -1,5 +1,6 @@
 /*
  * @file malfunction_indicator.c
+ * @brief We can blink out OBD-II error codes using Malfunction Indicator Light (MIL)
  *
  *
  * @date Dec 20, 2013
