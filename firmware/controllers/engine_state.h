3d532500262db9ff9b9433fd91506d05fa80b33f
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-20 15:33:05 +0000

    refactoring

--- firmware/controllers/engine_state.h
+++ firmware/controllers/engine_state.h
@@ -2,8 +2,8 @@
  * @file engine_state.h
  * @brief One header which acts as gateway to current engine state
  *
- *  Created on: Dec 20, 2013
- *      Author: Andrey
+ * @date Dec 20, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2013
  */
 
 #ifndef ENGINE_STATE_H_
