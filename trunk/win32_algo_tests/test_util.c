993d7b8a08e35237d114436c591f0fd263e4575c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-20 16:33:20 +0000

    unit test for histogram

--- trunk/win32_algo_tests/test_util.c
+++ trunk/win32_algo_tests/test_util.c
@@ -32,4 +32,37 @@ void testHistogram(void) {
 	assertEquals(80, histogramGetIndex(239));
 	assertEquals(223, histogramGetIndex(239239));
 	assertEquals(364, histogramGetIndex(239239239));
+
+	histogram_s h;
+
+	resetHistogram(&h);
+
+	int result[5];
+	assertEquals(0, hsReport(&h, result));
+
+	hsAdd(&h, 10);
+	assertEquals(1, hsReport(&h, result));
+	assertEquals(10, result[0]);
+
+	// let's add same value one more time
+	hsAdd(&h, 10);
+	assertEquals(2, hsReport(&h, result));
+	assertEquals(10, result[0]);
+	assertEquals(10, result[1]);
+
+	hsAdd(&h, 10);
+	hsAdd(&h, 10);
+	hsAdd(&h, 10);
+
+	hsAdd(&h, 1000);
+	hsAdd(&h, 100);
+
+	assertEquals(5, hsReport(&h, result));
+
+	assertEquals(5, result[0]);
+	assertEquals(10, result[1]);
+	assertEquals(10, result[2]);
+	assertEquals(100, result[3]);
+	// values are not expected to be exactly the same, it's the shape what matters
+	assertEquals(1011, result[4]);
 }
