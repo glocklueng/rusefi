d60b400c65db6173bcb616e3a57f3528963281db
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-21 14:59:42 +0000

    MIL

--- firmware/controllers/malfunction_indicator.c
+++ firmware/controllers/malfunction_indicator.c
@@ -8,4 +8,20 @@
  * @author Andrey Belomutskiy, (c) 2012-2013
  */
 
+#include "main.h"
+#include "malfunction_indicator.h"
+
+
+static WORKING_AREA(mfiThreadStack, UTILITY_THREAD_STACK_SIZE);	// declare thread
+
+//  our main thread for show check engine error
+static msg_t mfiThread(void *arg) {
+	return 0;
+}
+
+void initMalfunctionIndicator(void)
+{
+	// create static thread
+	chThdCreateStatic(mfiThreadStack, sizeof(mfiThreadStack), LOWPRIO, (tfunc_t) mfiThread, NULL);
+}
 
