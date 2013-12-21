c6b09df96e70e2f323cb7d36b8288f8d033ba100
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-21 15:20:42 +0000

    error codes central

--- trunk/firmware/controllers/algo/malfunction_central.c
+++ trunk/firmware/controllers/algo/malfunction_central.c
@@ -6,4 +6,10 @@
  * @author Andrey Belomutskiy, (c) 2012-2013
  */
 
+#include "malfunction_central.h"
 
+static error_codes_set_s error_codes_set;
+
+void initMalfunctionCentral(void) {
+	error_codes_set.count = 0;
+}
