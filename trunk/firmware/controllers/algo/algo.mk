75025d0c520c2a7a4d5ebe1b128868857b6fb7e9
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-22 20:34:39 +0000

    bugfix

--- trunk/firmware/controllers/algo/algo.mk
+++ trunk/firmware/controllers/algo/algo.mk
@@ -4,6 +4,7 @@ CONTROLLERS_ALGO_SRC = $(PROJECT_DIR)/controllers/algo/map_adjuster.c \
 	$(PROJECT_DIR)/controllers/algo/engine_configuration.c \
 	$(PROJECT_DIR)/controllers/algo/signal_executor.c \
 	$(PROJECT_DIR)/controllers/algo/interpolation.c \
+	$(PROJECT_DIR)/controllers/algo/malfunction_central.c \
 	$(PROJECT_DIR)/controllers/algo/event_registry.c \
 	$(PROJECT_DIR)/controllers/algo/idle_controller.c \
 	$(PROJECT_DIR)/controllers/algo/signal_executor_single_timer_algo.c
