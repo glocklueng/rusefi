50fd1cd022bd84279b9fad28be39b8138a8e79c0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-23 22:10:31 +0000

    trying to fix the tests

--- trunk/firmware/controllers/algo/trigger_structure.h
+++ trunk/firmware/controllers/algo/trigger_structure.h
@@ -8,7 +8,34 @@
 #ifndef TRIGGER_STRUCTURE_H_
 #define TRIGGER_STRUCTURE_H_
 
-#include "pwm_generator.h"
+#define PWM_PHASE_MAX_COUNT 150
+#define PWM_PHASE_MAX_WAVE_PER_PWM 2
+
+
+/**
+ * @brief   PWM configuration for the specific output pin
+ */
+typedef struct {
+	int pinStates[PWM_PHASE_MAX_COUNT];
+} single_wave_s;
+
+typedef struct {
+	/**
+	 * Number of events in the cycle
+	 */
+	int phaseCount;
+	/**
+	 * Number of signal wires
+	 */
+	int waveCount;
+	single_wave_s waves[PWM_PHASE_MAX_WAVE_PER_PWM];
+	/**
+	 * values in the (0..1] range which refer to points within the period at at which pin state should be changed
+	 * So, in the simplest case we turn pin off at 0.3 and turn it on at 1 - that would give us a 70% duty cycle PWM
+	 */
+	myfloat switchTimes[PWM_PHASE_MAX_COUNT];
+} multi_wave_s;
+
 
 typedef enum {
 	T_PRIMARY = 0,
