74f3b8f33dcb05a6363a58c5c0c4e0a50e8ada5b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-23 21:06:33 +0000

    MIL

--- firmware/controllers/malfunction_indicator.c
+++ firmware/controllers/malfunction_indicator.c
@@ -21,57 +21,59 @@
 #define MFI_CHECKENGINE_LIGHT 10000
 
 int iMFIerror = 32;			// in future this error code will be check from main status engine structure
-// todo: we need to add a new output PIN (new value for PinEnum and new PIN configuration and initialization)
-int MFIled = LED_RUNNING;	// specify what pin must be lighting
 
 static WORKING_AREA(mfiThreadStack, UTILITY_THREAD_STACK_SIZE);	// declare thread
 
 static void blink_digits(int digit, int duration) {
 	for (int iter = 0; iter < digit; iter++) {
-		turnOutputPinOn(MFIled);
+		turnOutputPinOn(LED_CHECK_ENGINE);
 		chThdSleepMilliseconds(duration);
-		turnOutputPinOff(MFIled);
+		turnOutputPinOff(LED_CHECK_ENGINE);
 		chThdSleepMilliseconds(MFI_BLINK_SEPARATOR);
+	}
+}
+
+// calculate how many digits our code have
+static int DigitLength(int digit) {
+	int i = 0;
+	while (digit > 0) {
+		digit = digit / 10;
+		++i;
+	}
+	return i;
+}
+
+// display code
+static void DisplayErrorCode(int length, int code) {
+	for (int iter = length - 1; iter >= 0; iter--) {
+		int ourDigit = pow(10, iter);		// 10^0 = 1, 10^1 = 10, 10^2=100, 10^3 = 1000, ....
+		int digit = 1;						// as we remember "0" we show as one blink
+		while (code >= ourDigit) {
+			code = code - ourDigit;
+			digit++;
+		}
+		if (iter % 2 == 0)
+			blink_digits(digit, MFI_SHORT_BLINK);		// even 2,0 - long blink
+		else
+			blink_digits(digit, MFI_LONG_BLINK); 		// odd  3,1 - short blink
 	}
 }
 
 //  our main thread for show check engine error
 static msg_t mfiThread(void) {
-	// todo: while it is the an C tradition to declare fields in the beginning of a method
-	// todo: I think we better move declarations closer to the usages to reduce visibility
-	// todo: and simply future refactoring
-	int MFerr, digit, ErrLength;
 	chRegSetThreadName("MFIndicator");
 
 	while (TRUE) {
 		// todo: extract this as 'blinkOneCode' method
 		if (iMFIerror > 0) {
-			// First at all calculate how many digits in this integer
-			ErrLength = iMFIerror;
-			int i=0;
-			while (ErrLength>0) {
-				ErrLength = ErrLength / 10;
-				++i;
-			}
-			ErrLength = i;
-			print("size of ErrKegth = %d == %d\n\r", iMFIerror, i);
 			// switch on check engine light on 10sec (one long blink)
+			// todo: this needs to be a SLEEP, this has nothing to do with digits!
+			// todo: if the problem is with LED invertion, it should be solved properly
+			// todo: using 'blink_digits' to sleep is a hack :( we do not like hacks
 			blink_digits(1, MFI_CHECKENGINE_LIGHT);
-			// MFerr - local copy of iMFIerror (part of general structure)
-			MFerr = iMFIerror;
-			for (int iter = ErrLength-1; iter >= 0; iter--) {
-				int ourDigit = pow(10, iter);		// 10^0 = 1, 10^1 = 10, 10^2=100, 10^3 = 1000
-				// todo: '0' as one blink and '1' as two blinks is crazy! we should make '0' ten blinks
-				digit = 1;				// as we remember "0" we show as one blink 
-				while (MFerr >= ourDigit) {
-					MFerr = MFerr - ourDigit;
-					digit++;
-				}
-				if (iter % 2 == 0)
-					blink_digits(digit, MFI_SHORT_BLINK);		// even 2,0 - long blink
-				else
-					blink_digits(digit, MFI_LONG_BLINK); 		// odd  3,1 - short blink
-			}
+			// First at all calculate how many digits in this integer
+			// and display error code
+			DisplayErrorCode(DigitLength(iMFIerror), iMFIerror);
 		}
 	}
 	return 0;
