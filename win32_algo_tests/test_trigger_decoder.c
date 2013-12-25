724e6ccca5c1e10dabf09f16c335146ec34339b4
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-25 00:39:49 +0000

    trigger test skeleton

---
+++ win32_algo_tests/test_trigger_decoder.c
@@ -0,0 +1,12 @@
+/**
+ * @file	test_trigger_decoder.c
+ *
+ * @date Dec 24, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "test_trigger_decoder.h"
+
+void testTriggerDecoder(void) {
+
+}
