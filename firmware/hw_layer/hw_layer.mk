ae38263a601d606333f73a4733ffb8fcd05ec36c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-30 19:25:30 +0000

    FatFs

--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -8,6 +8,8 @@ HW_LAYERESRC = hw_layer/hardware.c \
 	hw_layer/can_hw.c \
 	hw_layer/HIP9011.c \
 	hw_layer/flash.c \
+	hw_layer/mmc_card.c \
+	hw_layer/neo6m.c \
 	hw_layer/gpio_helper.c \
 	hw_layer/wave_analyzer_hw.c \
 	hw_layer/pwm_generator.c
