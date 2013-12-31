36203de9a8f165713ef9390e39fa79c56e183f8d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-31 03:08:57 +0000

    ext folder for tests

--- trunk/win32_algo_tests/makelinks.bat
+++ trunk/win32_algo_tests/makelinks.bat
@@ -3,4 +3,5 @@ junction controllers_algo ..\firmware\controllers\algo
 junction controllers_math ..\firmware\controllers\math 
 junction controllers_sensors ..\firmware\controllers\sensors
 junction util ..\firmware\util
+junction ext ..\firmware\ext
 
