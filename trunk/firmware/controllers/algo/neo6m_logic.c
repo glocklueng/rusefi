171625ce591020c72fed22f27bcca2f0ff6aba92
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2013-12-31 02:23:07 +0000

    separating logic from hardware

---
+++ trunk/firmware/controllers/algo/neo6m_logic.c
@@ -0,0 +1,12 @@
+/**
+ * @file neo6m_logic.c
+ *
+ * @date Dec 30, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "neo6m_logic.h"
+
+void parseNeo6m(gps_position_s *position, char *) {
+
+}
