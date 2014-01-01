6c68588e1e77cc43351f1bee38f8625ae61bfe2e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-01 04:39:06 +0000

    making progress

--- hardware/frankenstein/rebuild.bat
+++ hardware/frankenstein/rebuild.bat
@@ -15,7 +15,7 @@ cp ../usb_mmc_spi/lib/* lib/
 cp ../usb_mmc_spi/mmc_usb_1.sch .
 
 rem Now let's merge all compoennt mappings automatically. Magic!
-java -jar ../../java_tools/pcb_sync.jar cmp_merge frankenstein.cmp ../1A_injector_6-channels/inj_6ch.cmp ../adc_amp_divider.vertival/adc_amp_divider_vert.cmp
+java -jar ../../java_tools/pcb_sync.jar cmp_merge frankenstein.cmp ../1A_injector_6-channels/inj_6ch.cmp ../adc_amp_divider.vertival/adc_amp_divider_vert.cmp ../usb_mmc_spi/mmc_usb_1.cmp
 
 rem Let's merge these PCBs into the Frankenstein!
-java -jar ../../java_tools/pcb_sync.jar pcb_merge frankenstein_template.kicad_pcb frankenstein.kicad_pcb ../1A_injector_6-channels/inj_6ch.kicad_pcb ../adc_amp_divider.vertical/adc_amp_divider_vert.kicad_pcb
+java -jar ../../java_tools/pcb_sync.jar pcb_merge frankenstein_template.kicad_pcb frankenstein.kicad_pcb ../1A_injector_6-channels/inj_6ch.kicad_pcb ../adc_amp_divider.vertical/adc_amp_divider_vert.kicad_pcb ../usb_mmc_spi/mmc_usb_1.kicad_pcb
