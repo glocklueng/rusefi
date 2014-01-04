42eb1ad5227a54e4612fd3db9d5bf727a4e1059c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-04 23:28:28 +0000

    interesting

--- win32_algo_tests/test_signal_executor.c
+++ win32_algo_tests/test_signal_executor.c
@@ -51,7 +51,7 @@ void testSignalExecutor() {
 
 	now = 100;
 	testToggleCounter = 0;
-	assertEquals(10, toggleSignalIfNeeded(&s1, now));
+	assertEquals(100, toggleSignalIfNeeded(&s1, now));
 	assertEquals(1, testToggleCounter);
 
 	now = 300; // let's see what happens if the handler is late
