f72638508247f5971ce8fceb2ab16787fb146a58
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-12 05:06:55 +0000

    fixing unit tests build

--- trunk/firmware/util/util.mk
+++ trunk/firmware/util/util.mk
@@ -4,4 +4,5 @@ UTILSRC = $(PROJECT_DIR)/util/data_buffer.c \
 	$(PROJECT_DIR)/util/crc.c \
 	$(PROJECT_DIR)/util/histogram.c \
 	$(PROJECT_DIR)/util/cyclic_buffer.c \
+	$(PROJECT_DIR)/util/rficonsole_logic.c \
 	$(PROJECT_DIR)/util/wave_math.c
