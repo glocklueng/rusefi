d8f828bb65c4ecdb05e6b817326d2f0e7523239b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-19 01:00:26 +0000

    improving .mk file

--- trunk/firmware/ext_algo/nmea/nmea.mk
+++ trunk/firmware/ext_algo/nmea/nmea.mk
@@ -1,11 +1,11 @@
 
-+NMEASRC = ext_algo/nmea/context.c \
-		ext_algo/nmea/generate.c \
-		ext_algo/nmea/generator.c \
-		ext_algo/nmea/gmath.c \
-		ext_algo/nmea/info.c \
-		ext_algo/nmea/parse.c \
-		ext_algo/nmea/parser.c \
-		ext_algo/nmea/sentence.c \
-		ext_algo/nmea/time.c \
-		ext_algo/nmea/tok.c  
++NMEASRC = $(PROJECT_DIR)/ext_algo/nmea/context.c \
+		$(PROJECT_DIR)/ext_algo/nmea/generate.c \
+		$(PROJECT_DIR)/ext_algo/nmea/generator.c \
+		$(PROJECT_DIR)/ext_algo/nmea/gmath.c \
+		$(PROJECT_DIR)/ext_algo/nmea/info.c \
+		$(PROJECT_DIR)/ext_algo/nmea/parse.c \
+		$(PROJECT_DIR)/ext_algo/nmea/parser.c \
+		$(PROJECT_DIR)/ext_algo/nmea/sentence.c \
+		$(PROJECT_DIR)/ext_algo/nmea/time.c \
+		$(PROJECT_DIR)/ext_algo/nmea/tok.c  
