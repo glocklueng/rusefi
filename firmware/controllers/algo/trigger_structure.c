df14292adfa8d97bd1118129741dfeb708ba033d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-21 00:20:27 +0000

    refactoring

---
+++ firmware/controllers/algo/trigger_structure.c
@@ -0,0 +1,34 @@
+/**
+ * @file	trigger_structure.c
+ *
+ * @date Jan 20, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "trigger_structure.h"
+
+void triggerShapeInit(trigger_shape_s *trigger) {
+	trigger->size = 0;
+}
+
+void triggerAddEvent(trigger_shape_s *trigger, float angle, trigger_wheel_e waveIndex, int state) {
+	angle /= 720;
+	if (trigger->size == 0) {
+		trigger->size = 1;
+		for (int i = 0; i < PWM_PHASE_MAX_WAVE_PER_PWM; i++)
+			trigger->wave.waves[i].pinStates[0] = 0;
+
+		trigger->wave.switchTimes[0] = angle;
+		trigger->wave.waves[waveIndex].pinStates[0] = state;
+		return;
+	}
+
+//	if(angle!=trigger->wave.switchTimes[trigger->currentIndex])
+
+	int index = trigger->size++;
+
+	for (int i = 0; i < PWM_PHASE_MAX_WAVE_PER_PWM; i++)
+		trigger->wave.waves[i].pinStates[index] = trigger->wave.waves[i].pinStates[index - 1];
+	trigger->wave.switchTimes[index] = angle;
+	trigger->wave.waves[waveIndex].pinStates[index] = state;
+}
