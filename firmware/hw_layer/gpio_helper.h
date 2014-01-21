bed3d8db766fca32a0ab3f0809676df2d80fc601
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-21 02:39:23 +0000

    I-O bugfix

--- firmware/hw_layer/gpio_helper.h
+++ firmware/hw_layer/gpio_helper.h
@@ -18,12 +18,12 @@ typedef struct {
 	 * we track current pin status so that we do not touch the actual hardware if we want to write new pin bit
 	 * which is same as current pin value. This maybe helps in case of status leds, but maybe it's a total over-engineering
 	 */
-	int currentValue;
+	int currentLogicValue;
 } OutputPin;
 
 void initOutputPin(char *msg, OutputPin *outputPin, GPIO_TypeDef *port, uint32_t pinNumber);
 void initOutputPinExt(char *msg, OutputPin *outputPin, GPIO_TypeDef *port, uint32_t pinNumber, iomode_t mode);
-int getPinValue(OutputPin * outputPin);
-void setPinValue(OutputPin * outputPin, int electricalValue);
+int getLogicPinValue(OutputPin * outputPin);
+void setPinValue(OutputPin * outputPin, int electricalValue, int logicValue);
 
 #endif /* GPIO_HELPER_H_ */
