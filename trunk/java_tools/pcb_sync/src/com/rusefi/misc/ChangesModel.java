e6ca633cb9c4519936fe5a0c45e6ca56241ebc88
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-24 20:32:21 +0000

    new request file format

--- trunk/java_tools/pcb_sync/src/com/rusefi/misc/ChangesModel.java
+++ trunk/java_tools/pcb_sync/src/com/rusefi/misc/ChangesModel.java
@@ -1,7 +1,11 @@
 package com.rusefi.misc;
 
 import com.rusefi.pcb.PcbMergeTool;
+import com.rusefi.util.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,11 +16,23 @@ import java.util.TreeSet;
  */
 public class ChangesModel {
     private static final ChangesModel instance = new ChangesModel();
+    public static final String REMOVE = "remove";
+    private static final String ADD = "add";
 
-    public Set<String> DEL = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+    public final Set<String> DEL_REQUESTS = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+    public final List<AddRequest> ADD_REQUESTS = new ArrayList<AddRequest>();
 
     public static ChangesModel getInstance() {
         return instance;
+    }
+
+    public static void readConfiguration() throws IOException {
+        String changesFileName = "pcb_merge_changes.txt";
+        if (new File(changesFileName).isFile()) {
+            List<String> a = FileUtils.readFileToList(changesFileName);
+
+            getInstance().read(a);
+        }
     }
 
     public void read(List<String> lines) {
@@ -24,8 +40,45 @@ public class ChangesModel {
             line = line.trim();
             if (line.isEmpty())
                 continue;
-            DEL.add(line);
+            if (line.startsWith("#"))
+                continue; // this line is a comment
+
+            if (line.toLowerCase().startsWith(REMOVE)) {
+                addDelRequest(line.substring(REMOVE.length()).trim());
+                continue;
+            } else if (line.toLowerCase().startsWith(ADD)) {
+
+                addAddRequest(line.substring(ADD.length()).trim());
+                continue;
+
+
+            }
+
+            System.err.println("Ignoring invalid line: " + line);
+
         }
-        PcbMergeTool.log("Got " + DEL.size() + " delete entries");
+        PcbMergeTool.log("Got " + DEL_REQUESTS.size() + " remove request(s)");
+        PcbMergeTool.log("Got " + ADD_REQUESTS.size() + " add request(s)");
+    }
+
+    private void addAddRequest(String request) {
+        String[] tokens = request.split(" ");
+        if (tokens.length == 1) {
+            ADD_REQUESTS.add(new AddRequest(tokens[0], 0, 0));
+        } else if (tokens.length == 3) {
+            double x = Double.parseDouble(tokens[1]);
+            double y = Double.parseDouble(tokens[1]);
+            ADD_REQUESTS.add(new AddRequest(tokens[0], x, y));
+        } else {
+            throw new IllegalArgumentException("Invalid: " + request);
+        }
+    }
+
+    private void addDelRequest(String request) {
+        DEL_REQUESTS.add(request);
+    }
+
+    public void clear() {
+        DEL_REQUESTS.clear();
     }
 }
