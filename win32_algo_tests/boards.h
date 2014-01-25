4f77d1c4402eadd50a2f1340a7ab82e1d48bbe1e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-01-25 15:06:54 +0000

    fixing build

--- win32_algo_tests/boards.h
+++ win32_algo_tests/boards.h
@@ -13,7 +13,7 @@
 #define ADC_LOGIC_MAF 0
 #define ADC_LOGIC_MAP 0
 #define ADC_CHANNEL_VREF 0
-#define ADC_LOGIC_COOLANT 0
+#define ADC_LOGIC_INTAKE_AIR 0
 #define ADC_LOGIC_AIR 0
 
 float getVoltage(int);
