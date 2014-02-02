0c0b0dee798533c52f99f78a9c92d24207fc0e53
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-02 22:26:36 +0000

    BOM tool

---
+++ java_tools/pcb_sync/src/com/rusefi/bom/BomComponent.java
@@ -0,0 +1,13 @@
+package com.rusefi.bom;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 2/2/14
+ */
+public class BomComponent {
+    private final String reference;
+
+    public BomComponent(String reference) {
+        this.reference = reference;
+    }
+}
