c4b3d517616656b2a47be1bb70627bc1a8d31c1d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-03 11:22:15 +0000

    testperf is back!

--- trunk/firmware/emulation/emulation.mk
+++ trunk/firmware/emulation/emulation.mk
@@ -2,6 +2,7 @@
 EMULATIONSRC = emulation/dist_emulator.c \
 	emulation/hw_layer/poten.c \
 	emulation/wave_chart.c \
+	emulation/rfi_perftest.c \
 	emulation/analog_chart.c \
 	emulation/wave_analyzer.c \
 	emulation/toothed_wheel_emulator.c \
