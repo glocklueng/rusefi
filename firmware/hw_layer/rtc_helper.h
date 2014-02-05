6df739b46cba9da552de59b8984871880dc5f2d5
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-05 20:07:12 +0000

    RTC

---
+++ firmware/hw_layer/rtc_helper.h
@@ -0,0 +1,14 @@
+/**
+ * @file rtc_helper.h
+ * @brief Real Time Clock helper
+ *
+ * @date Feb 5, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef RTC_HELPER_H_
+#define RTC_HELPER_H_
+
+void initRtc(void);
+
+#endif /* RTC_HELPER_H_ */
