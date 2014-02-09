27022a9d08bfe207bda65ce5167380b27b05e346
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-09 17:53:24 +0000

    log viewer

--- java_console/ui/src/com/irnems/ChartRepository.java
+++ java_console/ui/src/com/irnems/ChartRepository.java
@@ -1,7 +1,12 @@
 package com.irnems;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 7/27/13
@@ -29,5 +34,60 @@ public class ChartRepository {
 
     public void addChart(String value) {
         charts.add(value);
+    }
+
+    public void clear() {
+        charts.clear();
+    }
+
+    public interface CRListener {
+        public void onDigitalChart(String chart);
+    }
+
+    public JComponent createControls(final CRListener listener) {
+
+        final AtomicInteger index = new AtomicInteger();
+
+        JPanel result = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
+        result.setBorder(BorderFactory.createLineBorder(Color.red));
+
+        final JLabel info = new JLabel();
+
+        setInfoText(index, info);
+
+        JButton prev = new JButton("<");
+        prev.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index.intValue() > 0) {
+                    index.decrementAndGet();
+                    listener.onDigitalChart(charts.get(index.get()));
+                    setInfoText(index, info);
+                }
+            }
+        });
+
+
+        JButton next = new JButton(">");
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index.intValue() < getSize() - 1) {
+                    index.incrementAndGet();
+                    listener.onDigitalChart(charts.get(index.get()));
+                    setInfoText(index, info);
+                }
+            }
+        });
+
+        result.add(prev);
+        result.add(info);
+        result.add(next);
+
+        return result;
+    }
+
+    private void setInfoText(AtomicInteger index, JLabel info) {
+        info.setText(index.get() + "/" + getSize());
     }
 }
