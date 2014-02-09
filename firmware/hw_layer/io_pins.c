b13dd7f8b250753012c817d8bca11b7a8af0cf46
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-09 04:55:54 +0000

    better pinout

--- firmware/hw_layer/io_pins.c
+++ firmware/hw_layer/io_pins.c
@@ -21,7 +21,7 @@
 static pin_output_mode_e *pinDefaultState[IO_PIN_COUNT];
 static OutputPin outputs[IO_PIN_COUNT];
 static io_pin_e leds[] = { LED_CRANKING, LED_RUNNING, LED_ERROR, LED_COMMUNICATION_1, LED_DEBUG, LED_EXT_1, LED_EXT_2,
-		LED_EXT_3, LED_CHECK_ENGINE };
+		LED_CHECK_ENGINE };
 
 static pin_output_mode_e DEFAULT_OUTPUT = OM_DEFAULT;
 
@@ -76,13 +76,13 @@ static void comBlinkingThread(void *arg) {
 		setOutputPinValue(LED_COMMUNICATION_1, 0);
 		setOutputPinValue(LED_EXT_1, 1);
 		setOutputPinValue(LED_EXT_2, 1);
-		setOutputPinValue(LED_EXT_3, 1);
+//		setOutputPinValue(LED_EXT_3, 1);
 		chThdSleepMilliseconds(delay);
 
 		setOutputPinValue(LED_COMMUNICATION_1, 1);
 		setOutputPinValue(LED_EXT_1, 0);
 		setOutputPinValue(LED_EXT_2, 0);
-		setOutputPinValue(LED_EXT_3, 0);
+//		setOutputPinValue(LED_EXT_3, 0);
 		chThdSleepMilliseconds(delay);
 	}
 }
@@ -163,7 +163,7 @@ void initOutputPins(void) {
 
 	outputPinRegister("ext led 1", LED_EXT_1, EXTRA_LED_1_PORT, EXTRA_LED_1_PIN);
 	outputPinRegister("ext led 2", LED_EXT_2, EXTRA_LED_2_PORT, EXTRA_LED_2_PIN);
-	outputPinRegister("ext led 3", LED_EXT_3, EXTRA_LED_2_PORT, EXTRA_LED_3_PIN);
+//	outputPinRegister("ext led 3", LED_EXT_3, EXTRA_LED_2_PORT, EXTRA_LED_3_PIN);
 	outputPinRegister("alive1", LED_DEBUG, GPIOD, 6);
 
 	outputPinRegister("MalfunctionIndicator", LED_CHECK_ENGINE, LED_CHECK_ENGINE_PORT, LED_CHECK_ENGINE_PIN);
