bc8359767abe41fd3a802ff6ca53286b941867d3
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-12 17:10:21 +0000

    refactoring & inline 6 emulator

--- firmware/config/engines/honda_accord.c
+++ firmware/config/engines/honda_accord.c
@@ -26,11 +26,6 @@ void setHondaAccordConfiguration2(engine_configuration2_s *engineConfiguration2)
 	registerActuatorEvent(&config->ignitionEvents, 0, addOutputSignal(SPARKOUT_1_OUTPUT), x + 360);
 	registerActuatorEvent(&config->ignitionEvents, 0, addOutputSignal(SPARKOUT_4_OUTPUT), x + 540);
 
-
-	trigger_shape_s *s = &engineConfiguration2->triggerShape;
-	int TOTAL_TEETH_COUNT = 24;
-	int SKIPPED_TEETH_COUNT = 2;
-	engineConfiguration2->triggerShape.shaftPositionEventCount = ((TOTAL_TEETH_COUNT - SKIPPED_TEETH_COUNT) * 2);
-	skippedToothTriggerShape(s, TOTAL_TEETH_COUNT, SKIPPED_TEETH_COUNT);
+	skippedToothTriggerShapeExt(engineConfiguration2, 24, 2);
 }
 
--- firmware/config/engines/nissan_primera.c
+++ firmware/config/engines/nissan_primera.c
@@ -16,11 +16,7 @@
 #include "engine_configuration.h"
 
 void setNissanPrimeraengine_configuration2_s(engine_configuration2_s *engineConfiguration2) {
-	trigger_shape_s *s = &engineConfiguration2->triggerShape;
-	int TOTAL_TEETH_COUNT = 60;
-	int SKIPPED_TEETH_COUNT = 2;
-	engineConfiguration2->triggerShape.shaftPositionEventCount = ((TOTAL_TEETH_COUNT - SKIPPED_TEETH_COUNT) * 2);
-	skippedToothTriggerShape(s, TOTAL_TEETH_COUNT, SKIPPED_TEETH_COUNT);
+	skippedToothTriggerShapeExt(engineConfiguration2, 60, 2);
 }
 
 #endif /* EFI_SUPPORT_NISSAN_PRIMERA */
