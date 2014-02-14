1304658acc2ad6833b5150300d8fc149eed66ce3
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-14 02:05:13 +0000

    improving tests

--- win32_algo_tests/global.h
+++ win32_algo_tests/global.h
@@ -11,4 +11,9 @@
 #include <time.h>
 typedef float myfloat;
 
+#define EFI_SUPPORT_FORD_ASPIRE TRUE
+#define EFI_SUPPORT_DODGE_NEON TREU
+#define EFI_SUPPORT_1995_FORD_INLINE_6 TRUE
+
+
 #endif /* GLOBAL_H_ */
