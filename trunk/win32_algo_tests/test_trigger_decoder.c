e9f32b2546c22017e9f226a97c326e5533119b9a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-14 02:05:13 +0000

    improving tests

--- trunk/win32_algo_tests/test_trigger_decoder.c
+++ trunk/win32_algo_tests/test_trigger_decoder.c
@@ -9,46 +9,52 @@
 #include "test_trigger_decoder.h"
 #include "trigger_decoder.h"
 
-static void testFordAspireDecoder(void) {
-	printf("*************************************************** testFordAspireDecoder\r\n");
+#include "ford_aspire.h"
+#include "dodge_neon.h"
+
+static void testDodgeNeonDecoder(void) {
+	printf("*************************************************** testDodgeNeonDecoder\r\n");
 	initTriggerDecoder();
 
-	trigger_state_s state;
-	trigger_shape_s shape;
 
-	shape.useRiseEdge = FALSE;
-	shape.syncRatioFrom = 0.72 * 0.8;
-	shape.syncRatioTo = 0.72 * 1.3;
+	engine_configuration2_s ec2;
+	setFordAspireengine_configuration2_s(&ec2);
+
+	trigger_shape_s * shape = &ec2.triggerShape;
+	trigger_state_s state;
+
+	shape->syncRatioFrom = 0.72 * 0.8;
+	shape->syncRatioTo = 0.72 * 1.3;
 
 	int r = 0;
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 60);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 210);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 420);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 630);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 60);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 210);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 420);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 630);
 
 	printf("2nd\r\n");
 	r = 720;
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 60);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 210);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 60);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 210);
 	assertTrue(state.shaft_is_synchronized);
 	assertEquals(0, state.current_index);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 420);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 420);
 	assertEquals(1, state.current_index);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 630);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 630);
 	assertEquals(2, state.current_index);
 
 	printf("3rd\r\n");
 	r = 2 * 720;
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 60);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 60);
 	assertEquals(3, state.current_index);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 210);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 210);
 	assertTrue(state.shaft_is_synchronized);
 	assertEquals(0, state.current_index);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_UP, r + 420);
-	processTriggerEvent(&state, &shape, SHAFT_PRIMARY_DOWN, r + 630);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_UP, r + 420);
+	processTriggerEvent(&state, shape, SHAFT_PRIMARY_DOWN, r + 630);
 }
 
-static int test1995FordInline6TriggerDecoder(void) {
+static void test1995FordInline6TriggerDecoder(void) {
 	printf("*************************************************** test1995FordInline6TriggerDecoder\r\n");
 
 }
@@ -56,5 +62,6 @@ static int test1995FordInline6TriggerDecoder(void) {
 
 void testTriggerDecoder(void) {
 	printf("*************************************************** testTriggerDecoder\r\n");
-	testFordAspireDecoder();
+	testDodgeNeonDecoder();
+	test1995FordInline6TriggerDecoder();
 }
