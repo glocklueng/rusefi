aa87075a46f38393cc1784d8b7a99175031d2cad
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-17 20:52:58 +0000

    GPS stuff

--- trunk/firmware/controllers/algo/nmea.c
+++ trunk/firmware/controllers/algo/nmea.c
@@ -1,31 +1,60 @@
+/*
+ * @date Dec 20, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ * @author Kot_dnz 2014
+ */
 #include <string.h>
 #include <math.h>
+#include <time.h>
+#include "main.h"
 #include "nmea.h"
 #include "rficonsole_logic.h"
 
-double gps_deg_dec(double deg_point) {
+long hex2int(char *a, int len) {
+	int i;
+	long val = 0;
+
+	for (i = 0; i < len; i++)
+		if (a[i] <= 57)
+			val += (a[i] - 48) * (1 << (4 * (len - 1 - i))); // it's number
+		else
+			val += (a[i] - 87) * (1 << (4 * (len - 1 - i))); // it's a-f -> work only with low case hex
+	return val;
+}
+
+int str2int(char *a, int len) {
+	 int i = 0,  k = 0;
+	while (i<len) {
+		k = (k<<3)+(k<<1)+(*a)-'0';
+		a++;
+		i++;
+	}
+	return k;
+}
+
+float gps_deg_dec(float deg_point) {
 	double ddeg;
 	double sec = modf(deg_point, &ddeg) * 60;
 	int deg = (int) (ddeg / 100);
 	int min = (int) (deg_point - (deg * 100));
 
-	double absdlat = round(deg * 1000000.);
-	double absmlat = round(min * 1000000.);
-	double absslat = round(sec * 1000000.);
+	float absdlat = round(deg * 1000000.);
+	float absmlat = round(min * 1000000.);
+	float absslat = round(sec * 1000000.);
 
 	return round(absdlat + (absmlat / 60) + (absslat / 3600)) / 1000000;
 }
 
 // Convert lat e lon to decimals (from deg)
-void gps_convert_deg_to_dec(double *latitude, char ns, double *longitude, char we) {
-	double lat = (ns == 'N') ? *latitude : -1 * (*latitude);
-	double lon = (we == 'E') ? *longitude : -1 * (*longitude);
+void gps_convert_deg_to_dec(float *latitude, char ns, float *longitude, char we) {
+	float lat = (ns == 'N') ? *latitude : -1 * (*latitude);
+	float lon = (we == 'E') ? *longitude : -1 * (*longitude);
 
 	*latitude = gps_deg_dec(lat);
 	*longitude = gps_deg_dec(lon);
 }
 
-// in string collect all char till comma and convert to double
+// in string collect all char till comma and convert to float
 int str_till_comma(char *a, char *dStr) {
 
 	int i = 0, sLen = strlen(a);
@@ -40,17 +69,37 @@ int str_till_comma(char *a, char *dStr) {
 	return i;
 }
 
-void nmea_parse_gpgga(char *nmea, gpgga_t *loc) {
+/*
+GxGGA - name code
+Parameter	Value	Unit	Description
+UTC					hhmmss.sss	Universal time coordinated
+Lat					ddmm.mmmm	Latitude
+Northing Indicator			N=North, S=South
+Lon					dddmm.mmmm	Longitude
+Easting Indicator			E=East, W=West
+Status				0			0=Invalid, 1=2D/3D, 2=DGPS, 6=Dead Reckoning
+SVs Used			00			Number of SVs used for Navigation
+HDOP				99.99		Horizontal Dilution of Precision
+Alt (MSL)			m	Altitude (above means sea level)
+Unit				M=Meters
+Geoid Sep.			m			Geoid Separation = Alt(HAE) - Alt(MSL)
+Unit				M=Meters
+Age of DGPS Corr	s			Age of Differential Corrections
+DGPS Ref Station				ID of DGPS Reference Station
+*/
+void nmea_parse_gpgga(char *nmea, loc_t *loc) {
 	char *p = nmea;
 	char dStr[GPS_MAX_STRING];
 
-	p = strchr(p, ',') + 1; //skip time
+	p = strchr(p, ',') + 1; 				//skip time - we read date&time if Valid in GxRMC
 
-	p = strchr(p, ',') + 1;								// in p string started with searching address
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
-	loc->latitude = atof(dStr);
+	p = strchr(p, ',') + 1;					// in p string started with searching address
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
+	if(strlen(p) == 0) return;				// if no data in field - empty data - we return
+	
+	loc->latitude = atof(dStr);				// fulfil data
 
-	p = strchr(p, ',') + 1;
+	p = strchr(p, ',') + 1;					// see above
 	switch (p[0]) {
 	case 'N':
 		loc->lat = 'N';
@@ -64,7 +113,7 @@ void nmea_parse_gpgga(char *nmea, gpgga_t *loc) {
 	}
 
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->longitude = atof(dStr);
 
 	p = strchr(p, ',') + 1;
@@ -81,29 +130,63 @@ void nmea_parse_gpgga(char *nmea, gpgga_t *loc) {
 	}
 
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->quality = atoi(dStr);
 
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->satellites = atoi(dStr);
 
 	p = strchr(p, ',') + 1;
 
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->altitude = atof(dStr);
 }
 
-void nmea_parse_gprmc(char *nmea, gprmc_t *loc) {
+/*
+GxRMC - nmea code
+Parameter	Value		Unit			Description
+UTC						hhmmss.sss		Universal time coordinated
+Status		V		A=Valid, V=Invalid
+Lat			ddmm.mmmm					Latitude
+Northing Indicator			N=North, S=South
+Lon			dddmm.mmmm					Longitude
+Easting Indicator			E=East, W=West
+SOG						nots			Speed Over Ground
+COG (true)				°				Course Over Ground (true)
+Date					ddmmyy			Universal time coordinated
+Magnetic Variation		°				Magnetic Variation
+Magnetic Variation			E=East,W=West
+Mode Indicator	N		A=Autonomous, D=Differential, E=Dead Reckoning, N=None
+Navigational Status			S=Safe C=Caution U=Unsafe V=Not valid
+
+*/
+void nmea_parse_gprmc(char *nmea, loc_t *loc) {
 	char *p = nmea;
 	char dStr[GPS_MAX_STRING];
+	struct tm timp;
 
-	p = strchr(p, ',') + 1; //skip time
-	p = strchr(p, ',') + 1; //skip status
+	p = strchr(p, ',') + 1; 				//read time
+	str_till_comma(p, dStr);
+	if(strlen(dStr) > 5){
+		timp.tm_hour = str2int(dStr,2);
+		timp.tm_min  = str2int(dStr+2,2);
+		timp.tm_sec  = str2int(dStr+4,2);
+	}
+	
+	p = strchr(p, ',') + 1; 				//read field Valid status
+	str_till_comma(p, dStr);
 
-	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	if(dStr[0] == 'V') {					// if field is invalid
+		loc->quality = 0;
+		return;
+	}
+	
+	loc->quality = 4;						// this is declaration that last receive field VALID
+	
+	p = strchr(p, ',') + 1;					// latitude
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->latitude = atof(dStr);
 
 	p = strchr(p, ',') + 1;
@@ -119,8 +202,8 @@ void nmea_parse_gprmc(char *nmea, gprmc_t *loc) {
 		break;
 	}
 
-	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	p = strchr(p, ',') + 1; 				// longitude
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->longitude = atof(dStr);
 
 	p = strchr(p, ',') + 1;
@@ -137,12 +220,24 @@ void nmea_parse_gprmc(char *nmea, gprmc_t *loc) {
 	}
 
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->speed = atof(dStr);
-
+	
 	p = strchr(p, ',') + 1;
-	str_till_comma(p, dStr);				// str to double till comma saved modified string
+	str_till_comma(p, dStr);				// str to float till comma saved modified string
 	loc->course = atof(dStr);
+
+	p = strchr(p, ',') + 1; 				//read date
+	str_till_comma(p, dStr);
+	if(strlen(dStr) > 5){
+		timp.tm_mday = str2int(dStr,2);
+		timp.tm_mon  = str2int(dStr+2,2);
+		timp.tm_year = str2int(dStr+4,2)+100;	// we receive -200, but standard wait -1900 = add correction
+	}
+
+	if( timp.tm_year > 0 ) {				// check if date field is valid  
+		memcpy(&loc->GPStm, &timp, sizeof(timp));
+	}
 }
 
 /**
@@ -170,18 +265,6 @@ nmea_message_type nmea_get_message_type(const char *message) {
 	return NMEA_UNKNOWN;
 }
 
-long hex2int(char *a, int len) {
-	int i;
-	long val = 0;
-
-	for (i = 0; i < len; i++)
-		if (a[i] <= 57)
-			val += (a[i] - 48) * (1 << (4 * (len - 1 - i))); // it's number
-		else
-			val += (a[i] - 87) * (1 << (4 * (len - 1 - i))); // it's a-f -> work only with low case hex
-	return val;
-}
-
 int nmea_valid_checksum(const char *message) {
 	char p;
 	int sum = 0;
@@ -202,42 +285,18 @@ int nmea_valid_checksum(const char *message) {
 	return _EMPTY;
 }
 
-void resetLocation(loc_t *coord) {
-	coord->latitude = 0;
-	coord->longitude = 0;
-	coord->speed = 0;
-	coord->altitude = 0;
-	coord->course = 0;
-	coord->type = NMEA_UNKNOWN;
-}
-
 // Compute the GPS location using decimal scale
 void gps_location(loc_t *coord, char *buffer) {
-	gpgga_t gpgga;
-	gprmc_t gprmc;
-
-	resetLocation(coord);
-
-
 
 	coord->type = nmea_get_message_type(buffer);
 
 	switch (coord->type) {
 	case NMEA_GPGGA:
-		nmea_parse_gpgga(buffer, &gpgga);
-
-		gps_convert_deg_to_dec(&(gpgga.latitude), gpgga.lat, &(gpgga.longitude), gpgga.lon);
-
-		coord->latitude = gpgga.latitude;
-		coord->longitude = gpgga.longitude;
-		coord->altitude = gpgga.altitude;
+		nmea_parse_gpgga(buffer, coord);
+		gps_convert_deg_to_dec(&(coord->latitude), coord->lat, &(coord->longitude), coord->lon);
 		break;
 	case NMEA_GPRMC:
-		nmea_parse_gprmc(buffer, &gprmc);
-
-		coord->speed = gprmc.speed;
-		coord->course = gprmc.course;
-
+		nmea_parse_gprmc(buffer, coord);
 		break;
 	case NMEA_UNKNOWN:
 		// unknown message type
@@ -245,6 +304,3 @@ void gps_location(loc_t *coord, char *buffer) {
 	}
 
 }
-
-// 		print("GPS latitude = %f, speed = %f\r\n", GPSdata.latitude, GPSdata.speed);
-
--- trunk/firmware/controllers/algo/nmea.h
+++ trunk/firmware/controllers/algo/nmea.h
@@ -22,48 +22,25 @@ typedef enum {
 #define NMEA_CHECKSUM_ERR 0x80
 #define NMEA_MESSAGE_ERR 0xC0
 
-struct gpgga {
-	// Latitude eg: 4124.8963 (XXYY.ZZKK.. DEG, MIN, SEC.SS)
-	double latitude;
-	// Latitude eg: N
-	char lat;
-	// Longitude eg: 08151.6838 (XXXYY.ZZKK.. DEG, MIN, SEC.SS)
-	double longitude;
-	// Longitude eg: W
-	char lon;
-	// Quality 0, 1, 2
-	unsigned int quality;
-	// Number of satellites: 1,2,3,4,5...
-	unsigned int satellites;
-	// Altitude eg: 280.2 (Meters above mean sea level)
-	double altitude;
-};
-typedef struct gpgga gpgga_t;
-
-struct gprmc {
-	double latitude;
-	char lat;
-	double longitude;
-	char lon;
-	double speed;
-	double course;
-};
-typedef struct gprmc gprmc_t;
-
 struct GPSlocation {
-	double latitude;
-	double longitude;
-	double speed;
-	double altitude;
-	double course;
+	float latitude;
+	float longitude;
+	float speed;
+	float altitude;
+	float course;
+	struct tm GPStm;
 	nmea_message_type type;
+	int quality;
+	int satellites;
+	char lat;		// Northing Indicator			N=North, S=South
+	char lon;		// Easting Indicator			E=East, W=West
 };
 typedef struct GPSlocation loc_t;
 
 nmea_message_type nmea_get_message_type(const char *);
 int nmea_valid_checksum(const char *);
-void nmea_parse_gpgga(char *, gpgga_t *);
-void nmea_parse_gprmc(char *, gprmc_t *);
+void nmea_parse_gpgga(char *, loc_t *);
+void nmea_parse_gprmc(char *, loc_t *);
 void gps_location(loc_t *, char *);
 
 #endif
