d5bc021c82e3bf2eb1d72ac0ff64dcbc696d9ff6
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-24 02:48:26 +0000

    refactoring

--- trunk/firmware/console/console.mk
+++ trunk/firmware/console/console.mk
@@ -1,6 +1,4 @@
 
 CONSOLESRC = $(PROJECT_DIR)/console/eficonsole.c \
 	$(PROJECT_DIR)/console/console_io.c \
-	$(PROJECT_DIR)/console/usbcfg.c \
-	$(PROJECT_DIR)/console/usbconsole.c \
 	$(PROJECT_DIR)/console/status_loop.c
--- trunk/firmware/hw_layer/hw_layer.mk
+++ trunk/firmware/hw_layer/hw_layer.mk
@@ -1,18 +1,20 @@
 
-HW_LAYERESRC = hw_layer/hardware.c \
-	hw_layer/adc_inputs.c \
-	hw_layer/trigger_input.c \
-	hw_layer/pin_repository.c \
-	hw_layer/io_pins.c \
-	hw_layer/mcp3208.c \
-	hw_layer/lcd/lcd_2x16.c \
-	hw_layer/can_hw.c \
-	hw_layer/HIP9011.c \
-	hw_layer/flash.c \
-	hw_layer/rtc_helper.c \
-	hw_layer/mmc_card.c \
-	hw_layer/neo6m.c \
-	hw_layer/gpio_helper.c \
-	hw_layer/wave_analyzer_hw.c \
-	hw_layer/pwm_generator.c
+HW_LAYERESRC = $(PROJECT_DIR)/hw_layer/hardware.c \
+	$(PROJECT_DIR)/hw_layer/adc_inputs.c \
+	$(PROJECT_DIR)/hw_layer/trigger_input.c \
+	$(PROJECT_DIR)/hw_layer/pin_repository.c \
+	$(PROJECT_DIR)/hw_layer/io_pins.c \
+	$(PROJECT_DIR)/hw_layer/mcp3208.c \
+	$(PROJECT_DIR)/hw_layer/lcd/lcd_2x16.c \
+	$(PROJECT_DIR)/hw_layer/can_hw.c \
+	$(PROJECT_DIR)/hw_layer/HIP9011.c \
+	$(PROJECT_DIR)/hw_layer/serial_over_usb/usbcfg.c \
+	$(PROJECT_DIR)/hw_layer/serial_over_usb/usbconsole.c \
+	$(PROJECT_DIR)/hw_layer/flash.c \
+	$(PROJECT_DIR)/hw_layer/rtc_helper.c \
+	$(PROJECT_DIR)/hw_layer/mmc_card.c \
+	$(PROJECT_DIR)/hw_layer/neo6m.c \
+	$(PROJECT_DIR)/hw_layer/gpio_helper.c \
+	$(PROJECT_DIR)/hw_layer/wave_analyzer_hw.c \
+	$(PROJECT_DIR)/hw_layer/pwm_generator.c
 
