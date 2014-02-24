84ae3de4c67edb3405878c559ab6f85f3127186e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-02-24 03:47:35 +0000

    rus084

--- firmware/config/engines/GY6_139QMB.c
+++ firmware/config/engines/GY6_139QMB.c
@@ -13,9 +13,10 @@ void setGy6139qmbDefaultEngineConfiguration(engine_configuration_s *engineConfig
 	engineConfiguration->rpmHardLimit = 9000;
 	engineConfiguration->crankingSettings.crankingRpm = 800;
 	engineConfiguration->analogInputDividerCoefficient = 1.52;
-	engineConfiguration->engineLoadMode = LM_MAP;
-	engineConfiguration->cylindersCount = 1;
-	engineConfiguration->rpmMultiplier = 1;
+ 	engineConfiguration->engineLoadMode = LM_MAP;
+	engineConfiguration->globalTriggerOffsetAngle = 15;
+	engineConfiguration->analogChartMode = AC_MAP;
+ 	engineConfiguration->cylindersCount = 1;	engineConfiguration->rpmMultiplier = 1;
 
 	engineConfiguration->firingOrder = FO_ONE_CYLINDER;
 
