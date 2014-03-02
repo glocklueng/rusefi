4e5c282e14f4d749a4ea5b26d6f7ebc678ab12ee
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-02 16:04:52 +0000

    isolating actions from scheduling

---
+++ firmware/controllers/system/pwm_generator_logic.h
@@ -0,0 +1,61 @@
+/*
+ * @file    pwm_generator_logic.h
+ *
+ * @date Mar 2, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef PWM_GENERATOR_LOGIC_H_
+#define PWM_GENERATOR_LOGIC_H_
+
+#include "global.h"
+#include "trigger_structure.h"
+#include "io_pins.h"
+#include "signal_executor.h"
+
+typedef struct {
+	/**
+	 * a copy so that all phases are executed on the same period, even if another thread
+	 * would be adjusting PWM parameters
+	 */
+	float period;
+	/**
+	 * Iteration counter
+	 */
+	int iteration;
+	/**
+	 * Start time of current iteration
+	 */
+	systime_t start;
+	int phaseIndex;
+} pwm_config_safe_state_s;
+
+typedef struct PwmConfig_struct PwmConfig;
+
+typedef void (pwm_gen_callback)(PwmConfig *state, int stateIndex);
+
+/**
+ * @brief   Multi-channel software PWM output configuration
+ */
+struct PwmConfig_struct {
+	io_pin_e outputPins[PWM_PHASE_MAX_WAVE_PER_PWM];
+	multi_wave_s multiWave;
+	char *name;
+	/**
+	 * float value of PWM period
+	 * PWM generation is not happening while this value is zero
+	 */
+	float period;
+
+	WORKING_AREA(deThreadStack, UTILITY_THREAD_STACK_SIZE);
+	scheduling_s scheduling;
+
+	pwm_config_safe_state_s safe;
+
+	pwm_gen_callback *changeStateCallback;
+};
+
+void weComplexInit(char *msg, PwmConfig *state,
+		int phaseCount, float *swithcTimes, int waveCount, int **pinStates, pwm_gen_callback *callback);
+
+#endif /* PWM_GENERATOR_LOGIC_H_ */
