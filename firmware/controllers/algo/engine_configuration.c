bf4c9e8c1d3fafbb7e089247f4ef2a75b2b128d5
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-02 03:16:14 +0000

    softer error handling

--- firmware/controllers/algo/engine_configuration.c
+++ firmware/controllers/algo/engine_configuration.c
@@ -153,6 +153,8 @@ void setDefaultConfiguration(engine_configuration_s *engineConfiguration) {
 	engineConfiguration->injectionPins[1] = GPIOB_8;
 	engineConfiguration->injectionPins[2] = GPIOE_3;
 	engineConfiguration->injectionPins[3] = GPIOE_5;
+	engineConfiguration->injectionPins[4] = GPIOE_6;
+//	engineConfiguration->injectionPins[5] = GPIOE_5;
 }
 
 void setDefaultNonPersistentConfiguration(engine_configuration2_s *engineConfiguration2) {
