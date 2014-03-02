0b86c5365ed55d73ac94bc7fd1441bb03e9e4172
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-02 16:08:29 +0000

    fixing makefile

--- firmware/controllers/system/system.mk
+++ firmware/controllers/system/system.mk
@@ -1,5 +1,6 @@
 
 SYSTEMSRC = \
+	$(PROJECT_DIR)/controllers/system/pwm_generator_logic.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_sleep.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_single_timer.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_hw_timer.c
