64b75adbc9b7ecdbee2d1e126b8695d39163cf40
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-02 21:07:39 +0000

    virtual stream

--- win32_functional_tests/global.h
+++ win32_functional_tests/global.h
@@ -4,6 +4,10 @@
 #include <time.h>
 #include <math.h>
 
+#ifndef GLOBAL_FT_H_
+#define GLOBAL_FT_H_
+
+
 #define EFI_SUPPORT_FORD_ASPIRE TRUE
 #define EFI_SIGNAL_EXECUTOR_SLEEP TRUE
 
@@ -15,6 +19,19 @@
 // project-wide default thread stack size
 #define UTILITY_THREAD_STACK_SIZE 1384
 
+/**
+ * @brief   @p Win32TestStream virtual methods table.
+ */
+struct Win32TestStreamVMT {
+  _base_sequential_stream_methods
+};
+
+typedef struct {
+  const struct Win32TestStreamVMT *vmt;
+} TestStream;
+
+extern TestStream testStream;
 
 //##define TRUE 1
 //#define FALSE 0
+#endif /* GLOBAL_FT_H_ */
