9920b1df034da66d31c351e4fd118543dca19571
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-03 22:53:08 +0000

    new file

--- firmware/controllers/trigger/trigger.mk
+++ firmware/controllers/trigger/trigger.mk
@@ -1,4 +1,5 @@
 
 TRIGGERSRC = \
 	$(PROJECT_DIR)/controllers/trigger/trigger_central.c \
+	$(PROJECT_DIR)/controllers/trigger/trigger_emulator_algo.c \
 	$(PROJECT_DIR)/controllers/trigger/rpm_calculator.c
---
+++ firmware/controllers/trigger/trigger_emulator_algo.h
@@ -0,0 +1,13 @@
+/*
+ * @file trigger_emulator_algo.h
+ *
+ * @date Mar 3, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef TRIGGER_EMULATOR_ALGO_H_
+#define TRIGGER_EMULATOR_ALGO_H_
+
+
+
+#endif /* TRIGGER_EMULATOR_ALGO_H_ */
