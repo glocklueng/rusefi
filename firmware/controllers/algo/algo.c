a62a240c4e253b730d4bfe4a79696f5a5dc86397
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-03 00:27:12 +0000

    new files

---
+++ firmware/controllers/algo/algo.c
@@ -0,0 +1,13 @@
+/*
+ * @file	algo.c
+ *
+ * @date Mar 2, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "global.h"
+#include "algo.h"
+
+void initAlgo(void) {
+
+}
--- firmware/controllers/algo/algo.mk
+++ firmware/controllers/algo/algo.mk
@@ -12,5 +12,6 @@ CONTROLLERS_ALGO_SRC = $(PROJECT_DIR)/controllers/algo/map_adjuster.c \
 	$(PROJECT_DIR)/controllers/algo/wave_chart.c \
 	$(PROJECT_DIR)/controllers/algo/trigger_structure.c \
 	$(PROJECT_DIR)/controllers/algo/nmea.c \
+	$(PROJECT_DIR)/controllers/algo/algo.c \
 	$(PROJECT_DIR)/controllers/algo/trigger_decoder.c \
 	$(PROJECT_DIR)/controllers/algo/signal_executor_single_timer_algo.c
