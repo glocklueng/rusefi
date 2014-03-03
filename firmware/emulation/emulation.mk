b9b07caf71af035502ed58cf89ca605023f08eef
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-03 00:09:01 +0000

    refactoring

--- firmware/emulation/emulation.mk
+++ firmware/emulation/emulation.mk
@@ -1,10 +1,8 @@
 
 EMULATIONSRC = emulation/trigger_emulator.c \
 	emulation/hw_layer/poten.c \
-	emulation/wave_chart.c \
 	emulation/rfi_perftest.c \
 	emulation/analog_chart.c \
 	emulation/wave_analyzer.c \
-	emulation/toothed_wheel_emulator.c \
 	emulation/engine_emulator.c 
 	
