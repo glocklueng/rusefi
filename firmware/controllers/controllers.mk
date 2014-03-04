c57f01de3a39ffa2addb1a06a3de248463410945
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-04 18:04:15 +0000

    main logic into simulator

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -7,7 +7,6 @@ CONTROLLERSSRC = \
 	controllers/ignition_central.c \
 	controllers/settings.c \
 	controllers/flash_main.c \
-	controllers/main_trigger_callback.c \
 	$(PROJECT_DIR)/controllers/malfunction_indicator.c \
 	controllers/map_averaging.c \
 	controllers/map_multiplier_thread.c
--- firmware/controllers/trigger/trigger.mk
+++ firmware/controllers/trigger/trigger.mk
@@ -1,4 +1,5 @@
 
 TRIGGERSRC = \
 	$(PROJECT_DIR)/controllers/trigger/trigger_central.c \
+	$(PROJECT_DIR)/controllers/trigger/main_trigger_callback.c \
 	$(PROJECT_DIR)/controllers/trigger/rpm_calculator.c
