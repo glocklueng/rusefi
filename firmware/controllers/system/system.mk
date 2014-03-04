8c43c633a8ff07636270deb2ae0703daf7e5e380
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-04 17:26:29 +0000

    folder structure

--- firmware/controllers/system/system.mk
+++ firmware/controllers/system/system.mk
@@ -3,4 +3,5 @@ SYSTEMSRC = \
 	$(PROJECT_DIR)/controllers/system/pwm_generator_logic.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_sleep.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_single_timer.c \
+	$(PROJECT_DIR)/controllers/system/trigger_emulator_algo.c \
 	$(PROJECT_DIR)/controllers/system/signal_executor_hw_timer.c
--- firmware/controllers/trigger/trigger.mk
+++ firmware/controllers/trigger/trigger.mk
@@ -1,5 +1,4 @@
 
 TRIGGERSRC = \
 	$(PROJECT_DIR)/controllers/trigger/trigger_central.c \
-	$(PROJECT_DIR)/controllers/trigger/trigger_emulator_algo.c \
 	$(PROJECT_DIR)/controllers/trigger/rpm_calculator.c
