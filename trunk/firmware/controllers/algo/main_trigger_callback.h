87acb77f0551f0e0c53c5a48fa9bfba2b1e5d05c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-06 03:38:38 +0000

    C++ is getting closer

--- trunk/firmware/controllers/algo/main_trigger_callback.h
+++ trunk/firmware/controllers/algo/main_trigger_callback.h
@@ -11,16 +11,39 @@
 #define MAIN_LOOP_H_
 
 #include "main.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+
 #include "event_registry.h"
+
+#else
+
+#include "event_registry.h"
+
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #define MAX_INJECTOR_COUNT 12
 #define MAX_IGNITER_COUNT 4
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 void initMainEventListener(void);
 void showMainHistogram(void);
 void onEveryMillisecondTimerSignal(void);
 int isIgnitionTimingError(void);
 
 float getFuel(int rpm, float key);
-
+#ifdef __cplusplus
+}
+#endif
 #endif /* MAIN_LOOP_H_ */
