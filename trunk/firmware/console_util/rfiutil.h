80d8e03d5d6d690939983001c093fb929d5327e1
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-08 04:24:08 +0000

    not really

--- trunk/firmware/console_util/rfiutil.h
+++ trunk/firmware/console_util/rfiutil.h
@@ -20,7 +20,7 @@
 
 #define _MAX_FILLER 11
 
-char* itoa(const uint8_t *p, int num);
+char* itoa(uint8_t *p, int num);
 char hexC(int v);
 int isIsrContext(void);
 void chVTSetAny(VirtualTimer *vtp, systime_t time, vtfunc_t vtfunc, void *par);
