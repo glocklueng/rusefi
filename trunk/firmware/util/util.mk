271876d4a9acbf3be3eebbb2c9ad136b2e172e21
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-08 06:27:10 +0000

    total C++

--- trunk/firmware/util/util.mk
+++ trunk/firmware/util/util.mk
@@ -4,6 +4,7 @@ UTILSRC = $(PROJECT_DIR)/util/data_buffer.c \
 	$(PROJECT_DIR)/util/crc.c \
 	$(PROJECT_DIR)/util/efilib.c \
 	$(PROJECT_DIR)/util/histogram.c \
-	$(PROJECT_DIR)/util/cyclic_buffer.c \
 	$(PROJECT_DIR)/util/eficonsole_logic.c \
 	$(PROJECT_DIR)/util/wave_math.c
+	
+UTILSRC_CPP = $(PROJECT_DIR)/util/cyclic_buffer.cpp
