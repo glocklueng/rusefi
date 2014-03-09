95bfd3d699c0caacc2e79edfc239b9c9a81af9a3
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-09 03:22:20 +0000

    C++

--- trunk/firmware/controllers/system/error_handling.h
+++ trunk/firmware/controllers/system/error_handling.h
@@ -8,11 +8,21 @@
 #ifndef ERROR_HANDLING_H_
 #define ERROR_HANDLING_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
 int warning(const char *fmt, ...);
 void firmwareError(const char *fmt, ...);
 
 int hasFatalError(void);
 void fatal3(char *msg, char *file, int line);
 #define fatal(x) (fatal3(x, __FILE__, __LINE__));
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 
 #endif /* ERROR_HANDLING_H_ */
