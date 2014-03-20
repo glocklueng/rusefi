0c8fa95dae4c5ef95efed87e7d6179f3094e91c1
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-20 15:22:59 +0000

    clean-up

--- trunk/win32_functional_tests/main.c
+++ trunk/win32_functional_tests/main.c
@@ -33,38 +33,6 @@ void printToWin32Console(char *p) {
 
 TestStream testStream;
 
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-	size_t n, size;
-
-	(void) argv;
-	if (argc > 0) {
-		chprintf(chp, "Usage: mem\r\n");
-		return;
-	}
-	n = chHeapStatus(NULL, &size);
-	chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
-	chprintf(chp, "heap fragments   : %u\r\n", n);
-	chprintf(chp, "heap free total  : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-	static const char *states[] = { THD_STATE_NAMES };
-	Thread *tp;
-
-	(void) argv;
-	if (argc > 0) {
-		chprintf(chp, "Usage: threads\r\n");
-		return;
-	}
-	chprintf(chp, "    addr    stack prio refs     state time\r\n");
-	tp = chRegFirstThread();
-	do {
-		chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n", (uint32_t) tp, (uint32_t) tp->p_ctx.esp,
-				(uint32_t) tp->p_prio, (uint32_t) (tp->p_refs - 1), states[tp->p_state], (uint32_t) tp->p_time);
-		tp = chRegNextThread(tp);
-	} while (tp != NULL);
-}
-
 /*
  * Console print server done using synchronous messages. This makes the access
  * to the C printf() thread safe and the print operation atomic among threads.
