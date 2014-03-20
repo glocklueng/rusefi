0454430616503a0df6669f279f1938a543ba9e64
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-20 19:19:17 +0000

    C++

--- trunk/firmware/config/engines/engines.mk
+++ trunk/firmware/config/engines/engines.mk
@@ -1,7 +1,6 @@
 
 ENGINES_SRC = $(PROJECT_DIR)/config/engines/audi_aan.c \
 	$(PROJECT_DIR)/config/engines/dodge_neon.c \
-	$(PROJECT_DIR)/config/engines/ford_aspire.c \
 	$(PROJECT_DIR)/config/engines/ford_fiesta.c \
 	$(PROJECT_DIR)/config/engines/ford_1995_inline_6.c \
 	$(PROJECT_DIR)/config/engines/nissan_primera.c \
@@ -10,3 +9,5 @@ ENGINES_SRC = $(PROJECT_DIR)/config/engines/audi_aan.c \
 	$(PROJECT_DIR)/config/engines/snow_blower.c \
 	$(PROJECT_DIR)/config/engines/GY6_139QMB.c \
 	$(PROJECT_DIR)/config/engines/mazda_323.c
+
+ENGINES_SRC_CPP = $(PROJECT_DIR)/config/engines/ford_aspire.cpp 
