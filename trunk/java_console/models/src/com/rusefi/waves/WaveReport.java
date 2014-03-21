b11058f9e2f402d1b44ce4b72240f61d23e472be
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-21 12:59:42 +0000

    fixing & improving tests

--- trunk/java_console/models/src/com/rusefi/waves/WaveReport.java
+++ trunk/java_console/models/src/com/rusefi/waves/WaveReport.java
@@ -159,7 +159,7 @@ public class WaveReport implements TimeAxisTranslator {
         }
 
         public double getDutyCycle(RevolutionLog rl) {
-            double angleDuration = (rl.getCrankAngleByTime(downTime) + 360 - rl.getCrankAngleByTime(upTime)) % 360;
+            double angleDuration = (rl.getCrankAngleByTime(downTime) + 720 - rl.getCrankAngleByTime(upTime)) % 360;
             if (angleDuration < 0)
                 throw new IllegalStateException("Negative width: " + angleDuration);
             return angleDuration / 360;
