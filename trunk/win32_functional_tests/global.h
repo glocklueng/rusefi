88dbec75e1a5231e2a198350fba693ec8683ce2b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-25 02:41:25 +0000

    refactoring

--- trunk/win32_functional_tests/global.h
+++ trunk/win32_functional_tests/global.h
@@ -43,3 +43,27 @@ extern TestStream testStream;
 //##define TRUE 1
 //#define FALSE 0
 #endif /* GLOBAL_FT_H_ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+void printToWin32Console(char *p);
+int systicks2ms(int systicks);
+
+// todo: move somewhere else?
+void lockAnyContext(void);
+void unlockAnyContext(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+/**
+ * number of SysClock ticks in one ms
+ */
+#define TICKS_IN_MS  (CH_FREQUENCY / 1000)
+
+#define hal_lld_get_counter_value() 0
