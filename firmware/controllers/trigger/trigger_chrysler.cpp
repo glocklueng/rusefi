0f61c71026103ab3e2a99b5131666d2fc721a79d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-25 00:40:43 +0000

    refactoring

--- firmware/controllers/trigger/trigger_chrysler.cpp
+++ firmware/controllers/trigger/trigger_chrysler.cpp
@@ -10,12 +10,13 @@
 void configureNeonTriggerShape(trigger_shape_s *s) {
 	triggerShapeInit(s);
 
-	triggerAddEvent(s, 60, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, 210, T_PRIMARY, TV_LOW);
-	triggerAddEvent(s, 420, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, 630, T_PRIMARY, TV_LOW);
 	// voodoo magic - we always need 720 at the end
-	triggerAddEvent(s, 720, T_PRIMARY, TV_LOW);
+	int base = 720 - 630;
+
+	triggerAddEvent(s, base + 60 , T_PRIMARY, TV_HIGH);
+	triggerAddEvent(s, base + 210, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, base + 420, T_PRIMARY, TV_HIGH);
+	triggerAddEvent(s, base + 630, T_PRIMARY, TV_LOW);
 
 	s->shaftPositionEventCount = 4;
 }
