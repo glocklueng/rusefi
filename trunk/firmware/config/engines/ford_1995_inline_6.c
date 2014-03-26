a600a6db67cfafdcd5b60fcd836daf9d85304b00
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-26 13:47:55 +0000

    making progress

--- trunk/firmware/config/engines/ford_1995_inline_6.c
+++ trunk/firmware/config/engines/ford_1995_inline_6.c
@@ -74,10 +74,6 @@ void setFordInline6(engine_configuration_s *engineConfiguration) {
  * @brief These settings are not persistent yet
  */
 void setFordInline6_2(engine_configuration_s *engineConfiguration, engine_configuration2_s *engineConfiguration2) {
-	EventHandlerConfiguration *config = &engineConfiguration2->engineEventConfiguration;
-
-	addFuelEvents(engineConfiguration, engineConfiguration2, &config->crankingInjectionEvents, engineConfiguration->crankingInjectionMode);
-	addFuelEvents(engineConfiguration, engineConfiguration2, &config->injectionEvents, engineConfiguration->injectionMode);
 }
 
 #endif /* EFI_SUPPORT_1995_FORD_INLINE_6 */
