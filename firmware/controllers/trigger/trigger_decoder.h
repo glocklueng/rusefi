d104df8d01f84807785ef4386dafc724fa33b69d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-26 17:30:37 +0000

    making progress

--- firmware/controllers/trigger/trigger_decoder.h
+++ firmware/controllers/trigger/trigger_decoder.h
@@ -22,7 +22,10 @@ int isTriggerDecoderError(void);
 void processTriggerEvent(trigger_state_s *shaftPositionState, trigger_shape_s const*triggerShape, trigger_config_s const*triggerConfig, ShaftEvents signal, time_t now);
 void initializeSkippedToothTriggerShapeExt(engine_configuration2_s *engineConfiguration2, int totalTeethCount, int skippedCount);
 void initializeTriggerShape(engine_configuration_s *engineConfiguration, engine_configuration2_s *engineConfiguration2);
+int findTriggerZeroEventIndex(trigger_shape_s const * shape, trigger_config_s const*triggerConfig);
+
 void initTriggerDecoder(void);
+
 #ifdef __cplusplus
 }
 #endif
