02fb54147bcce6b1cdc4f3b3058a9c80d1813cb1
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-26 16:33:33 +0000

    C++

--- firmware/controllers/algo/fuel_math.h
+++ firmware/controllers/algo/fuel_math.h
@@ -8,6 +8,11 @@
 #ifndef FUEL_MAP_H_
 #define FUEL_MAP_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
 void prepareFuelMap(void);
 
 float getBaseFuel(int rpm, float engineLoad);
@@ -17,5 +22,9 @@ float getCltCorrection(float clt);
 float getRunningFuel(int rpm, float engineLoad);
 float getStartingFuel(float coolantTemperature);
 float getFuelMs(int rpm);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* FUEL_MAP_H_ */
