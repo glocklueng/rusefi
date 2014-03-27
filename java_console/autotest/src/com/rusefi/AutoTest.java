54aad2722cda5afa834590c15a626dcfd289cf52
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-27 22:24:14 +0000

    refactoring

--- java_console/autotest/src/com/rusefi/AutoTest.java
+++ java_console/autotest/src/com/rusefi/AutoTest.java
@@ -2,21 +2,10 @@ package com.rusefi;
 
 
 import com.irnems.FileLog;
-import com.irnems.core.Sensor;
-import com.irnems.core.SensorCentral;
-import com.rusefi.io.LinkManager;
-import com.rusefi.io.tcp.TcpConnector;
-import com.rusefi.waves.RevolutionLog;
 import com.rusefi.waves.WaveChart;
-import com.rusefi.waves.WaveReport;
 
-import java.util.List;
-import java.util.concurrent.*;
-
-import static com.rusefi.IoUtil.*;
-import static com.rusefi.TestingUtils.assertCloseEnough;
-import static com.rusefi.TestingUtils.assertTrue;
-import static com.rusefi.waves.WaveReport.isCloseEnough;
+import static com.rusefi.IoUtil.nextChart;
+import static com.rusefi.IoUtil.sendCommand;
 
 /**
  * rusEfi firmware simulator functional test suite
@@ -27,74 +16,6 @@ import static com.rusefi.waves.WaveReport.isCloseEnough;
  *         3/5/14
  */
 public class AutoTest {
-    public static void main(String[] args) throws InterruptedException {
-        FileLog.SIMULATOR_CONSOLE.start();
-        FileLog.MAIN.start();
-
-        boolean failed = false;
-        try {
-            runTest();
-        } catch (Throwable e) {
-            e.printStackTrace();
-            failed = true;
-        } finally {
-            ExecHelper.destroy();
-        }
-        if (failed)
-            System.exit(-1);
-        FileLog.MAIN.logLine("*******************************************************************************");
-        FileLog.MAIN.logLine("************************************  Looks good! *****************************");
-        FileLog.MAIN.logLine("*******************************************************************************");
-        System.exit(0);
-    }
-
-    private static void runTest() throws InterruptedException {
-        if (!TcpConnector.getAvailablePorts().isEmpty())
-            throw new IllegalStateException("Port already binded on startup?");
-
-        ExecHelper.startSimulator();
-
-
-//        FileLog.rlog("Waiting for TCP port...");
-//        for (int i = 0; i < 180; i++) {
-//            if (!TcpConnector.getAvailablePorts().isEmpty())
-//                break;
-//            Thread.sleep(1000);
-//        }
-//        if (TcpConnector.getAvailablePorts().isEmpty())
-//            throw new IllegalStateException("Did we start it?");
-//        /**
-//         * If we open a connection just to validate that the process has started, we are getting
-//         * weird issues with the second - actual connection
-//         */
-//        FileLog.rlog("Time for simulator to close the port...");
-//        Thread.sleep(3000);
-//
-//        FileLog.rlog("Got a TCP port! Connecting...");
-        LinkManager.start("" + TcpConnector.DEFAULT_PORT);
-        /**
-         * TCP connector is blocking
-         */
-        LinkManager.open();
-
-        FileLog.MAIN.logLine("Let's give it some time to start...");
-
-        final CountDownLatch startup = new CountDownLatch(1);
-        SensorCentral.AdcListener listener = new SensorCentral.AdcListener() {
-            @Override
-            public void onAdcUpdate(SensorCentral model, double value) {
-                startup.countDown();
-            }
-        };
-        long waitStart = System.currentTimeMillis();
-        SensorCentral.getInstance().addListener(Sensor.RPM, listener);
-        startup.await(5, TimeUnit.SECONDS);
-        SensorCentral.getInstance().removeListener(Sensor.RPM, listener);
-        FileLog.MAIN.logLine("Got first signal in " + (System.currentTimeMillis() - waitStart));
-
-        mainTestBody();
-    }
-
     private static void mainTestBody() {
         testFordAspire();
 
@@ -110,20 +31,20 @@ public class AutoTest {
 
     private static void testDodgeNeon() {
         WaveChart chart;
-        changeRpm(2000);
+        IoUtil.changeRpm(2000);
         chart = nextChart();
 
         String msg = "Neon";
 
-        assertWave(msg, chart, WaveChart.INJECTOR_4, 0.1, 51);
-        assertWave(msg, chart, WaveChart.INJECTOR_2, 0.1, 231);
-        assertWave(msg, chart, WaveChart.INJECTOR_1, 0.1, 411);
-        assertWave(msg, chart, WaveChart.INJECTOR_3, 0.1, 591);
+        TestingUtils.assertWave(msg, chart, WaveChart.INJECTOR_4, 0.1, 51);
+        TestingUtils.assertWave(msg, chart, WaveChart.INJECTOR_2, 0.1, 231);
+        TestingUtils.assertWave(msg, chart, WaveChart.INJECTOR_1, 0.1, 411);
+        TestingUtils.assertWave(msg, chart, WaveChart.INJECTOR_3, 0.1, 591);
 
-        assertWave(msg, chart, WaveChart.SPARK_4, 0.13333, 6);
-        assertWave(chart, WaveChart.SPARK_2, 0.13333, 186);
-        assertWave(chart, WaveChart.SPARK_1, 0.13333, 366);
-        assertWave(chart, WaveChart.SPARK_3, 0.13333, 546);
+        TestingUtils.assertWave(msg, chart, WaveChart.SPARK_4, 0.13333, 6);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_2, 0.13333, 186);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_1, 0.13333, 366);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_3, 0.13333, 546);
     }
 
     private static void testFordFiesta() {
@@ -134,11 +55,11 @@ public class AutoTest {
 
     private static void testFord6() {
         WaveChart chart;
-        changeRpm(2000);
+        IoUtil.changeRpm(2000);
         chart = nextChart();
 
         int x = 10;
-        assertWave("ford 6", chart, WaveChart.SPARK_1, 0.01666, x, x + 120, x + 240, x + 360, x + 480, x + 600);
+        TestingUtils.assertWave("ford 6", chart, WaveChart.SPARK_1, 0.01666, x, x + 120, x + 240, x + 360, x + 480, x + 600);
 
     }
 
@@ -146,34 +67,34 @@ public class AutoTest {
         WaveChart chart;
         // todo: interesting changeRpm(100);
         sendCommand("set_cranking_rpm 500");
-        changeRpm(200);
+        IoUtil.changeRpm(200);
 
         chart = nextChart();
         float x = 55;
-        assertWave("aspire", chart, WaveChart.SPARK_1, 0.18, x, x + 180, x + 360, x + 540);
+        TestingUtils.assertWave("aspire", chart, WaveChart.SPARK_1, 0.18, x, x + 180, x + 360, x + 540);
 
 
-        changeRpm(600);
+        IoUtil.changeRpm(600);
         chart = nextChart();
         x = 76;
-        assertWave(chart, WaveChart.SPARK_1, 0.04, x, x + 180, x + 360, x + 540);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_1, 0.04, x, x + 180, x + 360, x + 540);
         sendCommand("set_cranking_rpm 700");
         chart = nextChart();
         x = 55;
-        assertWave("cranking@600", chart, WaveChart.SPARK_1, 0.18, x, x + 180, x + 360, x + 540);
+        TestingUtils.assertWave("cranking@600", chart, WaveChart.SPARK_1, 0.18, x, x + 180, x + 360, x + 540);
 
-        changeRpm(2000);
+        IoUtil.changeRpm(2000);
         sendCommand("set_whole_fuel_map 1.57");
 
         chart = nextChart();
 
-        assertWave(chart, WaveChart.INJECTOR_1, 0.051, 238.75);
-        assertWave(chart, WaveChart.INJECTOR_2, 0.051, 53.04);
-        assertWave(chart, WaveChart.INJECTOR_3, 0.051, 417.04);
-        assertWave(chart, WaveChart.INJECTOR_4, 0.051, 594.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_1, 0.051, 238.75);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_2, 0.051, 53.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_3, 0.051, 417.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_4, 0.051, 594.04);
 
         x = 44;
-        assertWave(chart, WaveChart.SPARK_1, 0.133, x, x + 180, x + 360, x + 540);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_1, 0.133, x, x + 180, x + 360, x + 540);
 
         sendCommand("set_fuel_map 2200 4 15.66");
         sendCommand("set_fuel_map 2000 4 15.66");
@@ -183,63 +104,35 @@ public class AutoTest {
         sendCommand("set_fake_maf_voltage 2");
         chart = nextChart();
 
-        assertWave(chart, WaveChart.INJECTOR_1, 0.522, 238.75);
-        assertWave(chart, WaveChart.INJECTOR_2, 0.522, 53.04);
-        assertWave(chart, WaveChart.INJECTOR_3, 0.522, 417.04);
-        assertWave(chart, WaveChart.INJECTOR_4, 0.522, 594.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_1, 0.522, 238.75);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_2, 0.522, 53.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_3, 0.522, 417.04);
+        TestingUtils.assertWave(chart, WaveChart.INJECTOR_4, 0.522, 594.04);
 
         x = 58;
-        assertWave(chart, WaveChart.SPARK_1, 0.133, x, x + 180, x + 360, x + 540);
+        TestingUtils.assertWave(chart, WaveChart.SPARK_1, 0.133, x, x + 180, x + 360, x + 540);
     }
 
 
-    private static void assertWave(WaveChart chart, String key, double width, double... expectedAngles) {
-        assertWave("", chart, key, width, expectedAngles);
-    }
+    public static void main(String[] args) throws InterruptedException {
+        FileLog.SIMULATOR_CONSOLE.start();
+        FileLog.MAIN.start();
 
-    private static void assertWave(String msg, WaveChart chart, String key, double width, double... expectedAngles) {
-        RevolutionLog revolutionLog = chart.getRevolutionsLog();
-        if (revolutionLog.keySet().isEmpty())
-            throw new IllegalStateException(msg + " Empty revolutions in " + chart);
-
-        StringBuilder events = chart.get(key);
-        assertTrue(msg + " Events not null for " + key, events != null);
-        List<WaveReport.UpDown> wr = WaveReport.parse(events.toString());
-        assertTrue(msg + " waves for " + key, !wr.isEmpty());
-        for (WaveReport.UpDown ud : wr) {
-            double angleByTime = revolutionLog.getCrankAngleByTime(ud.upTime);
-            assertCloseEnough(msg + " angle for " + key, angleByTime, expectedAngles);
-
-            assertCloseEnough(msg + "width for " + key, ud.getDutyCycle(revolutionLog), width);
-        }
-    }
-
-    private static void changeRpm(final int rpm) {
-        sendCommand("rpm " + rpm);
-        long time = System.currentTimeMillis();
-
-        final CountDownLatch rpmLatch = new CountDownLatch(1);
-        SensorCentral.AdcListener listener = new SensorCentral.AdcListener() {
-            @Override
-            public void onAdcUpdate(SensorCentral model, double value) {
-                double actualRpm = SensorCentral.getInstance().getValue(Sensor.RPM);
-                if (isCloseEnough(rpm, actualRpm))
-                    rpmLatch.countDown();
-            }
-        };
-        SensorCentral.getInstance().addListener(Sensor.RPM, listener);
+        boolean failed = false;
         try {
-            rpmLatch.await(10, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new IllegalStateException(e);
+            IoUtil.launchSimulator();
+            mainTestBody();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            failed = true;
+        } finally {
+            ExecHelper.destroy();
         }
-        FileLog.MAIN.logLine("RPM change [" + rpm + "] executed in " + (System.currentTimeMillis() - time));
-        SensorCentral.getInstance().removeListener(Sensor.RPM, listener);
-
-        double actualRpm = SensorCentral.getInstance().getValue(Sensor.RPM);
-
-        if (!isCloseEnough(rpm, actualRpm))
-            throw new IllegalStateException("rpm change did not happen: " + rpm + ", actual " + actualRpm);
+        if (failed)
+            System.exit(-1);
+        FileLog.MAIN.logLine("*******************************************************************************");
+        FileLog.MAIN.logLine("************************************  Looks good! *****************************");
+        FileLog.MAIN.logLine("*******************************************************************************");
+        System.exit(0);
     }
-
 }
--- java_console/autotest/src/com/rusefi/TestingUtils.java
+++ java_console/autotest/src/com/rusefi/TestingUtils.java
@@ -1,8 +1,12 @@
 package com.rusefi;
 
 import com.irnems.FileLog;
+import com.rusefi.waves.RevolutionLog;
+import com.rusefi.waves.WaveChart;
+import com.rusefi.waves.WaveReport;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.rusefi.waves.WaveReport.isCloseEnough;
 
@@ -32,5 +36,26 @@ public class TestingUtils {
     static void assertTrue(boolean b) {
         if (!b)
             fail("Not true");
+    }
+
+    static void assertWave(WaveChart chart, String key, double width, double... expectedAngles) {
+        assertWave("", chart, key, width, expectedAngles);
+    }
+
+    static void assertWave(String msg, WaveChart chart, String key, double width, double... expectedAngles) {
+        RevolutionLog revolutionLog = chart.getRevolutionsLog();
+        if (revolutionLog.keySet().isEmpty())
+            throw new IllegalStateException(msg + " Empty revolutions in " + chart);
+
+        StringBuilder events = chart.get(key);
+        assertTrue(msg + " Events not null for " + key, events != null);
+        List<WaveReport.UpDown> wr = WaveReport.parse(events.toString());
+        assertTrue(msg + " waves for " + key, !wr.isEmpty());
+        for (WaveReport.UpDown ud : wr) {
+            double angleByTime = revolutionLog.getCrankAngleByTime(ud.upTime);
+            assertCloseEnough(msg + " angle for " + key, angleByTime, expectedAngles);
+
+            assertCloseEnough(msg + "width for " + key, ud.getDutyCycle(revolutionLog), width);
+        }
     }
 }
