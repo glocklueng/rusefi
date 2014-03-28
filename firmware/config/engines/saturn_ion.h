1eaba6bf661418bbea1f76cd7175635da36b3e58
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-03-28 17:47:10 +0000

    7x GM DIS trigger

---
+++ firmware/config/engines/saturn_ion.h
@@ -0,0 +1,13 @@
+/**
+ * @file saturn_ion.h
+ *
+ * @date Mar 28, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef SATURN_ION_H_
+#define SATURN_ION_H_
+
+
+
+#endif /* SATURN_ION_H_ */
