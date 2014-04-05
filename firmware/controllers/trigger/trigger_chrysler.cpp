7d26c8ac9add20a1d6a8b7c6c33dae0940a1a3c0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-05 15:06:44 +0000

    making progress

--- firmware/controllers/trigger/trigger_chrysler.cpp
+++ firmware/controllers/trigger/trigger_chrysler.cpp
@@ -13,46 +13,17 @@ void configureNeonTriggerShape(trigger_shape_s *s) {
 	// voodoo magic - we always need 720 at the end
 	int base = 720 - 560;
 
-//	triggerAddEvent(s, 5 , T_SECONDARY, TV_HIGH); // 1
-//	triggerAddEvent(s, 8, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, 11, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 14, T_SECONDARY, TV_LOW);
-//
-//	triggerAddEvent(s, 18, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 21, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, 24, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 29, T_SECONDARY, TV_LOW);
-//
-//
-//	triggerAddEvent(s, 40, T_SECONDARY, TV_HIGH); // 2
-//	triggerAddEvent(s, 45, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, 50, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 55, T_SECONDARY, TV_LOW);
-//
-//	triggerAddEvent(s, 60, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 65, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, 70, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 75, T_SECONDARY, TV_LOW);
-//
-//
-//	triggerAddEvent(s, 79, T_SECONDARY, TV_HIGH); // 3
-//	triggerAddEvent(s, 83, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, 87, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 91, T_SECONDARY, TV_LOW);
-//
-//	triggerAddEvent(s, 95, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, 99, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s,103, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s,106, T_SECONDARY, TV_LOW);
-//
-//
-//	triggerAddEvent(s,109, T_SECONDARY, TV_HIGH); // 4
-//	triggerAddEvent(s,112, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s,115, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s,119, T_SECONDARY, TV_LOW);
-//
-//	triggerAddEvent(s,131, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s,135, T_SECONDARY, TV_LOW);
+//	s->initialState[0] = 1;
+
+//	triggerAddEvent(s, base - 720 + 600, T_SECONDARY, TV_HIGH);
+//	triggerAddEvent(s, base - 720 + 604, T_SECONDARY, TV_LOW);
+//	triggerAddEvent(s, base - 720 + 616, T_SECONDARY, TV_HIGH);
+//	triggerAddEvent(s, base - 720 + 620, T_SECONDARY, TV_LOW);
+//	triggerAddEvent(s, base - 720 + 643, T_SECONDARY, TV_HIGH);
+//	triggerAddEvent(s, base - 720 + 648, T_SECONDARY, TV_LOW);
+//	triggerAddEvent(s, base - 720 + 671, T_SECONDARY, TV_HIGH);
+//	triggerAddEvent(s, base - 720 + 676, T_SECONDARY, TV_LOW);
+
 
 	triggerAddEvent(s, base + 0, T_PRIMARY, TV_LOW);
 
@@ -83,14 +54,14 @@ void configureNeonTriggerShape(trigger_shape_s *s) {
 	triggerAddEvent(s, base + 418, T_SECONDARY, TV_LOW);
 	triggerAddEvent(s, base + 436, T_SECONDARY, TV_HIGH);
 	triggerAddEvent(s, base + 441, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, base + 463, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, base + 448, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, base + 492, T_SECONDARY, TV_HIGH);
-//	triggerAddEvent(s, base + 497, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, base + 463, T_SECONDARY, TV_HIGH);
+	triggerAddEvent(s, base + 468, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, base + 492, T_SECONDARY, TV_HIGH);
+	triggerAddEvent(s, base + 497, T_SECONDARY, TV_LOW);
 
 
 	triggerAddEvent(s, base + 560, T_PRIMARY, TV_HIGH); // width =
 
-	s->shaftPositionEventCount = 4 + 8 + 8 + 4;
+	s->shaftPositionEventCount = 4 + 8 + 8 + 8;
 }
 
