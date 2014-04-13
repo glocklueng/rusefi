bb577c77d49d1c2fa3ef515de7cfd32388085163
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-13 17:06:24 +0000

    inverting trigger

--- trunk/firmware/controllers/trigger/trigger_mazda.c
+++ trunk/firmware/controllers/trigger/trigger_mazda.c
@@ -26,33 +26,33 @@ void initializeMazdaMiataNbShape(trigger_shape_s *s) {
 	/**
 	 * cam sensor is primary, crank sensor is secondary
 	 */
-	triggerAddEvent(s, 20, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, 20, T_PRIMARY, 1);
 
-	triggerAddEvent(s, 66, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 70, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 136, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 140, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 246, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 250, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 316, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 320, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 66, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 70, T_SECONDARY, 1);
+	triggerAddEvent(s, 136, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 140, T_SECONDARY, 1);
+	triggerAddEvent(s, 246, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 250, T_SECONDARY, 1);
+	triggerAddEvent(s, 316, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 320, T_SECONDARY, 1);
 
-	triggerAddEvent(s, 340, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, 360, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, 340, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, 360, T_PRIMARY, 1);
 
-	triggerAddEvent(s, 380, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, 400, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, 380, T_PRIMARY, TV_LOW);
+	triggerAddEvent(s, 400, T_PRIMARY, 1);
 
-	triggerAddEvent(s, 426, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 430, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 496, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 500, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 606, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 610, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 676, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 680, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 426, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 430, T_SECONDARY, 1);
+	triggerAddEvent(s, 496, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 500, T_SECONDARY, 1);
+	triggerAddEvent(s, 606, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 610, T_SECONDARY, 1);
+	triggerAddEvent(s, 676, T_SECONDARY, TV_LOW);
+	triggerAddEvent(s, 680, T_SECONDARY, 1);
 
-	triggerAddEvent(s, 720, T_PRIMARY, TV_HIGH);
+	triggerAddEvent(s, 720, T_PRIMARY, TV_LOW);
 
 	s->shaftPositionEventCount = 6 + 16;
 }
