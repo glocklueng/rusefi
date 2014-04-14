5ea6a59e1ee4ae098c621c66b90b9ce6f252f398
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 21:32:05 +0000

    warn command

--- firmware/controllers/error_handling.c
+++ firmware/controllers/error_handling.c
@@ -13,12 +13,14 @@ static time_t timeOfPreviousWarning = (systime_t) -10 * CH_FREQUENCY;
 
 static Logging logger;
 
+extern int warningEnabled;
+
 /**
  * @returns TRUE in case there are too many warnings
  */
 int warning(obd_code_e code, const char *fmt, ...) {
 	time_t now = chTimeNow();
-	if (overflowDiff(now, timeOfPreviousWarning) < CH_FREQUENCY)
+	if (overflowDiff(now, timeOfPreviousWarning) < CH_FREQUENCY || !warningEnabled)
 		return TRUE; // we just had another warning, let's not spam
 	timeOfPreviousWarning = now;
 
