88c5bd433c07808d65f55835c48f7c1d045971dd
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 01:26:30 +0000

    making progress

--- trunk/firmware/config/engines/ford_aspire.cpp
+++ trunk/firmware/config/engines/ford_aspire.cpp
@@ -118,6 +118,10 @@ void setFordAspireEngineConfiguration(engine_configuration_s *engineConfiguratio
 	engineConfiguration->triggerConfig.triggerType = TT_FORD_ASPIRE;
 	engineConfiguration->triggerConfig.isSynchronizationNeeded = FALSE;
 
+
+	boardConfiguration->injectionPins[4] = GPIO_NONE;
+	boardConfiguration->injectionPins[5] = GPIO_NONE;
+
 	engineConfiguration->HD44780width = 20;
 	engineConfiguration->HD44780height = 4;
 
