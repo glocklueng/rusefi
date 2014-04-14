cb4cd1dc6a3ef6ef87ecd7630e3984e439ae3951
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 15:22:23 +0000

    making progress

---
+++ firmware/controllers/system/signal_executor_gpt.c
@@ -0,0 +1,12 @@
+/**
+ * @file	signal_executor_gpt.c
+ *
+ * @date Apr 14, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ *
+ * 32 bit timers in stm32f4: TIM2, TIM5
+ *
+ * Output resolution in general is discussed @ http://rusefi.com/forum/viewtopic.php?t=2
+ */
+
+
