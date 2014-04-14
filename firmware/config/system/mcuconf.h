0ddb2fc5b518f494e849bcefb8c0295410309982
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 02:19:53 +0000

    making some RTC progress

--- firmware/config/system/mcuconf.h
+++ firmware/config/system/mcuconf.h
@@ -39,7 +39,10 @@
 #define STM32_HSI_ENABLED                   TRUE
 #define STM32_LSI_ENABLED                   TRUE
 #define STM32_HSE_ENABLED                   TRUE
+
+// change this 'FALSE to TRUE if you have the LSE 32768 quarts
 #define STM32_LSE_ENABLED                   FALSE
+
 #define STM32_CLOCK48_REQUIRED              TRUE
 #define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSE
@@ -54,7 +57,11 @@
 #define STM32_HPRE                          STM32_HPRE_DIV1
 #define STM32_PPRE1                         STM32_PPRE1_DIV4
 #define STM32_PPRE2                         STM32_PPRE2_DIV2
-#define STM32_RTCSEL                        STM32_RTCSEL_LSI
+#if STM32_LSE_ENABLED
+ #define STM32_RTCSEL                        STM32_RTCSEL_LSE
+#else
+ #define STM32_RTCSEL                        STM32_RTCSEL_LSI
+#endif
 #define STM32_RTCPRE_VALUE                  8
 #define STM32_MCO1SEL                       STM32_MCO1SEL_HSI
 #define STM32_MCO1PRE                       STM32_MCO1PRE_DIV1
