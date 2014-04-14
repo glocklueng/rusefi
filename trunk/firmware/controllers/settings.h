dbafae8f1405d86ae1e923e0feb0e64165088d9b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 23:14:12 +0000

    C++

--- trunk/firmware/controllers/settings.h
+++ trunk/firmware/controllers/settings.h
@@ -9,10 +9,19 @@
 #ifndef INJECTOR_CONTROL_H_
 #define INJECTOR_CONTROL_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
 void initSettings(void);
 void pokeControl(void);
 int getInjectionPeriod(void);
 int getInjectionOffset(void);
 int getInjectionDivider(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* INJECTOR_CONTROL_H_ */
