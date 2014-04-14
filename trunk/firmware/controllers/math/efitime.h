6a25af1e0f258c84b81e31ce4c94e990815eee2e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-14 19:35:16 +0000

    header for everything time

---
+++ trunk/firmware/controllers/math/efitime.h
@@ -0,0 +1,16 @@
+/*
+ * @file efitime.h
+ *
+ * By the way, there are 86400000 milliseconds in a day
+ *
+ * @date Apr 14, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef EFITIME_H_
+#define EFITIME_H_
+
+
+
+
+#endif /* EFITIME_H_ */
