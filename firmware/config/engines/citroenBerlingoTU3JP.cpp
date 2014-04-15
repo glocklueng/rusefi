b315cd7aceafe47d737eac4a64d2068ce2ace7fd
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-15 21:17:07 +0000

    making progress

---
+++ firmware/config/engines/citroenBerlingoTU3JP.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file	citroenBerlingoTU3JP.cpp
+ *
+ * @date Apr 15, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "citroenBerlingoTU3JP.h"
+
+void setCitroenBerlingoTU3JPConfiguration(engine_configuration_s *engineConfiguration, board_configuration_s *boardConfiguration) {
+	engineConfiguration->engineType = CITROEN_TU3JP;
+
+
+	// set_cranking_injection_mode 0
+	engineConfiguration->crankingInjectionMode = IM_SIMULTANEOUS;
+	// set_injection_mode 2
+	engineConfiguration->injectionMode = IM_BATCH;
+	// set_ignition_mode 2
+	engineConfiguration->ignitionMode = IM_WASTED_SPARK;
+	// set_firing_order 2
+	engineConfiguration->firingOrder = FO_1_THEN_3_THEN_4_THEN2;
+
+
+}
+
