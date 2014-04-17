ffdf2f8ac7b713eef10eb36d734665aabfdcde3a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-17 17:38:38 +0000

    making progress

--- trunk/firmware/controllers/math/efitime.h
+++ trunk/firmware/controllers/math/efitime.h
@@ -10,9 +10,28 @@
 #ifndef EFITIME_H_
 #define EFITIME_H_
 
+#include <stdint.h>
 #include "efifeatures.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
 // todo: implement a function to work with times considering counter overflow
 #define overflowDiff(now, time) ((now) - (time))
+
+/**
+ * 64-bit counter of microseconds (1/1 000 000 of a second) since MCU reset
+ *
+ * By using 64 bit, we can achive a very precise timestamp which does not overflow.
+ * The primary implementation counts the number of CPU cycles from MCU reset.
+ */
+uint64_t getTimeNowUs(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* EFITIME_H_ */
