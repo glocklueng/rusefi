a1740b0cad02e4788db37a59f3f65f7a8af3c5bd
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-18 23:59:53 +0000

    making progress

--- firmware/controllers/system/SingleTimerExecutor.h
+++ firmware/controllers/system/SingleTimerExecutor.h
@@ -8,9 +8,14 @@
 #ifndef SINGLETIMEREXECUTOR_H_
 #define SINGLETIMEREXECUTOR_H_
 
+#include "signal_executor.h"
+#include "event_queue.h"
+
 class Executor {
 public:
 	Executor();
+	void schedule(scheduling_s *scheduling, uint64_t nowUs, int delayUs, schfunc_t callback, void *param);
+	EventQueue eq;
 };
 
 #endif /* SINGLETIMEREXECUTOR_H_ */
