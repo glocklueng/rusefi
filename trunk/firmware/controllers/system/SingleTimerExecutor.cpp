b5ac9ddce5c1a1fe7ab3b3503f3ef917009f2bcd
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-18 23:59:53 +0000

    making progress

--- trunk/firmware/controllers/system/SingleTimerExecutor.cpp
+++ trunk/firmware/controllers/system/SingleTimerExecutor.cpp
@@ -5,15 +5,42 @@
  *      Author: Andrey
  */
 
-
 #include "SingleTimerExecutor.h"
+#include "efitime.h"
+#include "signal_temp.h"
+
+#if EFI_SIGNAL_EXECUTOR_ONE_TIMER || defined(__DOXYGEN__)
+
+static Executor instance;
+
+extern schfunc_t globalTimerCallback;
+
+static void executorCallback(void *arg) {
+	instance.eq.execute(getTimeNowUs());
+}
 
 Executor::Executor() {
+	globalTimerCallback = executorCallback;
+}
+
+void Executor::schedule(scheduling_s *scheduling, uint64_t nowUs, int delayUs, schfunc_t callback, void *param) {
+	eq.schedule(scheduling, nowUs, delayUs, callback, param);
+	uint64_t nextEventTime = eq.getNextEventTime();
+	setTimer
 
 }
 
-//Executor
+void scheduleTask(scheduling_s *scheduling, float delayMs, schfunc_t callback, void *param) {
+	instance.schedule(scheduling, getTimeNowUs(), delayMs * 1000000, callback, param);
+}
+
+void initOutputSignal(OutputSignal *signal, io_pin_e ioPin) {
+	signal->io_pin = ioPin;
+	signal->name = getPinName(ioPin);
+//	signal->duration = 0;
+	initOutputSignalBase(signal);
+}
 
 
-
+#endif
 
