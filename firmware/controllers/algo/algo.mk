a318730fb4da9f1f862e7a435eccb10effc43d69
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-21 14:21:05 +0000

    accel enrichment

--- firmware/controllers/algo/algo.mk
+++ firmware/controllers/algo/algo.mk
@@ -14,6 +14,6 @@ CONTROLLERS_ALGO_SRC = $(PROJECT_DIR)/controllers/algo/map_adjuster.c \
 	
 CONTROLLERS_ALGO_SRC_CPP = $(PROJECT_DIR)/controllers/algo/OutputSignalArray.cpp \
 	$(PROJECT_DIR)/controllers/algo/fuel_math.cpp \
+	$(PROJECT_DIR)/controllers/algo/accel_enrichmemnt.cpp \
 	$(PROJECT_DIR)/controllers/algo/event_queue.cpp
 	
-
