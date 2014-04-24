e31afe75292c44149afbf48892d73c1ded040a91
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-24 00:25:58 +0000

    moving to microseconds

--- trunk/firmware/controllers/math/efitime.h
+++ trunk/firmware/controllers/math/efitime.h
@@ -27,6 +27,8 @@ extern "C"
 
 #define US_PER_SECOND 1000000
 
+#define US_TO_TI_TEMP 10
+
 // todo: implement a function to work with times considering counter overflow
 #define overflowDiff(now, time) ((now) - (time))
 
