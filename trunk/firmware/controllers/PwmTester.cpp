bcb4f841cf097b0df7950811f1590cfaa44e706d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-30 02:55:45 +0000

    making progress

--- trunk/firmware/controllers/PwmTester.cpp
+++ trunk/firmware/controllers/PwmTester.cpp
@@ -7,7 +7,25 @@
 
 #include "main.h"
 #include "PwmTester.h"
+#include "trigger_structure.h"
+#include "pwm_generator_logic.h"
+
+static float _switchTimes[2];
+
+// todo: extract helper for simple PWM?
+static int pinStates[2];
+static single_wave_s wave(pinStates);
+static single_wave_s sr[1] = {wave};
+
+static PwmConfig pwmTest(_switchTimes, sr);
 
 void initPwmTester(void) {
+//	startSimplePwm(&idleValve, "Idle Valve",
+//			boardConfiguration->idleValvePin,
+//			IDLE_VALVE,
+//			0.5,
+//			IDLE_AIR_CONTROL_VALVE_PWM_FREQUENCY
+//
+//			);
 
 }
