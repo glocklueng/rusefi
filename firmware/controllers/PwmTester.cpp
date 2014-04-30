51fcff50ed390d6e3342da26318e5c7d4faa429b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-04-30 03:19:49 +0000

    making progress

--- firmware/controllers/PwmTester.cpp
+++ firmware/controllers/PwmTester.cpp
@@ -15,17 +15,13 @@ static float _switchTimes[2];
 // todo: extract helper for simple PWM?
 static int pinStates[2];
 static single_wave_s wave(pinStates);
-static single_wave_s sr[1] = {wave};
+static single_wave_s sr[1] = { wave };
 
 static PwmConfig pwmTest(_switchTimes, sr);
 
 void initPwmTester(void) {
-//	startSimplePwm(&idleValve, "Idle Valve",
-//			boardConfiguration->idleValvePin,
-//			IDLE_VALVE,
-//			0.5,
-//			IDLE_AIR_CONTROL_VALVE_PWM_FREQUENCY
-//
-//			);
+	startSimplePwm(&pwmTest, "tester", boardConfiguration->injectionPins[0], INJECTOR_1_OUTPUT, 0.5, 100
+
+	);
 
 }
