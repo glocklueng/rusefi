7606ddd98f67ae0ebd98214e60c6eae22529849f
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-05-11 20:39:09 +0000

    making progress: mini cooper

---
+++ trunk/firmware/controllers/trigger/trigger_bmw.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file	trigger_bmw.cpp
+ *
+ * @date May 11, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2013
+ */
+
+#include "trigger_bmw.h"
+
+void configureMiniCooperTriggerShape(engine_configuration_s *engineConfiguration,
+		engine_configuration2_s *engineConfiguration2) {
+
+}
