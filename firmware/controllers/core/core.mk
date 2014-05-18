13c6306479dba8e9628fe0b05ff1188f7d50d333
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-05-18 19:39:18 +0000

    better folder structure

---
+++ firmware/controllers/core/core.mk
@@ -0,0 +1,5 @@
+
+CONTROLLERS_CORE_SRC = $(PROJECT_DIR)/controllers/core/interpolation.c \
+	$(PROJECT_DIR)/controllers/core/avg_values.c
+
+CONTROLLERS_CORE_SRC_CPP = 
--- firmware/controllers/math/math.mk
+++ firmware/controllers/math/math.mk
@@ -1,5 +1,4 @@
 
-CONTROLLERS_MATH_SRC = $(PROJECT_DIR)/controllers/math/avg_values.c \
-	$(PROJECT_DIR)/controllers/math/interpolation.c
+CONTROLLERS_MATH_SRC = 
 
 CONTROLLERS_MATH_SRC_CPP = $(PROJECT_DIR)/controllers/math/engine_math.cpp 
