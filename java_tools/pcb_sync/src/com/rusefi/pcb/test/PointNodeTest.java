306aad36ba0d9bce387b7f8aa163caf64c96cbd7
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-05-29 01:18:03 +0000

    improving unit test

--- java_tools/pcb_sync/src/com/rusefi/pcb/test/PointNodeTest.java
+++ java_tools/pcb_sync/src/com/rusefi/pcb/test/PointNodeTest.java
@@ -9,21 +9,23 @@ import org.junit.Test;
  * 5/28/2014
  */
 public class PointNodeTest {
+    private static final double EPS = 0.0000001;
+
     @Test
     public void testTurn() {
 
         PointNode x10y50 = new PointNode(10, 50);
 
         PointNode angle270 = new PointNode(0, 0, 270);
-        Assert.assertEquals("270 x", angle270.translate(x10y50).x, 50.0);
-        Assert.assertEquals(angle270.translate(x10y50).y, -10.0);
+        Assert.assertEquals("270 x", angle270.translate(x10y50).x, 50.0, EPS);
+        Assert.assertEquals(angle270.translate(x10y50).y, -10.0, EPS);
 
         PointNode angle180 = new PointNode(0, 0, 180);
-        Assert.assertEquals("180 x", angle180.translate(x10y50).x, -10.0);
-        Assert.assertEquals(angle180.translate(x10y50).y, -50.0);
+        Assert.assertEquals("180 x", angle180.translate(x10y50).x, -10.0, EPS);
+        Assert.assertEquals(angle180.translate(x10y50).y, -50.0, EPS);
 
         PointNode angle90 = new PointNode(0, 0, 90);
-        Assert.assertEquals(angle90.translate(x10y50).x, -50.0);
-        Assert.assertEquals(angle90.translate(x10y50).y, 10.0);
+        Assert.assertEquals(angle90.translate(x10y50).x, -50.0, EPS);
+        Assert.assertEquals(angle90.translate(x10y50).y, 10.0, EPS);
     }
 }
