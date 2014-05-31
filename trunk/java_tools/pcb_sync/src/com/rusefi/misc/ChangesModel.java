46612f9474b8b0c81b2581f55326f2ba290e5072
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-05-31 01:06:22 +0000

    getting ready to net merge

--- trunk/java_tools/pcb_sync/src/com/rusefi/misc/ChangesModel.java
+++ trunk/java_tools/pcb_sync/src/com/rusefi/misc/ChangesModel.java
@@ -22,6 +22,7 @@ public class ChangesModel {
     private static final String MOVE = "move_module";
     private static final String OPTIMIZE = "optimize";
     private static final String COPY = "copy";
+    private static final String MERGE_NET = "merge_net";
 
     public final Set<String> DEL_REQUESTS = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
     public final List<NameAndOffset> ADD_REQUESTS = new ArrayList<NameAndOffset>();
@@ -29,6 +30,7 @@ public class ChangesModel {
 
     public final List<TwoFileRequest> OPTIMIZE_REQUESTS = new ArrayList<TwoFileRequest>();
     public final List<TwoFileRequest> COPY_REQUESTS = new ArrayList<TwoFileRequest>();
+    public final List<TwoFileRequest> MERGE_REQUESTS = new ArrayList<TwoFileRequest>();
 
     public static ChangesModel getInstance() {
         return instance;
@@ -70,6 +72,9 @@ public class ChangesModel {
             } else if (line.toLowerCase().startsWith(COPY)) {
                 COPY_REQUESTS.add(TwoFileRequest.parseTwoFile(line.substring(COPY.length()).trim(), lineIndex));
                 continue;
+            } else if (line.toLowerCase().startsWith(MERGE_NET)) {
+                MERGE_REQUESTS.add(TwoFileRequest.parseTwoFile(line.substring(MERGE_NET.length()).trim(), lineIndex));
+                continue;
             }
 
             System.err.println("ChangesModel: Ignoring invalid line: " + line);
@@ -78,6 +83,7 @@ public class ChangesModel {
         log("Got " + DEL_REQUESTS.size() + " remove request(s)");
         log("Got " + ADD_REQUESTS.size() + " add request(s)");
         log("Got " + OPTIMIZE_REQUESTS.size() + " optimize request(s)");
+        log("Got " + MERGE_REQUESTS.size() + " merge net request(s)");
     }
 
     private void addMoveRequest(String request) {
