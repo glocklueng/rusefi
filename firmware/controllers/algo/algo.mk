2ccc4837e2d773553527ce19ffb2eaf34d06b3fd
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-06-02 23:24:12 +0000

    C++

--- firmware/controllers/algo/algo.mk
+++ firmware/controllers/algo/algo.mk
@@ -3,7 +3,6 @@ CONTROLLERS_ALGO_SRC = $(PROJECT_DIR)/controllers/algo/map_adjuster.c \
 	$(PROJECT_DIR)/controllers/algo/advance_map.c \
 	$(PROJECT_DIR)/controllers/algo/signal_executor.c \
 	$(PROJECT_DIR)/controllers/algo/malfunction_central.c \
-	$(PROJECT_DIR)/controllers/algo/event_registry.c \
 	$(PROJECT_DIR)/controllers/algo/idle_controller.c \
 	$(PROJECT_DIR)/controllers/algo/wave_chart.c \
 	$(PROJECT_DIR)/controllers/algo/nmea.c
@@ -13,5 +12,6 @@ CONTROLLERS_ALGO_SRC_CPP = $(PROJECT_DIR)/controllers/algo/OutputSignalArray.cpp
 	$(PROJECT_DIR)/controllers/algo/accel_enrichment.cpp \
 	$(PROJECT_DIR)/controllers/algo/engine_configuration.cpp \
 	$(PROJECT_DIR)/controllers/algo/engine.cpp \
+	$(PROJECT_DIR)/controllers/algo/event_registry.cpp \
 	$(PROJECT_DIR)/controllers/algo/algo.cpp
 	
