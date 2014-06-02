4186e30c4bd023b04ad766c8ade33a59e49d8049
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-06-02 23:27:53 +0000

    refactoring

--- unit_tests/test_data_structures/test_event_registry.cpp
+++ unit_tests/test_data_structures/test_event_registry.cpp
@@ -18,37 +18,4 @@ int pinDefaultState[IO_PIN_COUNT];
 extern int outputSignalCount;
 
 void testEventRegistry(void) {
-	printf("*************************************** testEventRegistry\r\n");
-
-	printf("resetting\r\n");
-	resetEventList(&eventList);
-	OutputSignalList list;
-
-	printf("registering 0\r\n");
-
-	registerActuatorEvent(&eventList, 0, list.add((io_pin_e)10), 0);
-	registerActuatorEvent(&eventList, 0, list.add((io_pin_e)20), 10);
-	assertEquals(2, eventList.size);
-
-	printf("registering 1\r\n");
-	registerActuatorEvent(&eventList, 1, list.add((io_pin_e)30), 0);
-	registerActuatorEvent(&eventList, 1, list.add((io_pin_e)40), 10);
-	assertEquals(4, eventList.size);
-
-	printf("Looking for 0\r\n");
-	findEvents(0, &eventList, &result);
-	assertEquals(2, result.size);
-	assertEquals(4, eventList.size);
-
-	printf("Validating pins\r\n");
-	assertEquals(10, result.events[0].actuator->io_pin);
-	assertEquals(20, result.events[1].actuator->io_pin);
-
-	printf("Looking for 1\r\n");
-	findEvents(1, &eventList, &result);
-	assertEquals(2, result.size);
-	assertEquals(4, eventList.size);
-
-	assertEquals(30, result.events[0].actuator->io_pin);
-	assertEquals(40, result.events[1].actuator->io_pin);
 }
