91a8f9460cd0a8aaf27d276aea2bcd4ec2e197f1
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-06-14 03:47:17 +0000

    speed density - making progress, reducing code duplication

--- trunk/firmware/controllers/math/engine_math.h
+++ trunk/firmware/controllers/math/engine_math.h
@@ -18,6 +18,35 @@ void findTriggerPosition(engine_configuration_s const *engineConfiguration, trig
 
 int isInjectionEnabled(engine_configuration2_s const *engineConfiguration2);
 
+// 'random' value to be sure we are not treating any non-zero trash as TRUE
+#define MAGIC_TRUE_VALUE 153351512
+
+template<int RPM_BIN_SIZE, int LOAD_BIN_SIZE>
+class Map3D {
+public:
+	void init(float table[RPM_BIN_SIZE][LOAD_BIN_SIZE]);
+	float getValue(float x, float xBin[], float y, float yBin[]);
+private:
+	float *pointers[LOAD_BIN_SIZE];
+	int initialized;
+};
+
+template<int RPM_BIN_SIZE, int LOAD_BIN_SIZE>
+void Map3D<RPM_BIN_SIZE, LOAD_BIN_SIZE>::init(float table[RPM_BIN_SIZE][LOAD_BIN_SIZE]) {
+	for (int k = 0; k < LOAD_BIN_SIZE; k++)
+		pointers[k] = table[k];
+	initialized = MAGIC_TRUE_VALUE;
+}
+
+template<int RPM_BIN_SIZE, int LOAD_BIN_SIZE>
+float Map3D<RPM_BIN_SIZE, LOAD_BIN_SIZE>::getValue(float x, float xBin[], float y, float yBin[]) {
+	efiAssert(initialized == MAGIC_TRUE_VALUE, "fuel map initialized", NAN);
+	return interpolate3d(x, xBin, LOAD_BIN_SIZE, y, yBin, RPM_BIN_SIZE, pointers);
+}
+
+
+typedef Map3D<16, 16> Map3D1616;
+
 #endif
 
 #ifdef __cplusplus
