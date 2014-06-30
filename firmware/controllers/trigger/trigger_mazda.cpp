54c44b0dc785c01ef10fda7504d2df9e837bb6b9
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-06-30 11:44:41 +0000

    refactoring

--- firmware/controllers/trigger/trigger_mazda.cpp
+++ firmware/controllers/trigger/trigger_mazda.cpp
@@ -26,33 +26,33 @@ void initializeMazdaMiataNbShape(trigger_shape_s *s) {
 	/**
 	 * cam sensor is primary, crank sensor is secondary
 	 */
-	triggerAddEvent(s, 20, T_PRIMARY, TV_HIGH);
+	s->addEvent(20, T_PRIMARY, TV_HIGH);
 
-	triggerAddEvent(s, 66, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 70, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 136, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 140, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 246, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 250, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 316, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 320, T_SECONDARY, TV_HIGH);
+	s->addEvent(66, T_SECONDARY, TV_LOW);
+	s->addEvent(70, T_SECONDARY, TV_HIGH);
+	s->addEvent(136, T_SECONDARY, TV_LOW);
+	s->addEvent(140, T_SECONDARY, TV_HIGH);
+	s->addEvent(246, T_SECONDARY, TV_LOW);
+	s->addEvent(250, T_SECONDARY, TV_HIGH);
+	s->addEvent(316, T_SECONDARY, TV_LOW);
+	s->addEvent(320, T_SECONDARY, TV_HIGH);
 
-	triggerAddEvent(s, 340, T_PRIMARY, TV_LOW);
-	triggerAddEvent(s, 360, T_PRIMARY, TV_HIGH);
+	s->addEvent(340, T_PRIMARY, TV_LOW);
+	s->addEvent(360, T_PRIMARY, TV_HIGH);
 
-	triggerAddEvent(s, 380, T_PRIMARY, TV_LOW);
-	triggerAddEvent(s, 400, T_PRIMARY, TV_HIGH);
+	s->addEvent(380, T_PRIMARY, TV_LOW);
+	s->addEvent(400, T_PRIMARY, TV_HIGH);
 
-	triggerAddEvent(s, 426, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 430, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 496, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 500, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 606, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 610, T_SECONDARY, TV_HIGH);
-	triggerAddEvent(s, 676, T_SECONDARY, TV_LOW);
-	triggerAddEvent(s, 680, T_SECONDARY, TV_HIGH);
+	s->addEvent(426, T_SECONDARY, TV_LOW);
+	s->addEvent(430, T_SECONDARY, TV_HIGH);
+	s->addEvent(496, T_SECONDARY, TV_LOW);
+	s->addEvent(500, T_SECONDARY, TV_HIGH);
+	s->addEvent(606, T_SECONDARY, TV_LOW);
+	s->addEvent(610, T_SECONDARY, TV_HIGH);
+	s->addEvent(676, T_SECONDARY, TV_LOW);
+	s->addEvent(680, T_SECONDARY, TV_HIGH);
 
-	triggerAddEvent(s, 720, T_PRIMARY, TV_LOW);
+	s->addEvent(720, T_PRIMARY, TV_LOW);
 
 	s->shaftPositionEventCount = 6 + 16;
 }
@@ -67,31 +67,31 @@ void configureMazdaProtegeLx(engine_configuration_s *engineConfiguration,
 
 	float w = 720 / 4 * 0.215;
 	float a = 5;
-//	triggerAddEvent(s, a, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, a + w, T_SECONDARY, TV_HIGH);
+//	s->addEvent(a, T_SECONDARY, TV_LOW);
+//	s->addEvent(a + w, T_SECONDARY, TV_HIGH);
 //	a += 180;
-//	triggerAddEvent(s, a, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, a + w, T_SECONDARY, TV_HIGH);
+//	s->addEvent(a, T_SECONDARY, TV_LOW);
+//	s->addEvent(a + w, T_SECONDARY, TV_HIGH);
 //	a += 180;
-//	triggerAddEvent(s, a, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, a + w, T_SECONDARY, TV_HIGH);
+//	s->addEvent(a, T_SECONDARY, TV_LOW);
+//	s->addEvent(a + w, T_SECONDARY, TV_HIGH);
 //	a += 180;
-//	triggerAddEvent(s, a, T_SECONDARY, TV_LOW);
-//	triggerAddEvent(s, a + w, T_SECONDARY, TV_HIGH);
+//	s->addEvent(a, T_SECONDARY, TV_LOW);
+//	s->addEvent(a + w, T_SECONDARY, TV_HIGH);
 
 	float z = 0.093;
 
 	a = 180;
-	triggerAddEvent(s, a - z * 720, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, a, T_PRIMARY, TV_LOW);
+	s->addEvent(a - z * 720, T_PRIMARY, TV_HIGH);
+	s->addEvent(a, T_PRIMARY, TV_LOW);
 
 	a += 180;
-	triggerAddEvent(s, a - z * 720, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, a, T_PRIMARY, TV_LOW);
+	s->addEvent(a - z * 720, T_PRIMARY, TV_HIGH);
+	s->addEvent(a, T_PRIMARY, TV_LOW);
 
 	a += 180;
-	triggerAddEvent(s, a - z * 720, T_PRIMARY, TV_HIGH);
-	triggerAddEvent(s, a, T_PRIMARY, TV_LOW);
+	s->addEvent(a - z * 720, T_PRIMARY, TV_HIGH);
+	s->addEvent(a, T_PRIMARY, TV_LOW);
 
 	a += 180;
 	s->addEvent(a - z * 720, T_PRIMARY, TV_HIGH);
