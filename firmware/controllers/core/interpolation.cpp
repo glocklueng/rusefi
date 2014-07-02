75cfbbbfe87bb779277fa0a860ffa6de5b83dcc0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-07-02 00:36:06 +0000

    refactoring

--- firmware/controllers/core/interpolation.cpp
+++ firmware/controllers/core/interpolation.cpp
@@ -82,7 +82,7 @@ int findIndex(float array[], int size, float value) {
 	int left = 0;
 	int right = size;
 
-	while (1) {
+	while (true) {
 #if 0
 		// that's an assertion to make sure we do not loop here
 		size--;
