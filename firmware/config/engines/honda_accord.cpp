4e3345d4c051c1241aef63822d6ea3aa3a64491e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-07-20 13:47:01 +0000

    making progress

--- firmware/config/engines/honda_accord.cpp
+++ firmware/config/engines/honda_accord.cpp
@@ -1,7 +1,9 @@
 /**
  * @file	honda_accord.cpp
  *
- * 1995 Honda Accord
+ * 1995 Honda Accord EX
+ * http://rusefi.com/wiki/index.php?title=Vehicle:Honda_Accord_1995
+ * http://rusefi.com/forum/viewtopic.php?f=3&t=621
  *
  * engine_type 6
  *
@@ -17,6 +19,24 @@ void setHondaAccordConfiguration(engine_configuration_s *engineConfiguration) {
 	engineConfiguration->engineType = HONDA_ACCORD;
 
 	engineConfiguration->map.sensor.sensorType = MT_HONDA3BAR;
+
+	engineConfiguration->cylindersCount = 4;
+	engineConfiguration->displacement = 2.156;
+
+	// Keihin 06164-P0A-A00
+	engineConfiguration->injectorFlow = 248;
+
+
+	engineConfiguration->algorithm = LM_SPEED_DENSITY;
+
+
+	engineConfiguration->crankingSettings.coolantTempMaxC = 65; // 8ms at 65C
+	engineConfiguration->crankingSettings.fuelAtMaxTempMs = 8;
+
+	engineConfiguration->crankingSettings.coolantTempMinC = 0; // 20ms at 0C
+	engineConfiguration->crankingSettings.fuelAtMinTempMs = 15;
+
+
 
 }
 
