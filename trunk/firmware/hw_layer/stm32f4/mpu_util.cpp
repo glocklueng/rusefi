b863be4ff9289f9c206af4513bd082a9e2b92cb7
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-07-28 00:50:47 +0000

    refactoring: extracting MPU-specific stuff

---
+++ trunk/firmware/hw_layer/stm32f4/mpu_util.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file	mpu_util.cpp
+ *
+ * @date Jul 27, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "main.h"
+#include "mpu_util.h"
+#include "error_handling.h"
+
+void DebugMonitorVector(void) {
+
+	chDbgPanic3("DebugMonitorVector", __FILE__, __LINE__);
+
+	while (TRUE)
+		;
+}
+
+void UsageFaultVector(void) {
+
+	chDbgPanic3("UsageFaultVector", __FILE__, __LINE__);
+
+	while (TRUE)
+		;
+}
+
+void BusFaultVector(void) {
+
+	chDbgPanic3("BusFaultVector", __FILE__, __LINE__);
+
+	while (TRUE)
+		;
+}
+
+void HardFaultVector(void) {
+
+	chDbgPanic3("HardFaultVector", __FILE__, __LINE__);
+
+	while (TRUE)
+		;
+}
+
+
+
