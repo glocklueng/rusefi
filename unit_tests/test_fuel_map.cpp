e5d3fd3cf77eb84bb40767ec5de68dce64be5c7d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-01 01:13:13 +0000

    making progress: simplifying trigger declaration

--- unit_tests/test_fuel_map.cpp
+++ unit_tests/test_fuel_map.cpp
@@ -124,10 +124,13 @@ static void confgiureFordAspireTriggerShape(trigger_shape_s * s) {
 	s->addEvent(720, T_PRIMARY, TV_LOW);
 
 	assertEquals(53.747 / 720, s->wave.getSwitchTime(0));
-	assertEquals(0, s->wave.getChannelState(0, 0));
-	assertEquals(0, s->wave.getChannelState(0, 1));
-	assertEquals(0, s->wave.getChannelState(0, 2));
-	assertEquals(1, s->wave.getChannelState(0, 8));
+	assertEqualsM("@0", 0, s->wave.getChannelState(0, 0));
+	assertEqualsM("@1", 0, s->wave.getChannelState(0, 1));
+	assertEqualsM("@2", 0, s->wave.getChannelState(0, 2));
+	assertEqualsM("@3", 0, s->wave.getChannelState(0, 3));
+	assertEqualsM("@4", 1, s->wave.getChannelState(0, 4));
+	assertEqualsM("@5", 1, s->wave.getChannelState(0, 5));
+	assertEqualsM("@8", 1, s->wave.getChannelState(0, 8));
 	assertEquals(121.90 / 720, s->wave.getSwitchTime(1));
 	assertEquals(657.03 / 720, s->wave.getSwitchTime(8));
 
