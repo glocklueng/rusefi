09909e300d7d29a8cf0312d3a037d07e875dcd6a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-05 23:51:35 +0000

    mitsubishi support

--- firmware/config/engines/mitsubishi.cpp
+++ firmware/config/engines/mitsubishi.cpp
@@ -6,11 +6,47 @@
  */
 
 #include "mitsubishi.h"
+#include "thermistors.h"
 
 void setMitsubishiConfiguration(engine_configuration_s *engineConfiguration, board_configuration_s *boardConfiguration) {
 	engineConfiguration->engineType = MITSU_4G93;
 
 	engineConfiguration->triggerConfig.triggerType = TT_MITSU;
+
+	engineConfiguration->cylindersCount = 4;
+	engineConfiguration->displacement = 1.800;
+
+	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
+	engineConfiguration->firingOrder = FO_1_THEN_3_THEN_4_THEN2;
+
+	setThermistorConfiguration(&engineConfiguration->cltThermistorConf, -20, 18000, 23.8889, 2100, 48.8889, 1000);
+	engineConfiguration->cltThermistorConf.bias_resistor = 2700;
+
+	// Frankenstein: low side - inj #1: PC14
+	// Frankenstein: low side - inj #2: PC15
+	// Frankenstein: low side - inj #3: PE6
+	// Frankenstein: low side - inj #4: PC13
+	// Frankenstein: low side - inj #5: PE4
+	// Frankenstein: low side - inj #6: PE5
+	// Frankenstein: low side - inj #7: PE2
+	// Frankenstein: low side - inj #8: PE3
+	// Frankenstein: low side - inj #9: PE0
+	// Frankenstein: low side - inj #10: PE1
+	// Frankenstein: low side - inj #11: PB8
+	// Frankenstein: low side - inj #12: PB9
+
+	boardConfiguration->injectionPins[0] = GPIOB_9; // Frankenstein: low side - inj #12
+	boardConfiguration->injectionPins[1] = GPIOB_8; // Frankenstein: low side - inj #11
+	boardConfiguration->injectionPins[2] = GPIOE_3; // Frankenstein: low side - inj #8
+	boardConfiguration->injectionPins[3] = GPIOE_5; // Frankenstein: low side - inj #6
+
+
+	// Frankenstein: high side #1: PE8
+	// Frankenstein: high side #2: PE10
+
+	boardConfiguration->ignitionPins[0] = GPIOE_8; // Frankenstein: high side #1
+	boardConfiguration->ignitionPins[1] = GPIO_NONE;
+	boardConfiguration->ignitionPins[2] = GPIOE_10; // // Frankenstein: high side #2
 
 }
 
