09f879213836d23bbacc6f5ec4e9d73ce974c065
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-11 11:01:57 +0000

    bugfix

--- trunk/firmware/controllers/system/pwm_generator_logic.cpp
+++ trunk/firmware/controllers/system/pwm_generator_logic.cpp
@@ -114,12 +114,15 @@ static uint64_t togglePwmState(PwmConfig *state) {
 	scheduleMsg(&logger, "%s: nextSwitchTime %d", state->name, nextSwitchTime);
 #endif
 	// signed value is needed here
-	uint64_t timeToSwitch = nextSwitchTimeUs - getTimeNowUs();
+	int64_t timeToSwitch = nextSwitchTimeUs - getTimeNowUs();
 	if (timeToSwitch < 1) {
 		/**
 		 * We are here if we are late for a state transition.
 		 * At 12000RPM=200Hz with a 60 toothed wheel we need to change state every
 		 * 1000000 / 200 / 120 = ~41 uS. We are kind of OK.
+		 *
+		 * We are also here after a flash write. Flash write freezes the whole chip for a couple of seconds,
+		 * so PWM generation and trigger simulation generation would have to recover from this time lag.
 		 */
 		//todo: introduce error and test this error handling		warning(OBD_PCM_Processor_Fault, "PWM: negative switch time");
 		timeToSwitch = 10;
