8a916173c890884df25368e59328c5ec0abc3329
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-13 03:20:45 +0000

    avoiding conflict

--- trunk/firmware/config/engines/honda_accord.cpp
+++ trunk/firmware/config/engines/honda_accord.cpp
@@ -125,5 +125,7 @@ void setHondaAccordConfiguration(engine_configuration_s *engineConfiguration, bo
 	boardConfiguration->HD44780_db5 = GPIOE_13;
 	boardConfiguration->HD44780_db6 = GPIOE_15;
 	boardConfiguration->HD44780_db7 = GPIOB_10;
+
+	boardConfiguration->logicAnalyzerPins[1] = GPIO_NONE;
 }
 
