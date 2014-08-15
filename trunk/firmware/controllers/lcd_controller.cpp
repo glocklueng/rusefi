ea3dfd4860f396d04f3466a8c058fe29463447fe
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-15 02:43:30 +0000

    making progress

--- trunk/firmware/controllers/lcd_controller.cpp
+++ trunk/firmware/controllers/lcd_controller.cpp
@@ -10,14 +10,59 @@
 #include "lcd_HD44780.h"
 #include "efilib.h"
 #include "rpm_calculator.h"
-#include "thermistors.h"
+#include "allsensors.h"
 #include "engine.h"
 #include "rtc_helper.h"
 
 extern Engine engine;
+extern engine_configuration_s *engineConfiguration;
 
-static char buffer[10];
+char * appendStr(char *ptr, const char *suffix) {
+	for (uint32_t i = 0; i < strlen(suffix); i++) {
+		*ptr++ = suffix[i];
+	}
+	return ptr;
+}
+
+static void prepareCltIatTpsLine(char *buffer) {
+	char *ptr = buffer;
+	*ptr++ = 'C';
+
+	ptr = ftoa(ptr, getCoolantTemperature(), 10.0f);
+	ptr = appendStr(ptr, " C");
+	ptr = ftoa(ptr, getIntakeAirTemperature(), 10.0f);
+
+	ptr = appendStr(ptr, " TP");
+	ptr = itoa10(ptr, (int) getTPS());
+}
+
+static const char* algorithmStr[] = {"MAF", "TPS", "MAP", "SD"};
+
+static void prepareInfoLine(char *buffer) {
+	char *ptr = buffer;
+
+	ptr = appendStr(ptr, " ");
+	ptr = appendStr(ptr, algorithmStr[engineConfiguration->algorithm]);
+
+	ptr = appendStr(ptr, " ");
+
+	ptr = appendStr(ptr, " ");
+
+}
+
+static char buffer[24];
 static char dateBuffer[30];
+
+static void prepareCurrentSecondLine() {
+	switch (getTimeNowSeconds() % 2) {
+	case 0:
+		prepareCltIatTpsLine(buffer);
+		break;
+	case 1:
+		prepareInfoLine(buffer);
+		break;
+	}
+}
 
 void updateHD44780lcd(void) {
 
@@ -31,13 +76,14 @@ void updateHD44780lcd(void) {
 	for (int i = 0; i < 6 - len; i++) {
 		lcd_HD44780_print_char(' ');
 	}
-
 	lcd_HD44780_print_string(buffer);
 
-	lcd_HD44780_set_position(2, 0);
-	lcd_HD44780_print_char('C');
+	memset(buffer, ' ', 20);
 
-	ftoa(buffer, getCoolantTemperature(), 100.0f);
+	prepareCurrentSecondLine();
+	buffer[20] = 0;
+
+	lcd_HD44780_set_position(2, 0);
 	lcd_HD44780_print_string(buffer);
 
 #if EFI_PROD_CODE
