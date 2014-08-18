1e9c28f2ba6b03cbcdf7744c073667ea2649288c
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-18 11:39:46 +0000

    better error dialog

--- trunk/java_console/ui/src/com/rusefi/ui/DefaultExceptionHandler.java
+++ trunk/java_console/ui/src/com/rusefi/ui/DefaultExceptionHandler.java
@@ -20,12 +20,28 @@ public class DefaultExceptionHandler implements Thread.UncaughtExceptionHandler 
     }
 
     public static void handleException(Throwable e) {
-        // Here you should have a more robust, permanent record of problems
-        JOptionPane.showMessageDialog(findActiveFrame(), e.toString(), CONSOLE_VERSION + ": Exception Occurred", OK_OPTION);
-        e.printStackTrace();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
         e.printStackTrace(ps);
+
+        JPanel content = new JPanel(new BorderLayout());
+        content.add(new JLabel(e.toString()), BorderLayout.NORTH);
+
+        JTextArea textArea = new JTextArea(baos.toString());
+
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea) {
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(500, 400);
+            }
+        };
+
+        content.add(scrollPane, BorderLayout.CENTER);
+
+        JOptionPane.showConfirmDialog(findActiveFrame(), content, CONSOLE_VERSION + ": Exception Occurred", JOptionPane.DEFAULT_OPTION);
+        e.printStackTrace();
         FileLog.MAIN.logLine("handleException: " + baos.toString());
     }
 
