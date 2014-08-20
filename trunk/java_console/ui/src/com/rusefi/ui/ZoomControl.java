346e325fea290d3d49a72a411bf6d04b106d35e8
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-08-20 17:32:37 +0000

    refactoring

--- trunk/java_console/ui/src/com/rusefi/ui/ZoomControl.java
+++ trunk/java_console/ui/src/com/rusefi/ui/ZoomControl.java
@@ -11,10 +11,21 @@ import java.awt.event.ActionListener;
  * 7/7/13
  * (c) Andrey Belomutskiy
  */
-public class ZoomControl extends JPanel implements ZoomProvider {
+public class ZoomControl extends JPanel {
     private final JLabel currentValue = new JLabel();
     private double value;
     public ZoomControlListener listener = null;
+
+    ZoomProvider zoomProvider = new ZoomProvider() {
+        @Override
+        public double getZoomValue() {
+            return value;
+        }
+
+        public String toString() {
+            return "zoom " + value;
+        }
+    };
 
     public ZoomControl() {
         super(new FlowLayout());
@@ -68,9 +79,8 @@ public class ZoomControl extends JPanel implements ZoomProvider {
             listener.onZoomChange();
     }
 
-    @Override
-    public double getZoomValue() {
-        return value;
+    public ZoomProvider getZoomProvider() {
+        return zoomProvider;
     }
 
     interface ZoomControlListener {
