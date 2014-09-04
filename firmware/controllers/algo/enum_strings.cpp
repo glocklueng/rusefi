e00473b4fd4b4410f540cfd7d96a3294a938ae7a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-09-04 19:34:45 +0000

    making progress

---
+++ firmware/controllers/algo/enum_strings.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file enum_strings.cpp
+ *
+ * @date Sep 4, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "main.h"
+#include "enum_strings.h"
+#include "error_handling.h"
+
+
