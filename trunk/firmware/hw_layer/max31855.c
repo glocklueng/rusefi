450f035544fc8424a883cd0896adce870c0fc728
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-09-17 15:33:32 +0000

    making progress - EGT

---
+++ trunk/firmware/hw_layer/max31855.c
@@ -0,0 +1,13 @@
+/**
+ * @file max31855.c
+ * @brief MAX31855 Thermocouple-to-Digital Converter driver
+ *
+ * @date Sep 17, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "max31855.h"
+
+void initMax31855(board_configuration_s *boardConfiguration) {
+
+}
