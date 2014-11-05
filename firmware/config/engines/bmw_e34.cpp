32e53ab695d41f11745bc0daea32829da85ad587
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-11-05 19:40:21 +0000

    e34 config

--- firmware/config/engines/bmw_e34.cpp
+++ firmware/config/engines/bmw_e34.cpp
@@ -1,6 +1,8 @@
 /**
  * @file bmw_e34.cpp
  *
+ * set_engine_type 25
+ *
  * @date Nov 5, 2014
  * @author Andrey Belomutskiy, (c) 2012-2014
  */
@@ -13,5 +15,6 @@ void setBmwE43(engine_configuration_s *engineConfiguration) {
 	setOperationMode(engineConfiguration, FOUR_STROKE_CRANK_SENSOR);
 
 	engineConfiguration->triggerConfig.triggerType = TT_TOOTHED_WHEEL_60_2;
+	engineConfiguration->cylindersCount = 6;
 
 }
