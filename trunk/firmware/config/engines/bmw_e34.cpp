2b0ea3652a6539fd7fde4f9cdf7e286706e3fb13
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-11-05 21:23:42 +0000

    e34 config

--- trunk/firmware/config/engines/bmw_e34.cpp
+++ trunk/firmware/config/engines/bmw_e34.cpp
@@ -3,6 +3,8 @@
  *
  * set_engine_type 25
  *
+ * Coil pairs: 1-6, 2-5, 3-4
+ *
  * @date Nov 5, 2014
  * @author Andrey Belomutskiy, (c) 2012-2014
  */
@@ -16,5 +18,8 @@ void setBmwE43(engine_configuration_s *engineConfiguration) {
 
 	engineConfiguration->triggerConfig.triggerType = TT_TOOTHED_WHEEL_60_2;
 	engineConfiguration->cylindersCount = 6;
+	engineConfiguration->firingOrder = FO_1_THEN_5_THEN_3_THEN_6_THEN_2_THEN_4;
+	engineConfiguration->injectionMode = IM_BATCH;
+	engineConfiguration->ignitionMode = IM_WASTED_SPARK;
 
 }
