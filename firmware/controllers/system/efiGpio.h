5e03ee96bdff3994cbd6c9935a919b576c91508e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-11-24 03:23:41 +0000

    fixing build?

--- firmware/controllers/system/efiGpio.h
+++ firmware/controllers/system/efiGpio.h
@@ -93,13 +93,10 @@ const char *getPinName(io_pin_e io_pin);
     }
 #else
 		#define doSetOutputPinValue(pin, logicValue) {                                  \
-			if (outputs[(pin)].port != GPIO_NULL) {                                     \
-				efiAssertVoid(pinDefaultState[pin]!=NULL, "pin mode not initialized");  \
 				pin_output_mode_e mode = OM_DEFAULT;                                    \
 				OutputPin *output = &outputs[pin];                                      \
 				int eValue = getElectricalValue(logicValue, mode);                      \
 				setPinValue(output, eValue, logicValue);                                \
-			}                                                                           \
 		}
 #endif
 
