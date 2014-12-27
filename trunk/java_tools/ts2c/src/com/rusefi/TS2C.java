10e3c21f858565cf8ecb3bee5ac1925f6970acac
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-12-27 05:25:30 +0000

    looks to be ready

--- trunk/java_tools/ts2c/src/com/rusefi/TS2C.java
+++ trunk/java_tools/ts2c/src/com/rusefi/TS2C.java
@@ -1,9 +1,6 @@
 package com.rusefi;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 
 /**
@@ -16,6 +13,7 @@ public class TS2C {
 
     static float loadBins[] = new float[SIZE];
     static float rpmBins[] = new float[SIZE];
+    static float table[][] = new float[SIZE][];
 
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new FileReader(INPUT_NAME));
@@ -23,7 +21,53 @@ public class TS2C {
         readAxle(loadBins, r);
         readAxle(rpmBins, r);
 
+        for (int i = 0; i < SIZE; i++) {
+            table[i] = new float[SIZE];
+        }
 
+        readTable(table, r);
+
+        BufferedWriter w = new BufferedWriter(new FileWriter("output.c"));
+
+        for (int i = 0; i < SIZE; i++)
+            writeLine(table[i], w, i);
+        w.close();
+    }
+
+    private static void writeLine(float[] floats, BufferedWriter w, int index) throws IOException {
+        StringBuilder sb = new StringBuilder("{");
+
+
+        sb.append("/* " + index + " " + String.format("%3.3f", loadBins[index]) + "\t*/");
+        for (int i = 0; i < SIZE; i++) {
+            sb.append("/* " + i + " " + rpmBins[i] + "*/" + String.format("%3.3f", floats[i]) + ",\t");
+        }
+        sb.append("},\r\n");
+
+        w.write(sb.toString());
+    }
+
+    private static void readTable(float[][] table, BufferedReader r) throws IOException {
+        int index = 0;
+
+        while (index < SIZE) {
+            String line = r.readLine();
+            if (line == null)
+                throw new IOException("End of file?");
+            line = line.trim();
+            if (line.isEmpty())
+                continue;
+
+            String[] values = line.split(" ");
+            if (values.length != SIZE)
+                throw new IllegalStateException("Unexpected line: " + line);
+
+            for (int i = 0; i < SIZE; i++) {
+                table[index][i] = Float.parseFloat(values[i]);
+            }
+            System.out.println("Got line " + index + ": " + Arrays.toString(table[index]));
+            index++;
+        }
     }
 
     private static void readAxle(float[] bins, BufferedReader r) throws IOException {
@@ -37,11 +81,8 @@ public class TS2C {
             if (line.isEmpty())
                 continue;
             bins[index++] = Float.parseFloat(line);
-
-
         }
 
         System.out.println("Got bins " + Arrays.toString(bins));
-
     }
 }
