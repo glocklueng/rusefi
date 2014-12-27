9c8ca3b30ad52f9b97384e672d287bccc9ea00a0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2014-12-27 20:21:01 +0000

    refactoring

--- firmware/controllers/core/fsio_impl.h
+++ firmware/controllers/core/fsio_impl.h
@@ -33,4 +33,7 @@ float getLEValue(Engine *engine, calc_stack_t *s, le_action_e action);
 void setFsio(engine_configuration_s *engineConfiguration, int index, brain_pin_e pin, const char * exp);
 void setFsioExt(engine_configuration_s *engineConfiguration, int index, brain_pin_e pin, const char * exp, int freq);
 
+void initFsioImpl(Engine *engine);
+void applyFsioConfiguration(DECLARE_ENGINE_PARAMETER_F);
+
 #endif /* LE_FUNCTIONS_H_ */
