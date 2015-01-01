94129eb8d5b47f7c617c6e157edb75bb2eb48dfa
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-01 20:22:03 +0000

    better folder structure

--- firmware/development/development.mk
+++ firmware/development/development.mk
@@ -7,5 +7,6 @@ DEV_SRC_CPP = development/trigger_emulator.cpp \
 	development/analog_chart.cpp \
 	development/rfi_perftest.cpp \
 	development/engine_emulator.cpp \
+	$(PROJECT_DIR)/development/wave_chart.cpp \
 	development/wave_analyzer.cpp
 	
