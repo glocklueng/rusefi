b5baed91bccd9a8e9542facdfa114cf543c0e311
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-02 22:56:56 +0000

    refactoring

--- trunk/firmware/hw_layer/hardware.cpp
+++ trunk/firmware/hw_layer/hardware.cpp
@@ -58,13 +58,10 @@ static bool isSpiInitialized[5] = { false, false, false, false, false };
 static void initSpiModule(SPIDriver *driver, brain_pin_e sck, brain_pin_e miso,
 		brain_pin_e mosi, int af) {
 
-	mySetPadMode("SPI clock", getHwPort(sck), getHwPin(sck),
-			PAL_MODE_ALTERNATE(af));
+	mySetPadMode2("SPI clock", sck,	PAL_MODE_ALTERNATE(af));
 
-	mySetPadMode("SPI master out", getHwPort(mosi), getHwPin(mosi),
-			PAL_MODE_ALTERNATE(af));
-	mySetPadMode("SPI master in ", getHwPort(miso), getHwPin(miso),
-			PAL_MODE_ALTERNATE(af));
+	mySetPadMode2("SPI master out", mosi, PAL_MODE_ALTERNATE(af));
+	mySetPadMode2("SPI master in ", miso, PAL_MODE_ALTERNATE(af));
 }
 
 /**
