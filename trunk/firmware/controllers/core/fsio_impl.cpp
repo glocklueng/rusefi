40fe87da44fc3607cafbe7be5bb5671293e47cf2
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-13 16:51:18 +0000

    refactoring

--- trunk/firmware/controllers/core/fsio_impl.cpp
+++ trunk/firmware/controllers/core/fsio_impl.cpp
@@ -149,7 +149,6 @@ static SimplePwm fsioPwm[LE_COMMAND_COUNT] CCM_OPTIONAL;
 
 static LECalculator calc;
 extern LEElement * fsioLogics[LE_COMMAND_COUNT];
-extern NamedOutputPin outputs[IO_PIN_COUNT];
 
 // that's crazy, but what's an alternative? we need const char *, a shared buffer would not work for pin repository
 static const char *getGpioPinName(int index) {
