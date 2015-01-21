97b97aa5c7ef8ead369b86d7b97649fdbeb5ba7e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-21 22:27:08 +0000

    https://sourceforge.net/p/rusefi/tickets/125/

--- java_tools/configuration_definition/src/com/rusefi/ConfigField.java
+++ java_tools/configuration_definition/src/com/rusefi/ConfigField.java
@@ -1,5 +1,8 @@
 package com.rusefi;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * (c) Andrey Belomutskiy
  * 1/15/15
@@ -18,20 +21,30 @@ public class ConfigField {
     }
 
     public static ConfigField parse(String line) {
-        String[] a = line.split(";");
-        if (a.length != 2 && a.length != 3) {
-            System.err.println("Tow or three semicolon-separated elements expected");
-            return null;
-        }
+        String typePattern = "[\\w\\d\\s_]+";
+        String namePattern = "[[\\w\\d\\s_]]+";
+        String commentPattern = "\\;([\\w\\d\\s_]*)";
 
-        String name = a[1];
-        String comment = a.length == 3 ? a[2] : "";
+        Pattern FIELD = Pattern.compile("(" + typePattern + ");(" + namePattern + ")(" + commentPattern + ")?");
+        Matcher matcher = FIELD.matcher(line);
+        if (!matcher.matches())
+            return null;
+
+
+        String[] b = line.split(";");
+//        if (a.length != 2 && a.length != 3) {
+//            System.err.println("Tow or three semicolon-separated elements expected");
+//            return null;
+//        }
+
+        String name = matcher.group(2);
+        String comment = matcher.group(4);
         ConfigField field = new ConfigField(name, comment);
         int arraySize;
 
-        String type = a[0];
+        String type = matcher.group(1);
         if (type.startsWith("array ")) {
-            a = type.split(" ");
+            String[] a = type.split(" ");
             type = a[1];
             field.arraySizeAsText = a[2];
             arraySize = ConfigDefinition.getSize(field.arraySizeAsText);
