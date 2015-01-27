0f11b8bfa39a836850098f1aa61de96ea28a8508
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-27 21:30:41 +0000

    https://sourceforge.net/p/rusefi/tickets/125/

--- firmware/controllers/algo/engine_configuration_generated_structures.h
+++ firmware/controllers/algo/engine_configuration_generated_structures.h
@@ -72,6 +72,56 @@ typedef struct {
 } MAP_sensor_config_s;
 
 /**
+ * @brief Thermistor curve parameters
+
+ */
+typedef struct {
+	/**
+	 * these values are in Celcuus
+	 * offset 0
+	 */
+	float tempC_1;
+	/**
+	 * offset 4
+	 */
+	float tempC_2;
+	/**
+	 * offset 8
+	 */
+	float tempC_3;
+	/**
+	 * offset 12
+	 */
+	float resistance_1;
+	/**
+	 * offset 16
+	 */
+	float resistance_2;
+	/**
+	 * offset 20
+	 */
+	float resistance_3;
+	/**
+	 * offset 24
+	 */
+	float bias_resistor;
+	/**
+	 * offset 28
+	 */
+	float s_h_a;
+	/**
+	 * offset 32
+	 */
+	float s_h_b;
+	/**
+	 * offset 36
+	 */
+	float s_h_c;
+	/** total size 40*/
+} ThermistorConf;
+
+
+/**
  * @brief Trigger wheel(s) configuration
 
  */
