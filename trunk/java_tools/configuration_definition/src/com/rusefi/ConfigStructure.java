5963793bc4e5f2cf898dd215707aa99e6437dacf
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-01-30 21:33:18 +0000

    https://sourceforge.net/p/rusefi/tickets/125/

--- trunk/java_tools/configuration_definition/src/com/rusefi/ConfigStructure.java
+++ trunk/java_tools/configuration_definition/src/com/rusefi/ConfigStructure.java
@@ -1,7 +1,7 @@
 package com.rusefi;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,51 +30,6 @@ public class ConfigStructure {
         this.withPrefix = withPrefix;
     }
 
-    /**
-     * This method writes a C header version of a data structure
-     */
-    public void write(BufferedWriter cHeader) throws IOException {
-        if (comment != null) {
-            cHeader.write("/**\r\n" + ConfigDefinition.packComment(comment, "") + "\r\n*/\r\n");
-        }
-        cHeader.write("typedef struct {\r\n");
-
-        bitIndex = 0;
-        for (int i = 0; i < cFields.size(); i++) {
-            ConfigField cf = cFields.get(i);
-            cHeader.write(cf.getText(currentOffset, bitIndex));
-            ConfigField next = i == cFields.size() - 1 ? ConfigField.VOID : cFields.get(i + 1);
-            incrementBitIndex(cf, next);
-            currentOffset += cf.getSize(next);
-        }
-
-        cHeader.write("\t/** total size " + currentOffset + "*/\r\n");
-        cHeader.write("} " + name + ";\r\n\r\n");
-    }
-
-    private void incrementBitIndex(ConfigField cf, ConfigField next) {
-        if (!cf.isBit) {
-            bitIndex = 0;
-            return;
-        }
-        bitIndex++;
-        if (bitIndex == 32)
-            throw new IllegalStateException("todo: too many bits, not supported");
-    }
-
-    public void addBoth(ConfigField cf) {
-        cFields.add(cf);
-        tsFields.add(cf);
-    }
-
-    public void addC(ConfigField cf) {
-        cFields.add(cf);
-    }
-
-    public void addTs(ConfigField cf) {
-        tsFields.add(cf);
-    }
-
     public void addAlignmentFill() {
         bitIndex = 0;
         /**
@@ -99,12 +54,61 @@ public class ConfigStructure {
         totalSize += fillSize;
     }
 
-    public int writeTunerStudio(String prefix, BufferedWriter tsHeader, int tsPosition) throws IOException {
+    /**
+     * This method writes a C header version of a data structure
+     */
+    public void headerWrite(Writer cHeader) throws IOException {
+        if (comment != null) {
+            cHeader.write("/**\r\n" + ConfigDefinition.packComment(comment, "") + "\r\n*/\r\n");
+        }
+        cHeader.write("typedef struct {\r\n");
+
+        bitIndex = 0;
+        for (int i = 0; i < cFields.size(); i++) {
+            ConfigField cf = cFields.get(i);
+            cHeader.write(cf.getHeaderText(currentOffset, bitIndex));
+            ConfigField next = i == cFields.size() - 1 ? ConfigField.VOID : cFields.get(i + 1);
+
+            incrementBitIndex(cf, next);
+            currentOffset += cf.getSize(next);
+        }
+
+        cHeader.write("\t/** total size " + currentOffset + "*/\r\n");
+        cHeader.write("} " + name + ";\r\n\r\n");
+    }
+
+    public int writeTunerStudio(String prefix, Writer tsHeader, int tsPosition) throws IOException {
+        bitIndex = 0;
         for (int i = 0; i < tsFields.size(); i++) {
             ConfigField next = i == tsFields.size() - 1 ? ConfigField.VOID : tsFields.get(i + 1);
             ConfigField cf = tsFields.get(i);
-            tsPosition = cf.writeTunerStudio(prefix, tsHeader, tsPosition, next);
+            tsPosition = cf.writeTunerStudio(prefix, tsHeader, tsPosition, next, bitIndex);
+
+            incrementBitIndex(cf, next);
         }
         return tsPosition;
+    }
+
+    private void incrementBitIndex(ConfigField cf, ConfigField next) {
+        if (!cf.isBit) {
+            bitIndex = 0;
+            return;
+        }
+        bitIndex++;
+        if (bitIndex == 32)
+            throw new IllegalStateException("todo: too many bits, not supported");
+    }
+
+    public void addBoth(ConfigField cf) {
+        cFields.add(cf);
+        tsFields.add(cf);
+    }
+
+    public void addC(ConfigField cf) {
+        cFields.add(cf);
+    }
+
+    public void addTs(ConfigField cf) {
+        tsFields.add(cf);
     }
 }
