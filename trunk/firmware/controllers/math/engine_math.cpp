f32d3cb547db9876ff4aaaaffcde2f7f86f465b0
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-12 21:41:51 +0000

    making progress with injection phase

--- trunk/firmware/controllers/math/engine_math.cpp
+++ trunk/firmware/controllers/math/engine_math.cpp
@@ -157,7 +157,7 @@ void FuelSchedule::addFuelEvents(OutputSignalList *sourceList, injection_mode_e 
 
 	events.reset();
 
-	float baseAngle = engineConfiguration->injectionAngle;
+	float baseAngle = engineConfiguration->injectionAngle - MS2US(engine->fuelMs) / engine->rpmCalculator.oneDegreeUs;
 
 	switch (mode) {
 	case IM_SEQUENTIAL:
