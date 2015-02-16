1979ed03569ee4631b8f213269be2ab8e6862ca6
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-16 20:24:12 +0000

    making progress!

--- java_console/models/src/com/rusefi/config/Field.java
+++ java_console/models/src/com/rusefi/config/Field.java
@@ -1,6 +1,12 @@
 package com.rusefi.config;
 
+import com.rusefi.core.Pair;
+
+import java.util.Objects;
+
 public class Field {
+    private static final String INT_VALUE_PREFIX = "int @";
+
     private final int offset;
 
     public Field(int offset) {
@@ -9,5 +15,26 @@ public class Field {
 
     public int getOffset() {
         return offset;
+    }
+
+    public static boolean isIntValueMessage(String message) {
+        return message.startsWith(INT_VALUE_PREFIX);
+    }
+
+    public static Pair<Integer, ?> parseResponse(String message) {
+        try {
+            if (isIntValueMessage(message)) {
+                message = message.substring(INT_VALUE_PREFIX.length());
+                String[] a = message.split(" is ");
+                if (a.length != 2)
+                    return null;
+                int index = Integer.parseInt(a[0]);
+                int value = Integer.parseInt(a[1]);
+                return new Pair<>(index, value);
+            }
+            return null;
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
