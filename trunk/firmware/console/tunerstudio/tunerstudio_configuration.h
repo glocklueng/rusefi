31d6edf5f90f94f7c03f064ebc1980942377b32b
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-17 04:08:59 +0000

    space for future values

--- trunk/firmware/console/tunerstudio/tunerstudio_configuration.h
+++ trunk/firmware/console/tunerstudio/tunerstudio_configuration.h
@@ -16,10 +16,10 @@
 /**
  * this is used to confirm that firmware and TunerStudio are using the same rusefi.ini version
  */
-#define TS_FILE_VERSION 20150211
+#define TS_FILE_VERSION 20150216
 
 #define PAGE_0_SIZE 15160
-#define TS_OUTPUT_SIZE 116
+#define TS_OUTPUT_SIZE 196
 
 typedef struct {
 	uint16_t values[EGT_CHANNEL_COUNT];
@@ -83,7 +83,7 @@ typedef struct {
 	float rpmAcceleration;
 	float massAirFlowValue;
 	float veValue;
-	//int unused3[1];
+	int unused3[20];
 } TunerStudioOutputChannels;
 
 #endif /* TUNERSTUDIO_CONFIGURATION_H_ */
