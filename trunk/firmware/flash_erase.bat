2a4a9e083c5f70b2024c01642bbdceb7e4ee8a3e
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-23 03:38:39 +0000

    good tools matter!

--- trunk/firmware/flash_erase.bat
+++ trunk/firmware/flash_erase.bat
@@ -1,1 +1,2 @@
-st-link_cli -c SWD ur -ME
+rem st-link_cli -c SWD ur -ME
+openocd-0.8.0.exe -f interface/stlink-v2.cfg -f board/stm32f4discovery.cfg -c init -c targets -c "halt" -c "flash erase_address 0x08000000 0x040000" -c shutdown
