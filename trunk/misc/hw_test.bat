a3cd2d5d0d88d0a4dbdc514ef881d41f30378691
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-23 03:19:49 +0000

    cool stuff!

--- trunk/misc/hw_test.bat
+++ trunk/misc/hw_test.bat
@@ -1,9 +1,23 @@
 
 cd ../firmware
+echo Compiling
 pwd
+rm -rf .dep/
+
+rm -rf build\rusefi.bin
+make
+
+
+cd build
+arm-none-eabi-size  --format=berkeley "rusefi.elf"
+echo Flashing
+pwd
+call ../flash_openocd
+cd ..
 
 
 cd ..
+echo Running some commands
 pwd
 
 java -cp java_console_binary\rusefi_console.jar com.rusefi.CmdLine "set_led_blinking_period 10"
