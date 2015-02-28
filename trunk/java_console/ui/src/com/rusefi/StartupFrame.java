003c2b29c7729a11e3b4ac84c0f0ddc2775c1f9a
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-02-28 22:12:20 +0000

    refactoring

--- trunk/java_console/ui/src/com/rusefi/StartupFrame.java
+++ trunk/java_console/ui/src/com/rusefi/StartupFrame.java
@@ -3,6 +3,7 @@ package com.rusefi;
 import com.rusefi.io.LinkManager;
 import com.rusefi.io.tcp.TcpConnector;
 import com.rusefi.maintenance.FirmwareFlasher;
+import com.rusefi.maintenance.ProcessStatusWindow;
 import com.rusefi.ui.util.HorizontalLine;
 import com.rusefi.ui.util.UiUtils;
 import com.rusefi.ui.util.URLLabel;
@@ -87,7 +88,7 @@ public class StartupFrame {
 
         leftPanel.add(SimulatorHelper.createSimulatorComponent(this));
 
-        if (FirmwareFlasher.isWindows()) {
+        if (ProcessStatusWindow.isWindows()) {
             leftPanel.add(new HorizontalLine());
             leftPanel.add(FirmwareFlasher.getContent());
         }
