ef74e79d71200d135f24b0916d4ae537c5ea84ff
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-03-07 00:50:20 +0000

    now with CRC response

--- trunk/java_console/io/src/com/rusefi/binaryprotocol/BinaryProtocolCmd.java
+++ trunk/java_console/io/src/com/rusefi/binaryprotocol/BinaryProtocolCmd.java
@@ -1,17 +1,24 @@
 package com.rusefi.binaryprotocol;
 
+import com.rusefi.io.DataListener;
 import com.rusefi.io.serial.PortHolder;
+import com.rusefi.io.serial.SerialPortReader;
+import etch.util.CircularByteBuffer;
 import jssc.SerialPort;
 import jssc.SerialPortException;
+
+import java.io.EOFException;
+import java.util.Arrays;
 
 /**
  * (c) Andrey Belomutskiy
  * 3/6/2015
  */
 public class BinaryProtocolCmd {
+    private static final int BUFFER_SIZE = 10000;
     private static SerialPort serialPort;
 
-    public static void main(String[] args) throws SerialPortException {
+    public static void main(String[] args) throws SerialPortException, InterruptedException, EOFException {
         if (args.length != 1) {
             System.out.println("Exactly one parameter expected");
             return;
@@ -19,42 +26,59 @@ public class BinaryProtocolCmd {
 
         String port = args[0];
 
+
         serialPort = new SerialPort(port);
         boolean opened = serialPort.openPort();
         if (!opened) {
             System.out.println("failed to open " + port);
         }
+        BinaryProtocol bp = new BinaryProtocol(serialPort);
+
+
+        final CircularByteBuffer cbb = new CircularByteBuffer(BUFFER_SIZE);
+        DataListener listener = new DataListener() {
+            @Override
+            public void onDataArrived(byte[] freshData) {
+                System.out.println(freshData.length + " byte(s) arrived");
+                synchronized (cbb) {
+                    if (cbb.size() - cbb.length() < freshData.length) {
+                        System.out.println("buffer overflow not expected");
+                        cbb.clear();
+                    }
+                    cbb.put(freshData);
+                    cbb.notifyAll();
+                }
+            }
+        };
+        serialPort.addEventListener(new SerialPortReader(serialPort, listener));
         PortHolder.setupPort(serialPort, 38400);
         System.out.println("Looks good");
-        sendQueryCommand();
+        bp.sendQueryCommand();
 
-    }
 
-    private static void sendQueryCommand() throws SerialPortException {
-        byte[] command = {'S'};
-        sendCrc(command);
-    }
+        synchronized (cbb) {
+            BinaryProtocol.waitForBytes(cbb, 2);
 
-    private static void sendCrc(byte[] command) throws SerialPortException {
-        byte[] packet = makePacket(command);
+            int packetSize = BinaryProtocol.swap16(cbb.getShort());
+            System.out.println("Got packet size " + packetSize);
+            BinaryProtocol.waitForBytes(cbb, packetSize + 4);
+            byte[] packet = new byte[packetSize];
+            int packetCrc;
+            synchronized (cbb) {
+                cbb.get(packet);
+                packetCrc = BinaryProtocol.swap32(cbb.getInt());
+            }
+            int actualCrc = CRC.crc32(packet);
 
-        serialPort.writeBytes(packet);
-    }
+            boolean isCrcOk = actualCrc == packetCrc;
+            if(!isCrcOk) {
+                System.out.println(String.format("%x", actualCrc) + " vs " + String.format("%x", packetCrc));
+            }
 
-    public static byte[] makePacket(byte[] command) {
-        byte[] packet = new byte[command.length + 6];
-
-        packet[0] = (byte) (command.length / 256);
-        packet[1] = (byte) command.length;
-
-        int index = packet.length - 1;
-        System.arraycopy(command, 0, packet, 2, command.length);
-        int crc = CRC.crc32(command, command.length);
-
-        for (int i = 0; i < 4; i++) {
-            packet[index--] = (byte) crc;
-            crc >>= 8;
+            System.out.println("packet " + Arrays.toString(packet) + ": crc ok=" + isCrcOk);
         }
-        return packet;
+
+
     }
+
 }
