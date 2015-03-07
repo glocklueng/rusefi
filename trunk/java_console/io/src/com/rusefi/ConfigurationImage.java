258583fedfd1b6dc2de6aa455676a7a2ab997c4f
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-03-07 01:48:57 +0000

    save to file

--- trunk/java_console/io/src/com/rusefi/ConfigurationImage.java
+++ trunk/java_console/io/src/com/rusefi/ConfigurationImage.java
@@ -1,10 +1,14 @@
 package com.rusefi;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * (c) Andrey Belomutskiy
  * 3/6/2015
  */
 public class ConfigurationImage {
+    private final static String BIN_HEADER = "RUSEFI0.1";
     private final int size;
     private byte content[];
 
@@ -15,5 +19,16 @@ public class ConfigurationImage {
 
     public int getSize() {
         return size;
+    }
+
+    public void saveToFile(String fileName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        byte[] bytes = BIN_HEADER.getBytes();
+        if (bytes.length != BIN_HEADER.length())
+            throw new IllegalStateException("Encoding issue");
+        fos.write(bytes);
+        fos.write(content);
+        fos.close();
+        System.out.println("Saved to " + fileName);
     }
 }
