29c50692988b1bb3de1de95cd87a1dfceb6504d7
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-03-08 21:16:53 +0000

    refactoring

--- firmware/console/console_io.h
+++ firmware/console/console_io.h
@@ -24,11 +24,6 @@
 #define GET_CONSOLE_MODE_VALUE() palReadPad(CONSOLE_MODE_SWITCH_PORT, CONSOLE_MODE_SWITCH_PIN)
 #define SHOULD_INGORE_FLASH() (palReadPad(CONFIG_RESET_SWITCH_PORT, CONFIG_RESET_SWITCH_PIN) == 0)
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
-
 SerialDriver * getConsoleChannel(void);
 
 void consolePutChar(int x);
@@ -41,6 +36,11 @@ bool isConsoleReady(void);
 #else
 #define isConsoleReady() true
 #endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
 #ifdef __cplusplus
 }
