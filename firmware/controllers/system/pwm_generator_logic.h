00519605df091e13be1caf4ebc1e07b00ecee826
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-03-22 03:35:55 +0000

    doc

--- firmware/controllers/system/pwm_generator_logic.h
+++ firmware/controllers/system/pwm_generator_logic.h
@@ -79,7 +79,7 @@ public:
 	pwm_cycle_callback *cycleCallback;
 
 	/**
-	 * this main callback is invoked when it's time to switch level on amy of the output channels
+	 * this main callback is invoked when it's time to switch level on any of the output channels
 	 */
 	pwm_gen_callback *stateChangeCallback;
 };
