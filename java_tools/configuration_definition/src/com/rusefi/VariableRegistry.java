8e8cfe6b92220a4643a7115a4d5d9adfe3f309b8
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-03-30 18:23:10 +0000

    making progress

---
+++ java_tools/configuration_definition/src/com/rusefi/VariableRegistry.java
@@ -0,0 +1,38 @@
+package com.rusefi;
+
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 3/30/2015
+ */
+public class VariableRegistry extends TreeMap<String, String> {
+    public static final VariableRegistry INSTANCE = new VariableRegistry();
+
+    private final Pattern VAR = Pattern.compile("(@@(.*?)@@)");
+
+    private VariableRegistry() {
+    }
+
+    public String processLine(String line) {
+        Matcher m;
+        while ((m = VAR.matcher(line)).find()) {
+            String key = m.group(2);
+
+
+       //     key =
+
+            if (!containsKey(key))
+                throw new IllegalStateException("No such variable: " + key);
+            String s = get(key);
+            line = m.replaceFirst(s);
+        }
+        return line;
+    }
+
+    public void register(String var, int i) {
+        put(var, Integer.toString(i));
+        put(var + "_hex", Integer.toString(i, 16));
+    }
+}
