8cfd2053851d8b9588c327de23fea10968b64f75
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-04-06 22:11:26 +0000

    improving usability

--- trunk/firmware/controllers/trigger/rpm_calculator.h
+++ trunk/firmware/controllers/trigger/rpm_calculator.h
@@ -16,9 +16,12 @@
 
 #define WC_DOWN "d"
 #define WC_UP "u"
-#define CRANK1 "c1"
-#define CRANK2 "c2"
-#define CRANK3 "c3"
+/**
+ * See also 'CRANK1' in java dev console code
+ */
+#define CRANK1 "t1"
+#define CRANK2 "t2"
+#define CRANK3 "t3"
 
 #define NOISY_RPM -1
 #define UNREALISTIC_RPM 30000
