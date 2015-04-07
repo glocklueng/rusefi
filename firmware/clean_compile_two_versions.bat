7d9423c519dce0bd080082ea90e5d5385ce0ce6f
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-04-07 22:12:46 +0000

    https://sourceforge.net/p/rusefi/tickets/171/

--- firmware/clean_compile_two_versions.bat
+++ firmware/clean_compile_two_versions.bat
@@ -5,9 +5,11 @@ mkdir deliver
 call clean.bat
 make
 cp build\rusefi.elf deliver\rusefi_debug.elf
+cp build\rusefi.bin deliver\rusefi_debug.bin
 
 call clean.bat
 make EXTRA_PARAMS='-DEFI_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_TRACE=FALSE -DCH_DBG_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_STACK_CHECK=FALSE -DCH_DBG_FILL_THREADS=FALSE -DCH_DBG_THREADS_PROFILING=FALSE'
 
 cp build\rusefi.elf deliver\rusefi_release.elf
+cp build\rusefi.bin deliver\rusefi_release.bin
 
