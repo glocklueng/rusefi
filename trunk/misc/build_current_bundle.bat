e7f7079fe226a8968389f4922e22cee36aaec426
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-04-13 21:07:58 +0000

    fixing path

--- trunk/misc/build_current_bundle.bat
+++ trunk/misc/build_current_bundle.bat
@@ -75,7 +75,7 @@ cp misc/console_launcher/rusefi_console.exe %folder%
 
 cd temp
 zip -r rusefi_bundle.zip *
-zip rusefi_console.zip %folder%/rusefi_console.jar %folder%/rusefi.xml
+zip rusefi_console.zip ../%folder%/rusefi_console.jar ../%folder%/rusefi.xml
 cd ..                                                             
 
 date
