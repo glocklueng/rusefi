2b270f019c62f23e6c2f6f04412b0bb91a8698bf
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-04-27 15:25:25 +0000

    https://sourceforge.net/p/rusefi/tickets/189/

--- trunk/firmware/console/status_loop.cpp
+++ trunk/firmware/console/status_loop.cpp
@@ -476,6 +476,11 @@ static void setBlinkingPeriod(int value) {
 
 extern efitick_t lastDecodingErrorTime;
 
+static bool_t isTriggerErrorNow() {
+	bool_t justHadError = (getTimeNowNt() - lastDecodingErrorTime) < US2NT(2 * 1000 * 3 * blinkingPeriod);
+	return justHadError || isTriggerDecoderError();
+}
+
 /**
  * this thread has a lower-then-usual stack size so we cannot afford *print* methods here
  */
@@ -500,8 +505,7 @@ static void blinkingThread(void *arg) {
 
 		communicationPin.setValue(1);
 #if EFI_ENGINE_CONTROL || defined(__DOXYGEN__)
-		bool_t justHadError = (getTimeNowNt() - lastDecodingErrorTime) < US2NT(2 * 1000 * delayMs);
-		if (justHadError || isTriggerDecoderError() || isIgnitionTimingError())
+		if (isTriggerErrorNow() || isIgnitionTimingError())
 			warningPin.setValue(1);
 #endif
 		chThdSleepMilliseconds(delayMs);
@@ -563,6 +567,8 @@ void updateTunerStudioState(TunerStudioOutputChannels *tsOutputChannels DECLARE_
 	tsOutputChannels->currentMapAccelDelta = engine->mapAccelEnrichment.getMapEnrichment(PASS_ENGINE_PARAMETER_F) * 100 / getMap();
 	tsOutputChannels->tpsAccelFuel = engine->tpsAccelEnrichment.getTpsEnrichment(PASS_ENGINE_PARAMETER_F);
 	tsOutputChannels->deltaTps = engine->tpsAccelEnrichment.getDelta();
+	tsOutputChannels->triggerErrorsCounter = triggerCentral.triggerState.totalTriggerErrorCounter;
+	tsOutputChannels->isTriggerError = isTriggerErrorNow();
 
 
 	tsOutputChannels->checkEngine = hasErrorCodes();
