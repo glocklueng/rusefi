276a8025cf409f6b029cb1cad121ed2b24c5fc92
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-04-29 12:37:32 +0000

    https://sourceforge.net/p/rusefi/tickets/192/

--- firmware/controllers/trigger/main_trigger_callback.cpp
+++ firmware/controllers/trigger/main_trigger_callback.cpp
@@ -435,7 +435,7 @@ void MainTriggerCallback::init(Engine *engine) {
 static void showMainInfo(Engine *engine) {
 #if EFI_PROD_CODE
 	int rpm = engine->rpmCalculator.rpm(PASS_ENGINE_PARAMETER_F);
-	float el = getEngineLoadT(PASS_ENGINE_PARAMETER);
+	float el = getEngineLoadT(PASS_ENGINE_PARAMETER_F);
 	scheduleMsg(logger, "rpm %d engine_load %f", rpm, el);
 	scheduleMsg(logger, "fuel %fms timing %f", getFuelMs(rpm PASS_ENGINE_PARAMETER),
 			getAdvance(rpm, el PASS_ENGINE_PARAMETER));
