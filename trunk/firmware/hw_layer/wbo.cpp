00fb19c6791a9009d6d9344e68acdf75f104d352
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-12 01:05:01 +0000

    https://sourceforge.net/p/rusefi/tickets/208/

---
+++ trunk/firmware/hw_layer/wbo.cpp
@@ -0,0 +1,10 @@
+/*
+ * @file	wbo.cpp
+ *
+ * @date Jun 11, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2015
+ */
+
+#include "main.h"
+
+
---
+++ trunk/firmware/hw_layer/wbo.h
@@ -0,0 +1,15 @@
+/*
+ * @file	wbo.h
+ *
+ * @date Jun 11, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2015
+ */
+
+#ifndef HW_LAYER_WBO_H_
+#define HW_LAYER_WBO_H_
+
+
+
+
+
+#endif /* HW_LAYER_WBO_H_ */
