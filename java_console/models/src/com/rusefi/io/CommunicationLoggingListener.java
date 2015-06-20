2924898b0f0a3f10688a31ee1b0606093edc8d3d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-20 15:08:34 +0000

    refactoring

--- java_console/models/src/com/rusefi/io/CommunicationLoggingListener.java
+++ java_console/models/src/com/rusefi/io/CommunicationLoggingListener.java
@@ -1,11 +1,11 @@
-package com.rusefi.io.serial;
+package com.rusefi.io;
 
 /**
  * (c) Andrey Belomutskiy
  * 9/4/14
  */
-public interface PortHolderListener {
-    PortHolderListener VOID = new PortHolderListener() {
+public interface CommunicationLoggingListener {
+    CommunicationLoggingListener VOID = new CommunicationLoggingListener() {
         @Override
         public void onPortHolderMessage(Class clazz, String message) {
         }
