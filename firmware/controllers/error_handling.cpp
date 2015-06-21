94cc4945059a2c39c0d89dd09cb3a6d1619aacc7
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-21 01:07:05 +0000

    cleanup

--- firmware/controllers/error_handling.cpp
+++ firmware/controllers/error_handling.cpp
@@ -57,6 +57,8 @@ static MemoryStream warningStream;
  * @returns TRUE in case there are too many warnings
  */
 int warning(obd_code_e code, const char *fmt, ...) {
+        UNUSED(code);
+  
 	int now = getTimeNowSeconds();
 	if (absI(now - timeOfPreviousWarning) < 10 || !warningEnabled)
 		return true; // we just had another warning, let's not spam
