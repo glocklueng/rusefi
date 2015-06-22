554e60430e9608229b63fb51e3d98037ce5f17b8
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-22 23:20:26 +0000

    vw

--- trunk/firmware/config/engines/vw.cpp
+++ trunk/firmware/config/engines/vw.cpp
@@ -20,7 +20,7 @@ void setVwAba(DECLARE_ENGINE_PARAMETER_F) {
 //	engineConfiguration->trigger.type = TT_TOOTHED_WHEEL_60_2;
 	engineConfiguration->trigger.type = TT_60_2_VW;
 
-	engineConfiguration->mafAdcChannel = EFI_ADC_0;
+	engineConfiguration->mafAdcChannel = EFI_ADC_1;
 
 
 	//Base engine setting
