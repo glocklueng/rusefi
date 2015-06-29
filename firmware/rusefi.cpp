05ef8150fae9dcd2abd74fce8203bd9dfab44020
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-29 11:17:51 +0000

    doc

--- firmware/rusefi.cpp
+++ firmware/rusefi.cpp
@@ -282,7 +282,7 @@ void firmwareError(const char *errorMsg, ...) {
 	}
 }
 
-static char UNUSED_RAM_SIZE[300];
+static char UNUSED_RAM_SIZE[200];
 
 static char UNUSED_CCM_SIZE[3600] CCM_OPTIONAL;
 
