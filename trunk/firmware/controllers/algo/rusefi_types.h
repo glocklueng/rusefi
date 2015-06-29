e1cf8a322af167d7be339ca55ab09b56137f372d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-06-29 14:54:19 +0000

    https://sourceforge.net/p/rusefi/tickets/200/ hope this is the fix

--- trunk/firmware/controllers/algo/rusefi_types.h
+++ trunk/firmware/controllers/algo/rusefi_types.h
@@ -19,7 +19,12 @@
  */
 typedef uint32_t efitimems_t;
 
-typedef uint64_t efitime_t;
+/**
+ * We use a signed type here so that subtraction result is a proper negative value.
+ * A typical use-case negative result is when we do 'timeNow() - timeOfEvent' where timeOfEvent
+ * is actually after timeNow() due to interrupt context switches
+ */
+typedef int64_t efitime_t;
 
 /**
  * 64 bit time in microseconds, since boot
