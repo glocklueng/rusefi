2b7e21f2e5beddec297a45905c0109c3c166989d
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-09-06 00:40:00 +0000

    dodge stratus

---
+++ firmware/config/engines/dodge_stratus.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file dodge_stratus.cpp
+ *
+ * @date Sep 5, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2015
+ */
+
+#include "main.h"
+#include "dodge_stratus.h"
+
+EXTERN_ENGINE
+;
+
+void setDodgeStratus(DECLARE_ENGINE_PARAMETER_F) {
+
+}
+
+
