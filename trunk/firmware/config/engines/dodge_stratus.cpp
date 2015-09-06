98fd08cab9415bc215a7dfa1c7471831d466e775
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-09-06 02:59:29 +0000

    dodge stratus

--- trunk/firmware/config/engines/dodge_stratus.cpp
+++ trunk/firmware/config/engines/dodge_stratus.cpp
@@ -18,6 +18,14 @@ void setDodgeStratus(DECLARE_ENGINE_PARAMETER_F) {
 	engineConfiguration->specs.cylindersCount = 6;
 	engineConfiguration->specs.firingOrder = FO_1_THEN_2_THEN_3_THEN_4_THEN_5_THEN_6;
 
+	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
+
+	boardConfiguration->ignitionPins[0] = GPIOC_9;
+	boardConfiguration->ignitionPins[1] = GPIO_UNASSIGNED;
+	boardConfiguration->ignitionPins[2] = GPIOE_8;
+	boardConfiguration->ignitionPins[3] = GPIO_UNASSIGNED;
+	boardConfiguration->ignitionPins[4] = GPIO_UNASSIGNED;
+	boardConfiguration->ignitionPins[5] = GPIO_UNASSIGNED;
 
 }
 
