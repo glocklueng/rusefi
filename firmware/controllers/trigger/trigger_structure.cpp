9f27ce44e45859a4e8513a72e86f9ac0de274db4
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-09-12 04:37:53 +0000

    36_2_2_2

--- firmware/controllers/trigger/trigger_structure.cpp
+++ firmware/controllers/trigger/trigger_structure.cpp
@@ -48,6 +48,7 @@ TriggerShape::TriggerShape() :
 	skippedToothCount = totalToothCount = 0;
 	syncRatioFrom = syncRatioTo = 0;
 	secondSyncRatioFrom = 0.000001;
+	secondSyncRatioTo = 100000;
 	memset(eventAngles, 0, sizeof(eventAngles));
 	memset(frontOnlyIndexes, 0, sizeof(frontOnlyIndexes));
 	memset(isFrontEvent, 0, sizeof(isFrontEvent));
