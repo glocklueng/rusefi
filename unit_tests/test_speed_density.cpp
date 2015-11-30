2f530b9a3becd17eab8aab7e2f6f36c48f021901
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-11-30 03:01:09 +0000

    improving test

--- unit_tests/test_speed_density.cpp
+++ unit_tests/test_speed_density.cpp
@@ -29,6 +29,8 @@ void testSpeedDensity(void) {
 
 	float airMass = getAirMass(eth.ec, 0.92, 98, 293.16);
 
+	assertEquals(0.9371, airMass);
+
 	// 0.01414 sec or 14.14 ms
 	assertEquals(0.01414, sdMath(eth.ec, airMass, 12.5));
 }
