defa124579a208427a9658cba01ad652bbcef84f
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2015-12-26 16:48:42 +0000

    pause for formulas

--- trunk/java_console/ui/src/com/rusefi/ui/FormulasPane.java
+++ trunk/java_console/ui/src/com/rusefi/ui/FormulasPane.java
@@ -6,6 +6,7 @@ import com.rusefi.config.Fields;
 import com.rusefi.core.Sensor;
 import com.rusefi.core.SensorCentral;
 import com.rusefi.ui.config.ConfigField;
+import com.rusefi.ui.util.UiUtils;
 import org.jetbrains.annotations.NotNull;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
@@ -17,19 +18,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import static com.rusefi.ui.util.LocalizedMessages.PAUSE;
+
 public class FormulasPane {
     private final JPanel content = new JPanel(new BorderLayout());
     private final JPanel centerProxy = new JPanel(new BorderLayout());
     private final String newLine = "\r\n \\\\ ";
+    private boolean isPaused;
 
     public FormulasPane() {
         content.add(centerProxy, BorderLayout.CENTER);
+
+        final JButton pauseButton = new JButton(PAUSE.getMessage());
+        pauseButton.setMnemonic('p');
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isPaused = !isPaused;
+                UiUtils.setPauseButtonText(pauseButton, isPaused);
+            }
+        });
+        JPanel top = new JPanel(new FlowLayout());
+        top.add(pauseButton);
+        content.add(top, BorderLayout.NORTH);
 
         updateFormula();
         new Timer(200, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateFormula();
+                if (!isPaused)
+                    updateFormula();
             }
         }).start();
     }
