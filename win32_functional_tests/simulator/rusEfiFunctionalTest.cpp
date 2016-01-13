baac2cb0a483806ceec01f709b3e86f7be5f32d3
rusefi <rusefi@f70035ab-f2bc-4b8d-aa29-9bd2fd43cf08>
2016-01-13 02:19:21 +0000

    refactoring & FSIO for simulator

--- win32_functional_tests/simulator/rusEfiFunctionalTest.cpp
+++ win32_functional_tests/simulator/rusEfiFunctionalTest.cpp
@@ -85,8 +85,6 @@ void rusEfiFunctionalTest(void) {
 	resetConfigurationExt(NULL, FORD_ESCORT_GT PASS_ENGINE_PARAMETER);
 	prepareShapes(PASS_ENGINE_PARAMETER_F);
 
-	initSensors(&sharedLogger PASS_ENGINE_PARAMETER);
-
 	initAlgo(&sharedLogger, engineConfiguration);
 	commonInitEngineController();
 
@@ -107,12 +105,6 @@ void rusEfiFunctionalTest(void) {
 	initMainEventListener(&sharedLogger, engine);
 
 	startStatusThreads(engine);
-
-#if EFI_TUNER_STUDIO
-	if (engineConfiguration->isTunerStudioEnabled) {
-		startTunerStudioConnectivity();
-	}
-#endif
 
 	initPeriodicEvents(PASS_ENGINE_PARAMETER_F);
 }
